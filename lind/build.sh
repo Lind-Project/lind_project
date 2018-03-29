49c0894eda2df8809cafed2b2bdb9e0a617fb5d4
Joey Pabalinas <joeypabalinas@gmail.com>
2018-03-28 17:28:32 -1000

    build.sh: change back from patch to git apply
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- lind/build.sh
+++ lind/build.sh
@@ -146,7 +146,7 @@ function download_src() {
 
 	# convert files from python to python2
 	cd "$NACL_SRC/native_client" || exit 1
-	# patch -p1 <"$LIND_SRC/native_client.patch"
+	# git apply -v "$LIND_SRC/native_client.patch"
 	"${PYGREPL[@]}" 2>/dev/null | \
 		"${PYGREPV[@]}" | \
 		while read -r file; do
