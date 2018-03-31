f0f26152a5327db60023eaafc01681ebd07cb0d5
Joey Pabalinas <joeypabalinas@gmail.com>
2018-03-31 11:56:53 -1000

    build.sh: go back to cleaning toolchain by default
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- lind/build.sh
+++ lind/build.sh
@@ -388,7 +388,6 @@ function build_glibc() {
 	done
 
 	# turns out this works better if you do it from the nacl base dir
-	# cd "$NACL_TOOLCHAIN_BASE" || exit 1
 	cd "$NACL_TOOLCHAIN_BASE" && rm -fr BUILD out
 	make -j"$JOBS" clean build-with-glibc || exit -1
 	print "Done building toolchain"
