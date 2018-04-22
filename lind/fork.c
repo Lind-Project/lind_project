79aece1648fadb0a6016c6fb73964f50167b18ab
Joey Pabalinas <joeypabalinas@gmail.com>
2018-04-21 22:15:01 -1000

    fork.c: add fork test file
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- lind/fork.c
+++ lind/fork.c
@@ -7,14 +7,17 @@ int main(void)
 {
 	int ret;
 
+	/* puts("forking"); */
 	switch (fork()) {
 	case -1:
+		puts("fork() failed");
 		perror("fork()");
 		exit(EXIT_FAILURE);
 	case 0:
 		puts("child succeeded");
 		exit(EXIT_SUCCESS);
 	}
+	puts("parent waiting");
 	wait(&ret);
 	puts("parent succeeded");
 
