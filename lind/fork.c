0a023abafd8202852d8e614a9b0016b7bfd58f9c
Joey Pabalinas <joeypabalinas@gmail.com>
2018-05-04 23:15:17 -1000

    caging.sh: update branch names
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- lind/fork.c
+++ lind/fork.c
@@ -5,26 +5,38 @@
 
 int main(void)
 {
-	int ret;
+	int pid, ret;
 
-	puts("forking");
+	printf("%d\n", getpid());
+	puts("forking 1");
+	puts("forking 2");
+	puts("forking 3");
+	puts("forking 4");
+	puts("forking 5");
+	puts("forking 6");
+	puts("forking 7");
+	puts("forking 8");
+	puts("forking 9");
+	puts("forking 10");
 	fflush(0);
 
-	switch (fork()) {
+	switch ((pid = fork())) {
 	case -1:
-		puts("fork() failed");
-		perror("fork()");
+		perror("fork() failed");
+		fflush(0);
 		exit(EXIT_FAILURE);
 	case 0:
 		puts("child succeeded");
+		printf("%d\n", getpid());
 		fflush(0);
 		exit(EXIT_SUCCESS);
 	}
 
+	printf("%d\n", getpid());
 	puts("parent waiting");
 	fflush(0);
 	wait(&ret);
-	puts("parent succeeded");
+	printf("parent %d succeeded after waiting on %d\n", getpid(), pid);
 	fflush(0);
 
 	return 0;
