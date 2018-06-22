100e231af2613b679ef0d3d0da5bd14c3c94edde
Joey Pabalinas <joeypabalinas@gmail.com>
2018-06-21 21:23:23 -1000

    test_cases: update fork() testcase
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- test_cases/fork.c
+++ test_cases/fork.c
@@ -5,29 +5,20 @@
 
 int main(void)
 {
-	int ppid = -1, cpid = -1, pret = 0, cret = 0;
+	int pid = -1, cpid = -1, ppid = -1, pret = 0, cret = 0, ppret = 0;
 	static const char c[] = "child waiting\n", p[] = "parent waiting\n";
 
-	puts("parent forking");
+	puts("1st parent forking");
 	fflush(0);
 
-	/*
-	 * printf("%d\n", getpid());
-	 * fork();
-	 * puts("hi");
-	 * fflush(0);
-	 * exit(EXIT_SUCCESS);
-	 */
-
-	switch ((ppid = fork())) {
+	switch ((pid = fork())) {
 	case -1:
 		perror("fork() failed");
 		fflush(0);
-		/* _exit(EXIT_FAILURE); */
 		exit(EXIT_FAILURE);
 
 	case 0:
-		puts("child forking");
+		puts("1st child forking");
 		fflush(0);
 		switch ((cpid = fork())) {
 		case -1:
@@ -35,47 +26,58 @@ int main(void)
 			fflush(0);
 			exit(EXIT_FAILURE);
 		case 0:
-			printf("child %d succeeded\n", getpid());
+			printf("1st child %d succeeded\n", getpid());
 			fflush(0);
+			sleep(2);
 			exit(EXIT_SUCCESS);
 		}
 
 		write(STDOUT_FILENO, c, sizeof c - 1);
 		if (waitpid(cpid, &cret, 0) == -1)
-			perror("child");
+			perror("1st child");
+		if (wait(&cret) == -1)
+			perror("1st child");
 		fflush(0);
-		printf("child %d succeeded after waiting on %d\n", getpid(), cpid);
+		printf("1st child %d succeeded after waiting on %d\n", getpid(), cpid);
 		fflush(0);
+		sleep(2);
 		exit(EXIT_SUCCESS);
 	}
 
 	fflush(0);
 	write(STDOUT_FILENO, p, sizeof p - 1);
-	if (waitpid(ppid, &pret, 0) == -1)
+	/*
+	 * if (waitpid(pid, &pret, 0) == -1)
+	 *         perror("1st parent");
+	 */
+	if (wait(&pret) == -1)
 		perror("1st parent");
-	/* printf("parent wait ret: %d\n", wait(&pret)); */
 	fflush(0);
-	printf("parent %d succeeded after waiting on %d\n", getpid(), ppid);
+	printf("1st parent %d succeeded after waiting on %d\n", getpid(), ppid);
 	fflush(0);
 
+	puts("2nd parent forking");
 	switch ((ppid = fork())) {
 	case -1:
 		perror("fork() failed");
 		fflush(0);
 		exit(EXIT_FAILURE);
 	case 0:
-		printf("child %d succeeded\n", getpid());
+		printf("2nd child %d succeeded\n", getpid());
 		fflush(0);
+		sleep(2);
 		exit(EXIT_SUCCESS);
 	}
-	write(STDOUT_FILENO, p, sizeof c - 1);
-	if (waitpid(ppid, &pret, 0) == -1)
+	write(STDOUT_FILENO, p, sizeof p - 1);
+	/*
+	 * if (waitpid(ppid, &ppret, 0) == -1)
+	 *         perror("2nd parent");
+	 */
+	if (wait(&ppret) == -1)
 		perror("2nd parent");
-	/* printf("parent wait ret: %d\n", wait(&pret)); */
 	fflush(0);
-	printf("parent %d succeeded after waiting on %d\n", getpid(), ppid);
+	printf("2nd parent %d succeeded after waiting on %d\n", getpid(), ppid);
 	fflush(0);
 
-	/* _exit(EXIT_SUCCESS); */
 	return 0;
 }
