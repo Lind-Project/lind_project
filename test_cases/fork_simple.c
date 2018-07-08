0155a2356507b75278614232925fa9e49c8e844d
Joey Pabalinas <joeypabalinas@gmail.com>
2018-07-07 20:22:25 -1000

    test_cases: simplify test cases
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- test_cases/fork_simple.c
+++ test_cases/fork_simple.c
@@ -4,55 +4,51 @@
 #include <unistd.h>
 #include <wait.h>
 
+static volatile size_t counter = 0;
+#define printf(s, ...) printf("[#%zu:L%u] " s, ++counter, __LINE__, __VA_ARGS__)
+
 int main(void)
 {
-	int pret = ~0, cret = ~1, ppret = ~2;
+	int pret = -1, cret = -1, ppret = -1;
 	int pid = -1, cpid = -1, ppid = -1, unused = -1;
 
 	(void)pret, (void)cret, (void)ppret;
 	(void)pid, (void)cpid, (void)ppid, (void)unused;
 
-	printf("[1] pid %d forking\n", getpid());
+	printf("pid %d forking\n", getpid());
 	fflush(0);
 
 	switch ((pid = fork())) {
 	case 0:
-		printf("[2] pid %d forking\n", getpid());
+		printf("pid %d forking\n", getpid());
 		fflush(0);
 
 		/*
 		 * switch ((cpid = fork())) {
 		 * case 0:
-		 *         printf("[3] pid = %d fork ret = %d\n", getpid(), cpid);
-		 *         fflush(0);
-		 *         exit(EXIT_SUCCESS);
-		 * default:
-		 *         printf("[4] pid = %d fork ret = %d\n", getpid(), cpid);
+		 *         printf("pid = %d fork ret = %d\n", getpid(), cpid);
 		 *         fflush(0);
 		 *         exit(EXIT_SUCCESS);
 		 * }
 		 */
 
-		fork();
-		printf("[3] [4] pid %d forking\n", getpid());
-		exit(EXIT_SUCCESS);
-
-		puts("wtf");
+		cpid = fork();
+		printf("pid = %d fork ret = %d\n", getpid(), cpid);
 		fflush(0);
-		abort();
+		exit(EXIT_SUCCESS);
 	}
 
-	printf("[5]pid %d forking again\n", getpid());
+	printf("pid %d forking again\n", getpid());
 	fflush(0);
 
 	switch ((ppid = fork())) {
 	case 0:
-		printf("[6] pid = %d fork ret = %d\n", getpid(), ppid);
+		printf("pid = %d fork ret = %d\n", getpid(), ppid);
 		fflush(0);
 		exit(EXIT_SUCCESS);
 	}
 
-	printf("[7] pid = %d fork ret = %d\n", getpid(), ppid);
+	printf("pid = %d fork ret = %d\n", getpid(), ppid);
 	fflush(0);
 
 	return 0;
