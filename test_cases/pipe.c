5cd9cb26a8cba573d979cef566e12061b46dd0b2
Joey Pabalinas <joeypabalinas@gmail.com>
2018-07-19 18:44:27 -1000

    .travis.yml: split up targets
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- test_cases/pipe.c
+++ test_cases/pipe.c
@@ -1,9 +1,11 @@
 #undef _GNU_SOURCE
 #define _GNU_SOURCE
 
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <wait.h>
@@ -15,12 +17,21 @@ int main(void)
 
 	pipe(fd);
 	printf("pipe() ret: [%d, %d]\n", fd[0], fd[1]);
-	write(fd[1], "hi\n", 3);
-	if ((ret = read(fd[0], str, sizeof str)) == -1) {
-		perror("read()");
-		return 1;
+
+	if ((ret = write(fd[1], "hi\n", 3)) < 0) {
+		printf("write(): %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	printf("write() ret: %d\n", ret);
+
+	if ((ret = read(fd[0], str, 3)) < 0) {
+		printf("read(): %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
 	}
 	printf("read() ret: %d\n", ret);
+
+	close(fd[0]);
+	close(fd[1]);
 	puts(str);
 
 	return 0;
