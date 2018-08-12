694c2860d54f4b5402938f1b2735197e49e71fde
Joey Pabalinas <joeypabalinas@gmail.com>
2018-08-12 11:02:38 -1000

    test_cases: update read()/write() uses in test cases
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- test_cases/fork.c
+++ test_cases/fork.c
@@ -18,6 +18,7 @@ int main(void)
 	int pret = -1, cret = -1, ppret = -1, pid = -1, cpid = -1, ppid = -1;
 
 	printf("pid %d forking\n", getpid());
+	fflush(0);
 
 	switch ((pid = fork())) {
 	case -1:
@@ -26,26 +27,31 @@ int main(void)
 
 	case 0:
 		printf("pid %d forking\n", getpid());
+		fflush(0);
+
 		switch ((cpid = fork())) {
 		case -1:
 			perror("fork() failed");
 			exit(EXIT_FAILURE);
 		case 0:
 			printf("pid = %d, ret = %d, status = %d\n", getpid(), cpid, EXIT_SUCCESS);
+			fflush(0);
 			exit(EXIT_SUCCESS);
 		}
 		printf("pid %d waiting on -1 (PID_ANY), child is %d\n", getpid(), cpid);
-		if (waitpid(PID_ANY, &cret, 0) == -1)
+		if (waitpid(PID_ANY, &cret, 0) < 0)
 			perror("waitpid()");
 		printf("pid = %d, ret = %d, status = %d\n", getpid(), cpid, cret);
+		fflush(0);
 		exit(EXIT_SUCCESS);
 	}
 
 	printf("pid %d waiting on %d\n", getpid(), pid);
-	if (waitpid(pid, &pret, 0) == -1)
+	if (waitpid(pid, &pret, 0) < 0)
 		perror("waitpid()");
 	printf("pid = %d, ret = %d, status = %d\n", getpid(), pid, pret);
 	printf("pid %d forking\n", getpid());
+	fflush(0);
 
 	switch ((ppid = fork())) {
 	case -1:
@@ -53,12 +59,14 @@ int main(void)
 		exit(EXIT_FAILURE);
 	case 0:
 		printf("pid = %d, ret = %d, status = %d\n", getpid(), ppid, EXIT_SUCCESS);
+		fflush(0);
 		exit(EXIT_SUCCESS);
 	}
 
-	if (wait(&ppret) == -1)
+	if (wait(&ppret) < 0)
 		perror("wait()");
 	printf("pid = %d, ret = %d, status = %d\n", getpid(), ppid, ppret);
+	fflush(0);
 
 	return 0;
 }
