537a201e3f243c7fd40ceabacfa025a2af4319f1
kliu513 <62267195+kliu513@users.noreply.github.com>
2020-09-08 23:32:05 +0800

    Fixed a typo

--- tests/test_cases/write.py
+++ tests/test_cases/write.py
@@ -8,10 +8,10 @@ if len(lind_result) != len(host_result):
     print "mismatched number of lines"
     exit(-1)
 
-if lind_result[1] != host_result[0]:
+if lind_result[1] != host_result[1]:
     print "mismatched deterministic lines"
     exit(-1)
 
-if lind_result[0] != host_result[1]:
+if lind_result[0] != host_result[0]:
     print "mismatched messages written to STDOUT_FILENO"
     exit(-1)
