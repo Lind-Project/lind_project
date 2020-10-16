4880bc4213d6bb5887ba895a0f607018183a30fd
Çağlar Doğan <dogancaglar05@gmail.com>
2020-10-17 01:24:31 +0300

    Update ngx_auto_config.h

--- tests/applications/nginx/objs/ngx_auto_config.h
+++ tests/applications/nginx/objs/ngx_auto_config.h
@@ -1,3 +1,7 @@
+#ifndef NGX_SYS_NERR
+#define NGX_SYS_NERR  132
+#endif
+
 #define NGX_CONFIGURE " --with-compat --with-debug --with-file-aio --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_degradation_module --with-http_flv_module --with-http_geoip_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-pcre-jit --with-stream --with-stream_geoip_module --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-threads"
 
 #ifndef NGX_DEBUG
