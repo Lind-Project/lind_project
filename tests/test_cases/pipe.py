108f451563daf924dc515e1fa7e7a7ab844ef990
kliu513 <62267195+kliu513@users.noreply.github.com>
2020-11-10 23:49:47 +0800

    Fixed a typo

--- tests/test_cases/pipe.py
+++ tests/test_cases/pipe.py
@@ -9,7 +9,7 @@ lind_line0 = lind_result[0].split(',')
 host_line0 = host_result[0].split(',')
 
 if not (lind_line0[0][-1].isdigit() and lind_line0[1][1].isdigit() \
-    and host_line0[0][-1].isdigit() and host_line[1][1].isdigit()):
+    and host_line0[0][-1].isdigit() and host_line0[1][1].isdigit()):
     print "Nondeterministic lines of an unrecognized format!"
     exit(-1)
 
