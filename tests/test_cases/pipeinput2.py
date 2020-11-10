2472ece53cd43217a227ff1e56913482204ab979
kliu513 <62267195+kliu513@users.noreply.github.com>
2020-11-10 23:58:41 +0800

    pipeinput.py/pipeinput2.py

---
+++ tests/test_cases/pipeinput2.py
@@ -0,0 +1,11 @@
+#!/bin/python2
+# Checks if pipeinput2.c runs the same natively as it does within Lind
+import sys
+
+lind_result = sys.argv[1].split('\n')
+host_result = sys.argv[2].split('\n')
+
+if not (lind_result[1].split(": ")[1].isdigit() \
+    and host_result[1].split(": ")[1].isdigit()):
+    print "Nondeterministic lines of an unrecognized format!"
+    exit(-1)
