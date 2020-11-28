b2e4cceaef29f24d6378f6875f0050c7e016f8e6
Çağlar Doğan <dogancaglar05@gmail.com>
2020-11-28 22:51:29 +0300

    Create empty2.c

---
+++ tests/test_cases/empty2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() 
+{   
+    int buffersize = 256;
+    char hostbuffer[buffersize]; 
+  
+    printf("Hostname: %s\n", "hi"); 
+
+    return 0; 
+} 
