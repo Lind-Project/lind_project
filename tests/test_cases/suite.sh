4062b55c0325bbef741aa4f5d242142bf79058ed
kliu513 <62267195+kliu513@users.noreply.github.com>
2020-12-01 14:00:19 +0800

    test

--- tests/test_cases/suite.sh
+++ tests/test_cases/suite.sh
@@ -27,7 +27,7 @@ echo ${deterministicinput[@]}
 echo ${nondeterministicinput[@]}
 totalarray=( "${deterministicinput[@]}" "${nondeterministicinput[@]}" )
 echo "Compiling test cases"
-cp nondet.txt $PWD/test_out
+
 x86_64-nacl-gcc-4.4.3 hello.c -o test_out/hello -std=gnu99;
 for var in "${totalarray[@]}"; do
     echo "Compiling test: $var"
@@ -37,6 +37,7 @@ for var in "${totalarray[@]}"; do
     gcc $var -o test_out/$varnonexe -lpthread
 done
 echo "Copying test cases"
+lindfs cp $PWD/ nondet.txt
 lindfs cp $PWD/test_out/ &> /dev/null
 
 echo "Executing deterministic test cases"
