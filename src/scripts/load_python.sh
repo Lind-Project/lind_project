77931452f1372eb0eae6183d809ebac8032fe6b0
aKpkc <11726205+kapkic@users.noreply.github.com>
2022-01-27 20:38:40 -0500

    Endlines added.

--- src/scripts/load_python.sh
+++ src/scripts/load_python.sh
@@ -4,7 +4,7 @@ cd /home/lind/lind_project/tests/applications/python/;
 lindfs cp /home/lind/lind_project/tests/applications/python/python /bin/python;
 
 echo "Copying libraries and configs"
-lindfs cp /home/lind/lind_project/tests/applications/python/Lib /usr/local/lib/python2.7	
+lindfs cp /home/lind/lind_project/tests/applications/python/Lib /usr/local/lib/python2.7
 lindfs cp /usr/lib/python2.7/config/ /usr/lib/python2.7/config/
 lindfs cp /usr/include/python2.7/ /usr/include/python2.7/
 lindfs cp /home/lind/lind_project/src/scripts/includes/passwd /etc/passwd
