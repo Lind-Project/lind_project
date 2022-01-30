fa98733312fe1c1e3f5541ebaeb16efd26e42a00
Ahmet Kapkic <ahmetkapkic@gmail.com>
2022-01-30 15:47:30 -0500

    Python script (#190)
    * Renaming daily build script
    
    * Loading python and bash scripts.
    
    * Executable option on scripts.
    
    * Fixed python location.
    
    * Endlines added.
    
    * PR Fix + coreutils script

---
+++ src/scripts/includes/pythonscript.sh
@@ -0,0 +1,3 @@
+export PYTHONHOME="/usr"
+export PYTHONPATH="/usr/local/lib/python2.7/:/usr/local/lib/python2.7/lib-dynload/:/usr/local/lib/python2.7/site-packages/"
+/bin/python "/hello.py" #you can rename, edit and copy the files hello.py and pythonscript.sh to make changes.
---
+++ src/scripts/load_python.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Builds and loads python executable into lind.
+
+echo "Building python"
+cd /home/lind/lind_project/tests/applications/python/;
+./bootstrap_nacl;
+lindfs cp /home/lind/lind_project/tests/applications/python/python /bin/python;
+
+echo "Copying libraries and configs"
+lindfs cp /home/lind/lind_project/tests/applications/python/Lib /usr/local/lib/python2.7
+lindfs cp /usr/lib/python2.7/config/ /usr/lib/python2.7/config/
+lindfs cp /usr/include/python2.7/ /usr/include/python2.7/
+lindfs cp /home/lind/lind_project/src/scripts/includes/passwd /etc/passwd
+
+echo "Python installed! To test it, load bash and use test_python.sh."
