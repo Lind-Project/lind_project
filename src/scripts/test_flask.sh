77f0c3cb83aa7dfda010fc64710b14517f275244
aKpkc <11726205+kapkic@users.noreply.github.com>
2022-02-04 10:35:32 -0500

    Typo fix.

--- src/scripts/test_flask.sh
+++ src/scripts/test_flask.sh
@@ -6,7 +6,7 @@ cd /home/lind/lind_project/src/scripts
 cd /home/lind/lind_project/src/scripts
 ./load_python.sh
 cd /home/lind/lind_project/src/scripts
-./load_python_libraries.sh
+./load_python_libs.sh
 
 echo "Copying flask files:"
 lindfs cp /home/lind/lind_project/tests/applications/python-modules/flask/ /usr/local/lib/python2.7/
