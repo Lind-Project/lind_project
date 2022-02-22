ed0aeb35ac183d188a5a41a13aa4694af927b25e
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-21 22:52:58 -0500

    arch

--- tests/applications/trinity/config-includes.sh
+++ tests/applications/trinity/config-includes.sh
@@ -8,8 +8,7 @@ cp /usr/include/netinet/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/lin
 cp /usr/include/arpa/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/arpa/
 cp /usr/include/bits/stdint-intn.h /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/
 cp /usr/include/bits/stdint-uintn.h /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/
-cp /usr/include/bits/uintn-.h /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/
 cp /usr/include/bits/uintn-identity.h /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/
 # redefine largefile
-sed -i '64,70d' /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bitsfcntl.h
-sed -i '64i#define O_LARGEFILE     0' /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bitsfcntl.h
+sed -i '64,70d' /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/fcntl.h
+sed -i '64i#define O_LARGEFILE     0' /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/fcntl.h
