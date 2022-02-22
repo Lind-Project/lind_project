757e7fd5a58e9a7960bf575696a11a5bb1b19a4c
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-21 19:23:02 -0500

    arch

---
+++ tests/applications/trinity/config-includes.sh
@@ -0,0 +1,15 @@
+mkdir /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/mtd/
+mkdir /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/linux/hdlc
+cp /usr/include/asm-generic/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/asm-generic/
+cp /usr/include/asm/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/asm/
+cp /usr/include/linux/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/linux/
+cp /usr/include/linux/hdlc/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/linux/hdlc/
+cp /usr/include/netinet/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/netinet/
+cp /usr/include/arpa/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/arpa/
+cp /usr/include/bits/stdint-intn.h /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/
+cp /usr/include/bits/stdint-uintn.h /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/
+cp /usr/include/bits/uintn-.h /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/
+cp /usr/include/bits/uintn-identity.h /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/
+# redefine largefile
+sed -i '64,70d' /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bitsfcntl.h
+sed -i '64i#define O_LARGEFILE     0' /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bitsfcntl.h
