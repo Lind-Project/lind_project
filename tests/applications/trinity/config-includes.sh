3c1dfaba952c1aee785cba9d0deb2a883cee9294
Nicholas Renner <nicholassrenner@gmail.com>
2022-02-22 19:35:05 -0500

    config

--- tests/applications/trinity/config-includes.sh
+++ tests/applications/trinity/config-includes.sh
@@ -4,6 +4,7 @@ cp /usr/include/asm-generic/* /home/lind/lind_project/lind/lindenv/sdk/toolchain
 cp /usr/include/asm/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/asm/
 cp /usr/include/linux/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/linux/
 cp /usr/include/linux/hdlc/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/linux/hdlc/
+cp /usr/include/mtd/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/mtd/
 cp /usr/include/netinet/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/netinet/
 cp /usr/include/arpa/* /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/arpa/
 cp /usr/include/bits/stdint-intn.h /home/lind/lind_project/lind/lindenv/sdk/toolchain/linux_x86_glibc/x86_64-nacl/include/bits/
