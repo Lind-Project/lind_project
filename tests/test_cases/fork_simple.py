a064faf54c5544f2b64602b73cd94d41f5de41b5
aKpkc <11726205+kapkic@users.noreply.github.com>
2022-03-11 15:18:49 -0500

    Fixes in some pipe/fork test cases.

--- tests/test_cases/fork_simple.py
+++ tests/test_cases/fork_simple.py
@@ -23,12 +23,12 @@ lind_result_sort = lind_result_split.sort()
 native_result_sort = native_result_split.sort()
 
 # Deterministic line comparisons
-if len(lind_result_split) != len(lind_result_split):
+if len(lind_result_sort) != len(native_result_sort):
     print "Mismatched number of lines!"
     exit(-1)
 
-resultLen = len(lind_result_split)
+resultLen = len(lind_result_sort)
 for line in range(0,resultLen):
-    if lind_result_split[line].translate(None, string.digits+'-') != native_result_split[line].translate(None, string.digits+'-'):
+    if lind_result_sort[line].translate(None, string.digits+'-') != native_result_sort[line].translate(None, string.digits+'-'):
         print "Nondeterministic lines of an unrecognized format!"
         exit(-1)
--- tests/test_cases/pipeonestring.py
+++ tests/test_cases/pipeonestring.py
@@ -5,27 +5,43 @@ import sys
 import string
 
 lind_result = sys.argv[1].split('\n')
-host_result = sys.argv[2].split('\n')
+native_result = sys.argv[2].split('\n')
+
+lind_result_sort = lind_result.sort(reverse=True)
+native_result_sort = native_result.sort(reverse=True)
+
+""" A default output looks like this:
+Parent: Testing open in parent: pid 218238!
+Parent: Pid 218238 opened fd 4
+Child: Testing open in child: pid 0!
+Child: Pid 0 opened fd -1 
+
+But if we order them; it will make sure the order does match with lind as we don't promise the same order.
+
+Alphabetically reverse ordering will give us the output in the same order. P...Test/P...Pid/C...Test/C...Pid
+
+"""
 
 # Deterministic line comparison
-if len(lind_result) != len(host_result):
+if len(lind_result_sort) != len(native_result_sort):
     print "Mismatched number of lines!"
     exit(-1)
 
-resultLen = len(lind_result)
+resultLen = len(lind_result_sort)
 for line in range(0,resultLen):
-    if lind_result[line].rstrip(string.digits) != host_result[line].rstrip(string.digits):
+    if lind_result_sort[line].rstrip(string.digits) != native_result_sort[line].rstrip(string.digits):
         print "Lines do not match!"
         exit(-1)
 
 # Nondeterministic digit check
-lind_parent = lind_result[1].split(' ')[-1]
-lind_child = lind_result[2].split(' ')[-1]
-host_parent = host_result[1].split(' ')[-1]
-host_child = host_result[2].split(' ')[-1]
+lind_parent = lind_result_sort[1].split(' ')[-1]
+lind_child = lind_result_sort[2].split(' ')[-1]
+native_parent = native_result_sort[1].split(' ')[-1]
+native_child = native_result_sort[2].split(' ')[-1]
+
 if not (lind_parent.isdigit() or lind_child.isdigit()):
     print "Nondeterministic lines of unrecognized format in lind output."
     exit(-1)
-if not (host_parent.isdigit() or host_child.isdigit()):
+if not (native_parent.isdigit() or native_child.isdigit()):
     print "Nondeterministic lines of unrecognized format in regular output."
     exit(-1)
