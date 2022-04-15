98a95ba1db7e82debc4da80998442d309a8983d8
aKpkc <11726205+kapkic@users.noreply.github.com>
2022-04-14 20:15:26 -0400

    fork_simple fixes.

--- tests/test_cases/fork_simple.c
+++ tests/test_cases/fork_simple.c
@@ -15,6 +15,7 @@ int main(void)
 	int pret = -1, cret = -1, ppret = -1, pid = -1, cpid = -1, ppid = -1, unused = -1;
 
 	printf("pid %d forking\n", getpid());
+	fflush(NULL);
 
 	switch ((pid = fork())) {
 	case -1:
@@ -22,6 +23,7 @@ int main(void)
 		exit(EXIT_FAILURE);
 	case 0:
 		printf("pid %d forking\n", getpid());
+		fflush(NULL);
 
 		switch ((cpid = fork())) {
 		case -1:
@@ -29,15 +31,19 @@ int main(void)
 			exit(EXIT_FAILURE);
 		case 0:
 			printf("pid = %d, ret = %d\n", getpid(), cpid);
+			fflush(NULL);
 			exit(EXIT_SUCCESS);
 		}
 
 		printf("pid = %d, ret = %d\n", getpid(), cpid);
+		fflush(NULL);
 		exit(EXIT_SUCCESS);
 	}
 
 	printf("pid = %d, ret = %d\n", getpid(), pid);
+	fflush(NULL);
 	printf("pid %d forking again\n", getpid());
+	fflush(NULL);
 
 	switch ((ppid = fork())) {
 	case -1:
@@ -45,10 +51,12 @@ int main(void)
 		exit(EXIT_FAILURE);
 	case 0:
 		printf("pid = %d, ret = %d\n", getpid(), ppid);
+		fflush(NULL);
 		exit(EXIT_SUCCESS);
 	}
 
 	printf("pid = %d, ret = %d\n", getpid(), ppid);
+	fflush(NULL);
 
 	return 0;
 }
