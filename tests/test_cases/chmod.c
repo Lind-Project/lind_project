781b04f02a88c42c00501333c0eea9d27d8f4833
lind <lind@nyu.edu>
2022-05-21 08:59:34 -1000

    Used testfile.txt

--- tests/test_cases/chmod.c
+++ tests/test_cases/chmod.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv)
 {
-	char file_name[100] = "chmod_test.txt";
+	char file_name[100] = "testfile.txt";
 	if (chmod(file_name, S_IRWXU | S_IRWXG | S_IRWXO) !=  0)
 	{
 		perror("ERROR with CHMOD");
