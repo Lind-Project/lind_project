9470340529ef3bfa3c6ef55b1ed938e5656266fa
Nicholas Renner <nicholassrenner@gmail.com>
2022-06-19 19:04:58 -0400

    load confs script

--- src/scripts/load_bash.sh
+++ src/scripts/load_bash.sh
@@ -4,4 +4,7 @@ cd /home/lind/lind_project/tests/applications/bash/
 ./bootstrap_nacl;
 lindfs cp /home/lind/lind_project/tests/applications/bash/bash /bin/bash
 lindfs cp /home/lind/lind_project/tests/applications/bash/bash /bin/sh
+
+./load_confs.sh
+
 echo "Bash has been installed. Usage: lind /bin/bash /script.sh"
