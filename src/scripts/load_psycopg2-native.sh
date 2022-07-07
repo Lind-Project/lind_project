a066b6ea409abb15a862366a5802fb64c7ab3807
Devansh-Patel <devanshpatel1001@gmail.com>
2022-07-07 18:27:03 +0000

    fake_fs added

--- src/scripts/load_psycopg2-native.sh
+++ src/scripts/load_psycopg2-native.sh
@@ -1,7 +1,7 @@
 # Compiles postgres and psycopg2
 # build python and postgres before running
 
-mkdir -p /home/lind/lind_project/python-native/usr/local/lib/python2.7/lib-dynload/psycopg2/
+mkdir -p /home/lind/lind_project/fake_fs/usr/local/lib/python2.7/lib-dynload/psycopg2/
 cd /home/lind/lind_project/tests/applications/python-modules/psycopg2/
 ./build-native.sh
 
