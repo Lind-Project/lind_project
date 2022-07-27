f41771fe76eb43ac511e1f74a061ccd7ef9246ee
Devansh-Patel <devanshpatel1001@gmail.com>
2022-07-27 23:32:24 +0000

    added load_setuptools-native.sh

---
+++ src/scripts/load_setuptools-native.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/lind/lind_project/fake_fs/usr/local/lib/python2.7/site-packages/setuptools
+mkdir -p /home/lind/lind_project/fake_fs/usr/local/lib/python2.7/site-packages/setuptools-44.1.1.dist-info
+sudo cp -r /home/lind/.local/lib/python2.7/site-packages/setuptools/* /home/lind/lind_project/fake_fs/usr/local/lib/python2.7/site-packages/setuptools
+sudo cp -r /home/lind/.local/lib/python2.7/site-packages/setuptools-44.1.1.dist-info/* /home/lind/lind_project/fake_fs/usr/local/lib/python2.7/site-packages/setuptools-44.1.1.dist-info
