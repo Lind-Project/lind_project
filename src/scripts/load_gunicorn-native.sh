751fe4c8f43c0d38d2d5b0f1cd06db55be4d2279
Devansh-Patel <devanshpatel1001@gmail.com>
2022-07-28 00:09:15 +0000

    added load_gunicorn-native.sh

---
+++ src/scripts/load_gunicorn-native.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/lind/lind_project/fake_fs/usr/local/lib/python2.7/site-packages/gunicorn/
+echo "Copying files"
+cp -r /home/lind/lind_project/tests/applications/python-modules/gunicorn/* /home/lind/lind_project/fake_fs/usr/local/lib/python2.7/site-packages/gunicorn/
+
