19b696aa2006a9f0108621b1b035fab61c895476
Justin Koe <100324413+justyoyo123@users.noreply.github.com>
2022-08-08 18:35:43 -0400

    epoll test and textfile (#274)
    * just the test and text file
    
    * with .py and slight change to test
    
    * fixing .py error
    
    Co-authored-by: lind <lind@nyu.edu>

--- tests/test_cases/socketepoll.c
+++ tests/test_cases/socketepoll.c
@@ -19,7 +19,6 @@
 #define FALSE            0
 
 pthread_barrier_t closebarrier;
-pthread_barrier_t syncbarrier;
 
 void* client(void* v) { 
     int sock = 0, valread; 
@@ -41,7 +40,7 @@ void* client(void* v) {
     // Convert IPv4 and IPv6 addresses from text to binary form 
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-    pthread_barrier_wait(&syncbarrier);
+    sleep(1);
 
     connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     send(sock , hello , strlen(hello) , 0 );
@@ -150,8 +149,6 @@ void* server(void* v) {
       exit(-1);
    }
 
-   pthread_barrier_wait(&syncbarrier);
-
    while(1) 
    {
       int n, i;
@@ -163,8 +160,7 @@ void* server(void* v) {
       }
       if (connected == 1 && n == 0 && num_fd == 0) 
       {
-         pthread_barrier_wait(&closebarrier);
-	 return NULL;
+         exit(0);
       }
       for (i = 0; i < n; ++i) 
       {
@@ -269,13 +265,9 @@ void* server(void* v) {
 
 int main() {
     pthread_t serverthread, clientthread1;
-    
-    pthread_barrier_init(&syncbarrier, NULL, 2);
     pthread_create(&serverthread, NULL, server, NULL);
     pthread_create(&clientthread1, NULL, client, NULL);
     pthread_join(clientthread1, NULL);
     pthread_join(serverthread, NULL);
-    pthread_barrier_destroy(&syncbarrier);
-    pthread_barrier_destroy(&closebarrier);
     return 0;
 }
