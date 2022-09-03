0ba86dc59cf6d9e66053a4571660c9910dd51634
lind <lind@nyu.edu>
2022-09-03 01:08:33 +0000

    added pkg_resources

---
+++ src/scripts/load_setuptools.sh
@@ -0,0 +1,3 @@
+lindfs cp /home/lind/.local/lib/python2.7/site-packages/setuptools/ /usr/local/lib/python2.7/site-packages/setuptools
+lindfs cp /home/lind/.local/lib/python2.7/site-packages/setuptools-44.1.1.dist-info/ /usr/local/lib/python2.7/site-packages/setuptools-44.1.1.dist-info
+lindfs cp /home/lind/.local/lib/python2.7/site-packages/pkg_resources /usr/local/lib/python2.7/site-packages/pkg_resources
