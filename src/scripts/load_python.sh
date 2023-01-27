48a3b74b2c47943921fce422089e0c2bacc438a6
lind <lind@nyu.edu>
2023-01-27 21:16:59 +0000

    python gunicorn updates

--- src/scripts/load_python.sh
+++ src/scripts/load_python.sh
@@ -9,8 +9,8 @@ lindfs cp /home/lind/lind_project/tests/applications/python/python /bin/python;
 echo "Copying libraries and configs"
 lindfs cp /home/lind/lind_project/tests/applications/python/libpython2.7.so.1.0 /lib/glibc/libpython2.7.so.1.0
 lindfs cp /home/lind/lind_project/tests/applications/python/Lib /usr/local/lib/python2.7
-lindfs cp /home/lind/lind_project/tests/applications/python/Makefile /usr/lib/python2.7/config/Makefile
-lindfs cp /home/lind/lind_project/tests/applications/python/pyconfig.h /usr/include/python2.7/pyconfig.h
+lindfs cp /home/lind/lind_project/tests/applications/python/Makefile /usr/local/lib/python2.7/config/Makefile
+lindfs cp /home/lind/lind_project/tests/applications/python/pyconfig.h /usr/local/include/python2.7/pyconfig.h
 lindfs cp /home/lind/lind_project/src/scripts/includes/passwd /etc/passwd
 
 echo "Python installed!"
