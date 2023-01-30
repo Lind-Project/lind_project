c338d455a4a1e7794a4bd0f520fa13a34b20e961
lind <lind@nyu.edu>
2023-01-30 01:31:38 +0000

    pr fix

--- src/scripts/lamp/compile_lamp.sh
+++ src/scripts/lamp/compile_lamp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/home/lind//lind_project/src/scripts/base/compile_bash.sh
-/home/lind//lind_project/src/scripts/base/compile_coreutils.sh
-/home/lind//lind_project/src/scripts/base/compile_confs.sh
+/home/lind/lind_project/src/scripts/base/compile_bash.sh
+/home/lind/lind_project/src/scripts/base/compile_coreutils.sh
+/home/lind/lind_project/src/scripts/base/compile_confs.sh
 
-/home/lind//lind_project/src/scripts/python/compile_python.sh
-/home/lind//lind_project/src/scripts/nginx/compile_nginx.sh
-/home/lind//lind_project/src/scripts/postgres/compile_postgres.sh
-/home/lind//lind_project/src/scripts/psycopg2/compile_psycopg2.sh
+/home/lind/lind_project/src/scripts/python/compile_python.sh
+/home/lind/lind_project/src/scripts/nginx/compile_nginx.sh
+/home/lind/lind_project/src/scripts/postgres/compile_postgres.sh
+/home/lind/lind_project/src/scripts/psycopg2/compile_psycopg2.sh
 
