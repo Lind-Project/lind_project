39e4d1289eb4cc302fef2f4f7af37ca4aec91eb2
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-06-19 10:54:43 -0400

    Add output check

--- tests/test_cases/statfs.c
+++ tests/test_cases/statfs.c
@@ -8,17 +8,51 @@
 #include <string.h>
 #include <sys/statfs.h>
 
+#define EXPECTED_TYPE 0x6969
+
+void test_statfs(const char *file_name){
+  struct statfs buf = {0};
+
+  if (statfs(file_name, &buf) == -1) {
+    perror("Error in fstatfs");
+    printf("File: %s\n", file_name);
+    return;
+  }
+
+  printf("Testing file: %s\n", file_name);
+  printf("File system type: %d\n", buf.f_type);
+  printf("Total file system blocks: %ld\n", buf.f_blocks);
+  printf("Free blocks: %ld\n", buf.f_bfree);
+  printf("Total file system inodes: %ld\n", buf.f_files);
+  printf("Free inodes: %ld\n", buf.f_ffree);
+  fflush(stdout);
+
+  if (buf.f_type != EXPECTED_TYPE) {
+    printf("Unexpected file system type for file %s: expected %d, got %d\n",
+      file_name, EXPECTED_TYPE, buf.f_type);
+  }
+  
+}
+
 int main(int argc, char **argv) {
   char file_name[100] = "testfiles/statfsfile.txt";
 
-  // statfs
-  struct statfs buf = {0};
-  if (statfs(file_name, &buf) == -1) {
-    perror("Error in fstatfs\n");
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <file1> <file2> ...\n", argv[0]);
     exit(EXIT_FAILURE);
   }
+
+  // statfs
+  // struct statfs buf = {0};
+  // if (statfs(file_name, &buf) == -1) {
+  //   perror("Error in fstatfs\n");
+  //   exit(EXIT_FAILURE);
+  // }
   // expected output is: NFS_SUPER_MAGIC 0x6969 on native CentOS 7 on CIMS
-  printf("filesystem type: %d", buf.f_type);
-  fflush(stdout);
-  return 0;
+  // printf("filesystem type: %d", buf.f_type);
+  // fflush(stdout);
+  for (int i = 1; i < argc; ++i) {
+    test_statfs(argv[i]);
+  }
+  return EXIT_SUCCESS;
 }
