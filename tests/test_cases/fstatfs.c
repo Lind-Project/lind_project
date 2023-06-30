2190bbae869b23c24d07ed81435f97e939f63d3e
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-06-30 18:48:13 -0400

    Add fstatfs test

---
+++ tests/test_cases/fstatfs.c
@@ -0,0 +1,37 @@
+#undef _GNU_SOURCE
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/statfs.h>
+
+int main(int argc, char **argv) {
+  // int fd = open("/dev/urandom", O_RDONLY, S_IRWXU|S_IRWXG|S_IRWXO);
+  int fd = open("testfiles/fstatfsfile.txt", O_RDONLY | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
+  if (fd < 0){
+    perror("Error in open()\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  // fstatfs
+  struct statfs buf = {0};
+  if (fstatfs(fd, &buf) == -1) {
+    perror("Error in fstatfs\n");
+    close(fd);
+    exit(EXIT_FAILURE);
+  }
+  close(fd);
+
+  // system file info
+  printf("Filesystem type: %lx\n", buf.f_type);
+  printf("Total blocks: %ld\n", buf.f_blocks);
+  printf("Free blocks: %ld\n", buf.f_bfree);
+  printf("Available blocks (non-root): %ld\n", buf.f_bavail);
+  printf("Total file inodes: %ld\n", buf.f_files);
+  printf("Free file inodes: %ld\n", buf.f_ffree);
+  fflush(stdout);
+  return 0;
+}
