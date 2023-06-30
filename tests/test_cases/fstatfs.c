32291d03ebbc5e701b3dfc9014782b3aae50ba75
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-06-30 16:35:55 -0400

    Update test file

--- tests/test_cases/fstatfs.c
+++ tests/test_cases/fstatfs.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
   // int fd = open("/dev/urandom", O_RDONLY, S_IRWXU|S_IRWXG|S_IRWXO);
-  int fd = open("testfiles/statfsfile.txt", O_RDONLY | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
+  int fd = open("testfiles/fstatfsfile.txt", O_RDONLY | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
   if (fd < 0){
     perror("Error in open()\n");
     exit(EXIT_FAILURE);
