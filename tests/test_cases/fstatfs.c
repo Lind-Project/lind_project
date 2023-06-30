3ce7b59baddc7f589a3522980dc4c6474bd7a47e
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-06-30 16:34:35 -0400

    Delete unrelevant comment

--- tests/test_cases/fstatfs.c
+++ tests/test_cases/fstatfs.c
@@ -8,9 +8,6 @@
 #include <string.h>
 #include <sys/statfs.h>
 
-// extern char **environ;
-// const char* FILENAME = "testfiles/statfsfile.txt";
-
 int main(int argc, char **argv) {
   // int fd = open("/dev/urandom", O_RDONLY, S_IRWXU|S_IRWXG|S_IRWXO);
   int fd = open("testfiles/statfsfile.txt", O_RDONLY | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
@@ -19,7 +16,6 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
   
-
   // fstatfs
   struct statfs buf = {0};
   if (fstatfs(fd, &buf) == -1) {
@@ -30,7 +26,6 @@ int main(int argc, char **argv) {
   close(fd);
 
   // system file info
-  // expected output is: NFS_SUPER_MAGIC 0x6969 on native CentOS 7 on CIMS
   printf("Filesystem type: %lx\n", buf.f_type);
   printf("Total blocks: %ld\n", buf.f_blocks);
   printf("Free blocks: %ld\n", buf.f_bfree);
