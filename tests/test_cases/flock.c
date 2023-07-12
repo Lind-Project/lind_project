dd8378a59726667db52cd3e3c8e8a7e39f60aa7a
kpan2034 <keyur.panchal@gmail.com>
2023-07-12 15:46:38 -0400

    Added flock test.

---
+++ tests/test_cases/flock.c
@@ -0,0 +1,77 @@
+#undef _GNU_SOURCE
+#define _GNU_SOURCE
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// Helper function to remove a file
+int remove_file(char file_name[]) {
+  int rm = remove(file_name);
+  if (rm == -1) {
+    perror("FAILED TO REMOVE FILE\n");
+    exit(EXIT_FAILURE);
+  }
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  // Create a temporary file to use
+  char file_name[100] = "testfiles/flock.txt";
+  int fd = open(file_name, O_RDWR | O_CREAT, 0777);
+  if (fd == -1) {
+    perror("OPEN FAILED\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Get an exclusive lock on the file
+  int lock = flock(fd, LOCK_EX);
+  if (lock == -1) {
+    perror("FAILED TO GET EXCLUSIVE LOCK\n");
+    close(fd);
+    remove_file(file_name);
+    exit(EXIT_FAILURE);
+  }
+  printf("Obtained exclusive lock on file\n");
+
+  // Release the exclusive lock on the file
+  lock = flock(fd, LOCK_UN);
+  if (lock == -1) {
+    perror("FAILED TO RELEASE EXCLUSIVE LOCK\n");
+    close(fd);
+    remove_file(file_name);
+    exit(EXIT_FAILURE);
+  }
+  printf("Released exclusive lock on file\n");
+
+  // Get a shared lock on the file
+  lock = flock(fd, LOCK_SH);
+  if (lock == -1) {
+    perror("FAILED TO GET SHARED LOCK\n");
+    close(fd);
+    remove_file(file_name);
+    exit(EXIT_FAILURE);
+  }
+  printf("Obtained shared lock on file\n");
+
+  // Release the shared lock on the file
+  lock = flock(fd, LOCK_UN);
+  if (lock == -1) {
+    perror("FAILED TO RELEASE SHARED LOCK\n");
+    close(fd);
+    remove_file(file_name);
+    exit(EXIT_FAILURE);
+  }
+  printf("Released shared lock on file\n");
+
+  // Close file before removing
+  close(fd);
+
+  // Remove the temporary file
+  remove(file_name);
+  return 0;
+}
