0069c964689ae984652ccf8bfcfc60034c93388d
Keyur Panchal <keyur.panchal@nyu.edu>
2023-07-14 18:10:44 -0400

    Updated getppid test to verify that pids match.

--- tests/test_cases/getppid.c
+++ tests/test_cases/getppid.c
@@ -4,14 +4,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(void) {
-  puts("printing pid");
 
-  printf("%d\n", getppid());
-  perror("");
-  puts("getppid() succeeded");
+  // Get pid of current process
+  pid_t pid = getpid();
+  fflush(stdout);
 
+  // Fork
+  int f = fork();
+  if(f == -1){
+    perror("fork() failed");
+    exit(1);
+  }
+
+  // Child process
+  if(f == 0){
+    pid_t ppid = getppid();
+    if (ppid == pid){
+      printf("getppid() succeeded\n");
+      fflush(stdout);
+      exit(EXIT_SUCCESS);
+    }
+    else {
+      printf("getppid() failed\n");
+      fflush(stdout);
+      exit(EXIT_FAILURE);
+    }
+  }
+  // Parent process
+  else {
+    // Wait till child process returns, and then only then exit
+    wait(NULL);
+  }
   return 0;
 }
