6feaf99d48c9e15ca661e3d20e34725e97987832
Keyur Panchal <keyur.panchal@nyu.edu>
2023-07-15 16:50:52 -0400

    Updated the flock test case to verify concurrent shared lock acquisition.

--- tests/test_cases/flock.c
+++ tests/test_cases/flock.c
@@ -9,6 +9,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#define FILE "testfiles/flock.txt"
+
 // Helper function to remove a file
 int remove_file(char file_name[]) {
   int rm = remove(file_name);
@@ -21,8 +23,7 @@ int remove_file(char file_name[]) {
 
 int main(int argc, char **argv) {
   // Create a temporary file to use
-  char file_name[100] = "testfiles/flock.txt";
-  int fd = open(file_name, O_RDWR | O_CREAT, 0777);
+  int fd = open(FILE, O_RDWR | O_CREAT, 0777);
   if (fd == -1) {
     perror("OPEN FAILED\n");
     exit(EXIT_FAILURE);
@@ -33,45 +34,88 @@ int main(int argc, char **argv) {
   if (lock == -1) {
     perror("FAILED TO GET EXCLUSIVE LOCK\n");
     close(fd);
-    remove_file(file_name);
+    remove_file(FILE);
     exit(EXIT_FAILURE);
   }
   printf("Obtained exclusive lock on file\n");
+  fflush(stdout);
 
   // Release the exclusive lock on the file
   lock = flock(fd, LOCK_UN);
   if (lock == -1) {
     perror("FAILED TO RELEASE EXCLUSIVE LOCK\n");
     close(fd);
-    remove_file(file_name);
+    remove_file(FILE);
     exit(EXIT_FAILURE);
   }
   printf("Released exclusive lock on file\n");
+  fflush(stdout);
 
-  // Get a shared lock on the file
+  // Get an shared lock on the file
   lock = flock(fd, LOCK_SH);
   if (lock == -1) {
     perror("FAILED TO GET SHARED LOCK\n");
     close(fd);
-    remove_file(file_name);
+    remove_file(FILE);
     exit(EXIT_FAILURE);
   }
   printf("Obtained shared lock on file\n");
+  fflush(stdout);
+
+  // Now fork this process
+  int f = fork();
+  if(f == -1){
+    perror("FAILED TO FORK\n");
+    exit(EXIT_FAILURE);
+  }
+
+  /* 
+  * We cannot use the same file descriptor in the child process,
+  * since the locks created by flock are associated with an open file table entry.
+  * This means that duplicate file descriptors refer to the same lock,
+  * and can be hence used across processes to acquire/release it.
+  */
+  if(f == 0){
+    // Get a new file descriptor in the child process
+    fd = open(FILE, O_RDWR, 0777);
+    if (fd == -1) {
+      perror("OPEN FAILED\n");
+      exit(EXIT_FAILURE);
+    }
+
+    // Get an shared lock on the file
+    lock = flock(fd, LOCK_SH);
+    if (lock == -1) {
+      perror("FAILED TO GET SHARED LOCK\n");
+      close(fd);
+      remove_file(FILE);
+      exit(EXIT_FAILURE);
+    }
+    printf("Obtained shared lock on file\n");
+    fflush(stdout);
+  }
+  else {
+    // Wait for the child process to finish first
+    wait(NULL);
+  }
 
   // Release the shared lock on the file
   lock = flock(fd, LOCK_UN);
   if (lock == -1) {
     perror("FAILED TO RELEASE SHARED LOCK\n");
     close(fd);
-    remove_file(file_name);
+    remove_file(FILE);
     exit(EXIT_FAILURE);
   }
   printf("Released shared lock on file\n");
+  fflush(stdout);
 
   // Close file before removing
   close(fd);
 
-  // Remove the temporary file
-  remove(file_name);
+  // Let only the parent process remove the temporary file
+  if(f != 0) {
+    remove(FILE);
+  }
   return 0;
 }
