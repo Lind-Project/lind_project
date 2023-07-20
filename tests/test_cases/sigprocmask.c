dd0d90283d49e3762b5da1565f8dc240335ff4ee
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-07-20 14:22:04 -0400

    add sigprocmask test

---
+++ tests/test_cases/sigprocmask.c
@@ -0,0 +1,65 @@
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void sigusr2_handler(int signum) {
+    printf("Received SIGUSR2 signal!\n");
+}
+
+void* thread_function(void* arg) {
+   // Block SIGUSR2 in this thread
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR2);
+
+    if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1) {
+        perror("sigprocmask");
+        exit(EXIT_FAILURE);
+    }
+
+    // The thread continues to run
+    while (1) {
+        sleep(1);
+    }
+    // Wait for SIGUSR2 signal
+    int sig;
+    sigwait(&mask, &sig);
+    printf("Received SIGUSR2 signal in the thread!\n");
+    fflush(NULL);
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int ret;
+
+    // Register the SIGUSR2 signal handler for the main thread
+    if (signal(SIGUSR2, sigusr2_handler) == SIG_ERR) {
+        perror("signal");
+        return EXIT_FAILURE;
+    }
+
+    // Create the thread
+    ret = pthread_create(&thread, NULL, thread_function, NULL);
+    if (ret != 0) {
+        perror("pthread_create");
+        return EXIT_FAILURE;
+    }
+
+    // Sleep briefly to ensure the thread has started
+    sleep(1);
+
+    // Send SIGUSR2 to the new thread
+    pthread_kill(thread, SIGUSR2);
+
+    // Wait for the thread to finish
+    pthread_join(thread, NULL);
+    printf("Successful");
+    fflush(NULL);
+    return EXIT_SUCCESS;
+}
+
