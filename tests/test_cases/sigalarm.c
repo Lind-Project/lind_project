1ff1b49495357c78bde48edcfbb965fe35408ab8
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-07-21 16:47:37 -0400

    Add sigalarm test

--- tests/test_cases/sigalarm.c
+++ tests/test_cases/sigalarm.c
@@ -11,14 +11,15 @@ void sig_handler() {
 int main() {
 	int i;
 
+    signal(SIGALRM, sig_handler);
+    signal(SIGALRM, SIG_DFL);
+    
     sigset_t set;
     sigemptyset(&set);
     sigaddset(&set, SIGALRM);
     sigprocmask(SIG_BLOCK, &set, NULL);
-    
-	alarm(5);
 
-    signal(SIGALRM, sig_handler);
+	alarm(5);
 
 	for (i = 1; i < 11; i++) {
         // Should print alarm on 6th
