b7ab288c2740e6cb2084e6b7f7475648b8b753f9
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-07-20 22:33:36 -0400

    modify sigprocmask test

--- tests/test_cases/sigprocmask.c
+++ tests/test_cases/sigprocmask.c
@@ -8,7 +8,6 @@
 
 void sh1(int sig){
     printf("signal %d received\n", sig);
-    fflush(stdout);
 }
 
 // void sh2(int sig){
@@ -18,13 +17,26 @@ void sh1(int sig){
 
 void* th1(void* arg){
     printf("I am thread01\n");
-    fflush(stdout);
+    char buf[512];
+    int n;
+    struct sigaction sa_usr;
+    sa_usr.sa_flags = 0;
+    sa_usr.sa_handler = sh1;   
     
-    sigset_t mask;
-    sigemptyset(&mask);
-    sigaddset(&mask, SIGUSR1);
-
-    sigprocmask(SIG_BLOCK, &mask, NULL);
+    sigaction(SIGUSR2, &sa_usr, NULL);
+    while(1){
+        // Blocking main 
+        if((n = read(STDIN_FILENO, buf, 511)) == -1){
+            if(errno == EINTR){
+                printf("read is interrupted by signal\n");
+                break;
+            }
+        }
+        else{
+            buf[n] = '\0';
+            printf("%d bytes read: %s\n", n, buf);
+        }
+    }
     
     return NULL;
 }
@@ -49,13 +61,13 @@ void* th1(void* arg){
 //     return NULL;
 // }
  
-static void sig_usr(int signum){
-    if(signum == SIGUSR2){
-        printf("SIGUSR2 received\n");
-    }else{
-        printf("signal %d received\n", signum);
-    }
-}
+// static void sig_usr(int signum){
+//     if(signum == SIGUSR2){
+//         printf("SIGUSR2 received\n");
+//     }else{
+//         printf("signal %d received\n", signum);
+//     }
+// }
  
 int main(void){
     // char buf[512];
