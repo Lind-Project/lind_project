0229626ffb88d20e5a81513bf94753aed00a734c
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-07-20 21:38:46 -0400

    modify sigprocmask test

--- tests/test_cases/sigprocmask.c
+++ tests/test_cases/sigprocmask.c
@@ -96,38 +96,29 @@
 #include <signal.h>
 #include <errno.h>
  
-static void sig_usr(int signum)
-{
-    if(signum == SIGUSR1)
-    {
+static void sig_usr(int signum){
+    if(signum == SIGUSR1){
         printf("SIGUSR1 received\n");
-    }
-    else if(signum == SIGUSR2)
-    {
-        printf("SIGUSR2 received\n");
-    }
-    else
+    }else
     {
         printf("signal %d received\n", signum);
     }
 }
  
-int main(void)
-{
+int main(void){
     char buf[512];
     int  n;
     struct sigaction sa_usr;
     sa_usr.sa_flags = 0;
-    sa_usr.sa_handler = sig_usr;   //信号处理函数
+    sa_usr.sa_handler = sig_usr;   
     
     sigaction(SIGUSR1, &sa_usr, NULL);
-    sigaction(SIGUSR2, &sa_usr, NULL);
     
     printf("My PID is %d\n", getpid());
     
     while(1)
     {
-        if((n = read(STDIN_FILENO, buf, 511)) == -1)
+        if(read(STDIN_FILENO, buf, 511) == -1)
         {
             if(errno == EINTR)
             {
@@ -140,6 +131,6 @@ int main(void)
             printf("%d bytes read: %s\n", n, buf);
         }
     }
-    
+    printf("End\n");
     return 0;
 }
