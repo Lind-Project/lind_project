c2f905d082f4aa0101529a8dd9531187bf05a385
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-07-21 15:05:21 -0400

    Add sigalarm test

---
+++ tests/test_cases/sigalarm.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+ 
+/* Signal Handler */
+void sig_handler() {
+ 
+	printf( "ALARM! \n " );
+}
+ 
+int main() {
+	int i;
+	signal( SIGALRM, sig_handler );
+	alarm(5);
+	for ( i = 1; i < 11; i++ ) {
+        // Should print alarm on 5th
+		printf( " sleep % d ... \n ", i);
+		sleep(1);
+	}
+	return 0;
+}
