c5f348b88910f43dc656f44c786d549c326ea354
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-07-21 14:01:05 -0400

    modify sigprocmask test

--- tests/test_cases/sigprocmask.c
+++ tests/test_cases/sigprocmask.c
@@ -6,16 +6,43 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define STR(x)   #x
+#define SHOW_DEFINE(x) printf("%s=%s\n", #x, STR(x))
+
 static void sig_usr(int signum){
-    if(signum == SIGUSR1){
-        printf("SIGUSR1 received\n");
-    }else{
-        printf("signal %d received\n", signum);
-    }
+    SHOW_DEFINE(signum);
 }
+
+// static void sig_usr2(int signum){
+//     SHOW_DEFINE(signum);
+// }
 
 // Thread function
 void* th1(void* arg){
+    printf("I am thread %ld\n", (long)arg);
+    char buf[512];
+    int n;
+    // Set signal handler
+    struct sigaction sa_usr;
+    sa_usr.sa_flags = 0;
+    sa_usr.sa_handler = sig_usr;   
+
+    sigaction(SIGUSR1, &sa_usr, NULL);
+
+    while(1){
+        // Blocking thread1
+        if((n = read(STDIN_FILENO, buf, 511)) == -1){
+            if(errno == EINTR){
+                printf("Thread is interrupted by EINTR\n");
+                break;
+            }
+        }
+    }
+    
+    return NULL;
+}
+
+void* th2(void* arg){
     printf("I am thread %ld\n", (long)arg);
     char buf[512];
     int n;
@@ -50,16 +77,16 @@ int main(void){
     
     sigaction(SIGUSR1, &sa_usr, NULL);
     
-    printf("My PID is %d\n", getpid());
-    // Type: "kill -USR1 (pid)" from another terminal
-    while(1){
-        if((n = read(STDIN_FILENO, buf, 511)) == -1){
-            if(errno == EINTR){
-                printf("Main is interrupted by EINTR\n");
-                break;
-            }
-        }
-    }
+    // printf("My PID is %d\n", getpid());
+    // // Type: "kill -USR1 (pid)" from another terminal
+    // while(1){
+    //     if((n = read(STDIN_FILENO, buf, 511)) == -1){
+    //         if(errno == EINTR){
+    //             printf("Main is interrupted by EINTR\n");
+    //             break;
+    //         }
+    //     }
+    // }
 
     /* Thread 1 */
     pthread_t thread1;
@@ -67,14 +94,16 @@ int main(void){
         perror("Thread1 failed");
     }
     sleep(1);
-    pthread_kill(thread1, SIGUSR1);
+
     /* Thread 2 */
     pthread_t thread2;
     if (pthread_create(&thread2, NULL, th1, (void*)(long)thread2) != 0){
         perror("Thread2 failed");
     }
     sleep(1);
-    pthread_kill(thread2, SIGUSR1);
+
+    pthread_kill(thread1, SIGUSR1);
+    // pthread_kill(thread2, SIGUSR1);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
