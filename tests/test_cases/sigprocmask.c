016274410dd9398ed85d9e03d85d4d2ed949475a
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-07-20 21:51:01 -0400

    modify sigprocmask test

--- tests/test_cases/sigprocmask.c
+++ tests/test_cases/sigprocmask.c
@@ -1,40 +1,39 @@
-// #include <errno.h>
-// #include <string.h>
-// #include <signal.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <pthread.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 
-// void sh1(int sig){
-//     printf("signal %d received\n", sig);
-//     fflush(stdout);
-// }
+void sh1(int sig){
+    printf("signal %d received\n", sig);
+    fflush(stdout);
+}
 
 // void sh2(int sig){
 //     printf("signal %d received\n", sig);
 //     fflush(stdout);
 // }
 
-// void* th1(void* arg){
-//     printf("I am thread01\n");
-//     fflush(stdout);
+void* th1(void* arg){
+    printf("I am thread01\n");
+    fflush(stdout);
     
-//     sigset_t mask;
-//     sigemptyset(&mask);
-//     sigaddset(&mask, SIGUSR2);
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGUSR2);
     
-//     struct sigaction sa1;
-//     sa1.sa_handler = sh1;
-//     sa1.sa_flags = 0;
-//     sa1.sa_mask = mask;
-//     sigaction(SIGUSR2, &sa1, NULL);
-    
+    struct sigaction sa1;
+    sa1.sa_handler = sh1;
+    sa1.sa_flags = 0;
+    sa1.sa_mask = mask;
+    sigaction(SIGUSR2, &sa1, NULL);
 
-//     sigprocmask(SIG_BLOCK, &mask, NULL);
+    sigprocmask(SIG_BLOCK, &mask, NULL);
     
-//     return NULL;
-// }
+    return NULL;
+}
 
 // void* th2(void* arg){
 //     printf("I am thread02\n");
@@ -91,14 +90,10 @@
 //     return EXIT_SUCCESS;
 
 // }
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <errno.h>
  
 static void sig_usr(int signum){
-    if(signum == SIGUSR1){
-        printf("SIGUSR1 received\n");
+    if(signum == SIGUSR2){
+        printf("SIGUSR2 received\n");
     }else{
         printf("signal %d received\n", signum);
     }
@@ -106,17 +101,17 @@ static void sig_usr(int signum){
  
 int main(void){
     char buf[512];
-    int  n;
+    int n;
     struct sigaction sa_usr;
     sa_usr.sa_flags = 0;
     sa_usr.sa_handler = sig_usr;   
     
-    sigaction(SIGUSR1, &sa_usr, NULL);
+    sigaction(SIGUSR2, &sa_usr, NULL);
     
     printf("My PID is %d\n", getpid());
     
     while(1){
-        if(read(STDIN_FILENO, buf, 511) == -1){
+        if((n = read(STDIN_FILENO, buf, 511)) == -1){
             if(errno == EINTR){
                 printf("read is interrupted by signal\n");
                 break;
@@ -127,6 +122,14 @@ int main(void){
             printf("%d bytes read: %s\n", n, buf);
         }
     }
+
+    pthread_t thread1;
+    pthread_create(&thread1, NULL, th1, NULL);
+    if (pthread_kill(thread1, SIGUSR2) != 0){
+        printf("Error of Thread 1 is: %s\n", strerror(errno));
+    }
+    pthread_join(thread1, NULL);
+
     printf("End\n");
     return 0;
 }
