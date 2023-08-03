566c1d17d70da2d8c12d8290097d39f2f79a1aae
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-03 19:08:22 -0400

    update sigpoll with fork

--- tests/test_cases/sigpoll.c
+++ tests/test_cases/sigpoll.c
@@ -1,40 +1,29 @@
-#undef _GNU_SOURCE
-#define _GNU_SOURCE
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <poll.h>
-#include <pthread.h>
 #include <errno.h>
+#include <string.h>
 #include <signal.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h> 
+#include <poll.h>
 
 #define TIMEOUT -1
 
-static void sig_usr(int signum){
-    printf("Thread1 received signal %d\n", signum);
+void handler(int signum)
+{
+    printf("Signal Handler Test!\n");
+    printf("Exiting Cage 2\n");
+    fflush(stdout);
+    exit(0);
 }
 
-// Thread function
-void* th(void* arg){
-    int id = *((int*)arg);
-    printf("Thread %d\n", id);
-    int n;
-    // Set signal handler
-    struct sigaction sa_usr;
-    sa_usr.sa_flags = 0;
-    sa_usr.sa_handler = sig_usr;   
+int main(void)
+{
+    struct sigaction new_action;
+    struct sigaction old_action;
 
-    sigaction(SIGUSR1, &sa_usr, NULL);
-    
-    return NULL;
-}
+    pid_t pid = fork();
 
- 
-int main(void){
-    int user1 = 1;
-    int user2 = 2;
     struct pollfd fds[1];
     int ret;
 
@@ -42,31 +31,37 @@ int main(void){
     fds[0].fd = 1;
     fds[0].events = POLLOUT;
 
-    /* The poll call is blocked indefinitely. */
-    ret = poll(fds, 1, TIMEOUT);
+    /* The poll call is blocked for the specified duration (in this case, 5 seconds). */
+    ret = poll(fds, 1, TIMEOUT * 1000);
 
-    if (ret == -1) {
-        perror("poll");
-        return 1;
+    if (pid == 0) {
+            // Cage 2
+            printf("Cage 2 pid: %ld\n", (long)getpid());
+            printf("Handler function ptr %p\n", handler);
+            fflush(stdout);
+
+            new_action.sa_handler = handler;
+
+            sigaction(SIGUSR2, &new_action, NULL);
+            sigaction(SIGUSR2, NULL, &old_action);
+
+            if (new_action.sa_handler == old_action.sa_handler) {
+                    printf("Successfully changed the signal handler for Signal %d\n", SIGUSR2);
+                    fflush(NULL);
+            } else {
+                    printf("Fail to correctly change the signal handler for Signal %d\n", SIGUSR2);
+                    fflush(NULL);
+            }
+            while(1);
     }
-    
-    /* Thread 1 */
-    pthread_t thread1;
-    if (pthread_create(&thread1, NULL, th, &user1) != 0){
-        perror("Thread1 failed");
+
+    else {
+            // Cage 1
+            sleep(2);
+            printf("Killing Cage 2 using PID: %ld\n", (long)pid);
+            fflush(stdout);
+            kill(pid, SIGUSR2);
+            sleep(2);
     }
-
-    /* Thread 2 */
-    pthread_t thread2;
-    if (pthread_create(&thread2, NULL, th, &user2) != 0){
-        perror("Thread2 failed");
-    }
-    sleep(1);
-
-    pthread_kill(thread1, SIGUSR1);
-
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-    printf("End\n");
     return 0;
 }
