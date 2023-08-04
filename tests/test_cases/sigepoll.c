729ba5deccd4c7d7ae61c602b87506e278ccf744
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-03 21:40:20 -0400

    add sigepoll test

---
+++ tests/test_cases/sigepoll.c
@@ -0,0 +1,80 @@
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h> 
+#include <poll.h>
+#include <sys/epoll.h>
+
+#define MAX_EVENTS 5
+
+void handler(int signum)
+{
+    printf("Signal Handler Test!\n");
+    printf("Exiting Cage 2\n");
+    fflush(stdout);
+}
+
+int main(void)
+{
+    struct sigaction new_action;
+    struct sigaction old_action;
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // Cage 2
+        printf("Cage 2 pid: %ld\n", (long)getpid());
+        printf("Handler function ptr %p\n", handler);
+        fflush(stdout);
+
+        new_action.sa_handler = handler;
+
+        sigaction(SIGUSR2, &new_action, NULL);
+        sigaction(SIGUSR2, NULL, &old_action);
+
+        if (new_action.sa_handler == old_action.sa_handler) {
+                printf("Successfully changed the signal handler for Signal %d\n", SIGUSR2);
+                fflush(NULL);
+        } else {
+                printf("Fail to correctly change the signal handler for Signal %d\n", SIGUSR2);
+                fflush(NULL);
+        }
+
+        int epoll_fd, num_events, i;
+        struct epoll_event events[MAX_EVENTS];
+
+        epoll_fd = epoll_create(0);
+        if (epoll_fd < 0) {
+            perror("epoll_create");
+        }
+
+        struct epoll_event event;
+        event.events = EPOLLIN;
+        event.data.fd = STDIN_FILENO;
+
+        if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, STDIN_FILENO, &event) < 0) {
+            perror("epoll_ctl");
+        }
+
+        while (1) {
+            num_events = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+            if (num_events < 0) {
+                perror("epoll_wait");
+            }
+        }
+        
+    }
+
+    else {
+            // Cage 1
+            sleep(2);
+            printf("Killing Cage 2 using PID: %ld\n", (long)pid);
+            fflush(stdout);
+            kill(pid, SIGUSR2);
+            sleep(2);
+    }
+    return 0;
+}
