5dc0ef02b668f90a41702aa34013f4719beeff57
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-15 15:18:40 -0400

    EINTR pread

---
+++ tests/test_cases/sigpread.c
@@ -0,0 +1,59 @@
+#undef _GNU_SOURCE
+#define _GNU_SOURCE
+
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+static void sig_usr(int signum){
+    printf("Received signal %d\n", signum);
+}
+
+int main() {
+    printf("Start pread()\n");
+
+    char buf[512];
+    off_t offset = 0;
+
+    // Set signal handler
+    struct sigaction sa_usr;
+    sa_usr.sa_flags = 0;
+    sa_usr.sa_handler = sig_usr;   
+
+    // Create a test file
+    int fd = open("testfile.txt", O_CREAT|O_RDWR, 0666);
+    if (fd < 0) {
+        printf("Error open file\n");
+        fflush(NULL);
+    }
+
+    // Write to the file
+    const char *data = "TEST";
+    ssize_t wret = write(fd, data, strlen(data));
+    if (wret < 0) {
+        printf("Error write to file\n");
+        fflush(NULL);
+    }
+
+    // Register SIG handler
+    sigaction(SIGINT, &sa_usr, NULL);
+
+    // Blocking read
+    ssize_t prret = pread(fd, buf, sizeof(buf), offset);
+    if(prret < 0) {
+        if(errno == EINTR){
+            printf("Error code: %d\n", errno);
+            printf("EINTR error\n");
+            fflush(NULL);
+        }
+    }
+    close(fd);
+    return 0;
+}
