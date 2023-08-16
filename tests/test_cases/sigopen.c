642320ffd08f72ad868f761dfdfad0ba6e23a443
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-15 21:49:41 -0400

    lock with EINTR open

--- tests/test_cases/sigopen.c
+++ tests/test_cases/sigopen.c
@@ -21,18 +21,10 @@ int main() {
     int fd = open("testfile.txt", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
     if (fd == -1) {
         perror("open");
-        return 0;
+        return EXIT_FAILURE;
     }
 
-    // Initialize and create a named semaphore
-    sem_t *file_lock = sem_open("/file_lock", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1);
-    if (file_lock == SEM_FAILED) {
-        perror("sem_open");
-        return 0;
-    }
-
-
-
+    pid_t lock_pid = fork();
     // Fork a child process
     pid_t child_pid = fork();
     pid_t parent_pid = getppid();
@@ -40,53 +32,69 @@ int main() {
     if (child_pid == -1) {
         perror("fork");
         return 0;
-    } else if (child_pid == 0) {
-        // Child process
-        sem_wait(file_lock);  // Acquire the lock
-        printf("Child process acquired the lock.\n");
-        sleep(2);
-        kill(parent_pid, SIGUSR2);
-        // Simulate some work
+    } else if (lock_pid == 0) {     // lock() process
+        // Lock the file
+        struct flock lock;
+        lock.l_type = F_WRLCK;
+        lock.l_whence = SEEK_SET;
+        lock.l_start = 0;
+        lock.l_len = 0; // Lock the whole file
+
+        int result = fcntl(fd, F_SETLK, &lock);
+        if(result < 0) {
+            perror("fcntl");
+            return EXIT_FAILURE;
+        }
+        printf("lock() process acquired the lock.\n");
+        
         sleep(5);
 
-        sem_post(file_lock);  // Release the lock
+        // Release the lock
         printf("Child process released the lock.\n");
 
-        // Close and unlink the semaphore
-        sem_close(file_lock);
-        sem_unlink("/file_lock");
+        lock.l_type = F_UNLCK;
+        fcntl(fd, F_SETLK, &lock);
         
-        return EXIT_FAILURE;
-    } else {
-        // Parent process
-        printf("open() process trying to open the file...\n");
+        exit(0);
+    } else {    // Parent process
+        // Fork open() process
+        pid_t open_pid = fork();
+        if(open_pid < 0){
+            perror("fork");
+            return EXIT_FAILURE;
+        } else if(open_pid == 0){ // open() process
+            printf("open() process waiting for the lock...\n");
+            // Set signal handler
+            struct sigaction sa_usr;
+            sa_usr.sa_flags = 0;
+            sa_usr.sa_handler = sig_usr;   
 
-        // Set signal handler
-        struct sigaction sa_usr;
-        sa_usr.sa_flags = 0;
-        sa_usr.sa_handler = sig_usr;   
+            sigaction(SIGUSR2, &sa_usr, NULL);
 
-        sigaction(SIGUSR2, &sa_usr, NULL);
-
-        // Try to open the file while the lock is held by the child
-        int parent_fd = open("testfile.txt", O_RDWR);
-        if (parent_fd < 0 && errno == EINTR) {
-            printf("Error code: %d\n", errno);
-            printf("EINTR error\n");
-            fflush(NULL);
+            // Try to open the file while the lock is held by the child
+            int open_fd = open("testfile.txt", O_RDWR);
+            if (open_fd < 0 && errno == EINTR) {
+                printf("Error code: %d\n", errno);
+                printf("EINTR error\n");
+                fflush(NULL);
+            } else {
+                printf("open() process successfully opened the file.\n");
+                close(open_fd);
+            }
+            exit(0);
         } else {
-            printf("open() process successfully opened the file.\n");
-            close(parent_fd);
+            sleep(2);
+
+            printf("Sending SIGUSR2 signal to open() process...\n");
+            kill(open_pid, SIGUSR2);
+            // Wait for child processes to finish
+            waitpid(lock_pid, NULL, 0);
+            waitpid(open_pid, NULL, 0);
+
+            // Close and unlink the semaphore
+            close(fd);
         }
-
-        // Wait for the child process to finish
-        wait(NULL);
         
-        // Close and unlink the semaphore
-        sem_close(file_lock);
-        sem_unlink("/file_lock");
-
-        close(fd);
     }
 
     return 0;
