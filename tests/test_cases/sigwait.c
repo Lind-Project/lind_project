207bc50cbe86c21af068180d1d3b1754b05b5b73
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-16 14:34:14 -0400

    EINTR wait

---
+++ tests/test_cases/sigwait.c
@@ -0,0 +1,41 @@
+#undef _GNU_SOURCE
+#define _GNU_SOURCE
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
+
+
+static void sig_usr(int signum){
+   printf("Received signal %d\n", signum);
+}
+
+int main() {
+    pid_t child1_pid = fork();
+
+    if(child1_pid < 0) {
+        perror("fork");
+        return EXIT_FAILURE;
+    } else if(child1_pid == 0) {    // Child 1 process
+        printf("Child1 process start...\n");
+        kill(getppid(), SIGUSR2);
+        for(;;); // Didn't return, so wait() blocked
+    } else {   
+        // Set signal handler
+        struct sigaction sa_usr;
+        sa_usr.sa_flags = 0;
+        sa_usr.sa_handler = sig_usr;  
+
+        sigaction(SIGUSR2, &sa_usr, NULL);
+        wait(NULL);
+    }
+    return 0;
+}
