fa4d375835ade98e4bb03b2cbe68cf6cd5c0265d
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-16 21:04:51 -0400

    EINTR nanosleep

---
+++ tests/test_cases/signanosleep.c
@@ -0,0 +1,49 @@
+#undef _GNU_SOURCE
+#define _GNU_SOURCE
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+
+static void sig_usr(int signum){
+   printf("Received signal %d\n", signum);
+}
+
+int main() {
+    pid_t child_pid = fork();
+    
+    if(child_pid < 0){
+        perror("fork");
+        return EXIT_FAILURE;
+    } else if(child_pid == 0) {
+
+        // Set signal handler
+        struct sigaction sa_usr;
+        sa_usr.sa_flags = 0;
+        sa_usr.sa_handler = sig_usr;  
+
+        sigaction(SIGUSR2, &sa_usr, NULL);
+        
+        struct timespec req;
+    
+        req.tv_sec = 0;        
+        req.tv_sec = 10; 
+        int ret = nanosleep(&req, 0);
+
+        if(ret < 0) {
+            perror("nanosleep");
+        }
+        exit(0);
+    } else {
+        sleep(3);
+        kill(SIGUSR2, child_pid);
+        wait(NULL);
+        return 0;
+    }
+}
