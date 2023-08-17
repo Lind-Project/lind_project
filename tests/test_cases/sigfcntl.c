0675fd4e6738b2b4c1cc5e6f597a0c7e59402eb2
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-16 23:51:59 -0400

    EINTR pread

--- tests/test_cases/sigfcntl.c
+++ tests/test_cases/sigfcntl.c
@@ -1,7 +1,6 @@
 #undef _GNU_SOURCE
 #define _GNU_SOURCE
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,57 +12,53 @@
 #include <signal.h>
 #include <errno.h>
 
-
 static void sig_usr(int signum){
-   printf("Received signal %d\n", signum);
+    printf("Received signal %d\n", signum);
 }
 
-
 int main() {
-   pid_t lock_pid = fork();
-       // Create or open the testfile.txt
-       int fd = open("testfile.txt", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-       if (fd == -1) {
-           perror("open");
-           return EXIT_FAILURE;
-       }
-   if (lock_pid == -1) {
-       perror("fork");
-       return 0;
-   } else if (lock_pid == 0) {     // lock() process
-       // Lock the file
-       struct flock lock;
-       lock.l_type = F_RDLCK;
-       lock.l_whence = SEEK_SET;
-       lock.l_start = 0;
-       lock.l_len = 0; // Lock the whole file
+    pid_t lock_pid = fork();
+        // Create or open the testfile.txt
+        int fd = open("testfile.txt", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+        if (fd == -1) {
+            perror("open");
+            return EXIT_FAILURE;
+        }
+    if (lock_pid == -1) {
+        perror("fork");
+        return 0;
+    } else if (lock_pid == 0) {     // lock() process
+        // Lock the file
+        struct flock lock;
+        lock.l_type = F_RDLCK;
+        lock.l_whence = SEEK_SET;
+        lock.l_start = 0;
+        lock.l_len = 0; // Lock the whole file
 
-       int result = fcntl(fd, F_SETLK, &lock);
-       if(result < 0) {
+        int result = fcntl(fd, F_SETLK, &lock);
+        if(result < 0) {
            perror("fcntl");
            printf("Error code: %d\n", errno);
            // printf("EINTR error\n");
            fflush(NULL);
-       }
-       printf("lock() process acquired the lock.\n");
-      
-       sleep(10);
+        }
+        printf("lock() process acquired the lock.\n");
+        
+        sleep(10);
 
+        // Release the lock
+        printf("lock() process released the lock.\n");
 
-       // Release the lock
-       printf("lock() process released the lock.\n");
-
-
-       lock.l_type = F_UNLCK;
-       fcntl(fd, F_SETLK, &lock);
-      
-       exit(0);
-   } else {    // Parent process
-       pid_t child_pid = fork();
-       if(child_pid < 0){
+        lock.l_type = F_UNLCK;
+        fcntl(fd, F_SETLK, &lock);
+        
+        exit(0);
+    } else {    // Parent process
+        pid_t child_pid = fork();
+        if(child_pid < 0){
             perror("fork");
             return EXIT_FAILURE;
-       } else if(child_pid == 0){ // Child process
+        } else if(child_pid == 0){ // Child process
             sleep(5);
             printf("Child process waiting for the lock...\n");
             // Set signal handler
@@ -87,17 +82,17 @@ int main() {
                 // printf("EINTR error\n");
                 fflush(NULL);
             }
-       } else {
-           sleep(10);
-           printf("Sending SIGUSR2 signal to child process...\n");
-           kill(child_pid, SIGUSR2);
-           // Wait for child processes to finish
-           waitpid(lock_pid, NULL, 0);
-           waitpid(child_pid, NULL, 0);
-           close(fd);
-          
-       }
-      
+        } else {
+            sleep(10);
+            printf("Sending SIGUSR2 signal to child process...\n");
+            kill(child_pid, SIGUSR2);
+            // Wait for child processes to finish
+            waitpid(lock_pid, NULL, 0);
+            waitpid(child_pid, NULL, 0);
+            close(fd);
+            
+        }
+        
    }
 
 
--- tests/test_cases/sigpread.c
+++ tests/test_cases/sigpread.c
@@ -1,52 +1,99 @@
 #undef _GNU_SOURCE
 #define _GNU_SOURCE
 
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include <signal.h>
+#include <errno.h>
+
 
 static void sig_usr(int signum){
-    printf("Received signal %d\n", signum);
+   printf("Received signal %d\n", signum);
 }
 
+
 int main() {
-
-    pid_t pid = fork();
-
-    if (pid == 0) {
-        char buf[512];
-        // Set signal handler
-        struct sigaction sa_usr;
-        sa_usr.sa_flags = 0;
-        sa_usr.sa_handler = sig_usr;   
-
-        sigaction(SIGUSR2, &sa_usr, NULL);
-
-        // Blocking read
-        int ret = pread(STDIN_FILENO, buf, 511, 0);
-        printf("pread: %d\n", ret);
-        perror("pread");
-        if(ret < 0) {
-            if(errno == EINTR){
-                printf("Error code: %d\n", errno);
-                printf("EINTR error\n");
-                fflush(NULL);
-            }
-        }
-        
-    } else {
-        // Cage 1
-        sleep(2);
-        printf("Killing Cage 2 using PID: %ld\n", (long)pid);
-        fflush(stdout);
-        kill(pid, SIGUSR2);
-        sleep(2);
+    pid_t lock_pid = fork();
+    // Create or open the testfile.txt
+    int fd = open("testfile.txt", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        perror("open");
+        return EXIT_FAILURE;
     }
+    if (lock_pid == -1) {
+        perror("fork");
+        return 0;
+    } else if (lock_pid == 0) {     // lock() process
+        // Lock the file
+        struct flock lock;
+        lock.l_type = F_RDLCK;
+        lock.l_whence = SEEK_SET;
+        lock.l_start = 0;
+        lock.l_len = 0; // Lock the whole file
+
+        int result = fcntl(fd, F_SETLK, &lock);
+        if(result < 0) {
+            perror("fcntl");
+            printf("Error code: %d\n", errno);
+            // printf("EINTR error\n");
+            fflush(NULL);
+        }
+
+        printf("lock() process acquired the lock.\n");
+        printf("lock() will waiting for 10s\n");
+
+        sleep(10);
+        
+        // Release the lock
+        printf("lock() process released the lock.\n");
+
+        lock.l_type = F_UNLCK;
+        fcntl(fd, F_SETLK, &lock);
+        
+        exit(0);
+    } else {    
+        pid_t child_pid = fork();
+        if(child_pid < 0){
+            perror("fork");
+            return EXIT_FAILURE;
+        } else if(child_pid == 0){ // Child process
+            sleep(5);
+            printf("[pread] process start\n");
+            // Set signal handler
+            struct sigaction sa_usr;
+            sa_usr.sa_flags = 0;
+            sa_usr.sa_handler = sig_usr;  
+
+            sigaction(SIGUSR2, &sa_usr, NULL);
+
+            char buf[512];
+            int ret = pread(fd, buf, 511, 0);
+            if(ret < 0) {
+                if(errno == EINTR){
+                    printf("Error code: %d\n", errno);
+                    printf("EINTR error\n");
+                    fflush(NULL);
+                    exit(0);
+                }
+            }
+        } else {
+            sleep(10);
+            printf("Sending SIGUSR2 signal to child process...\n");
+            kill(child_pid, SIGUSR2);
+            // Wait for child processes to finish
+            wait(NULL);
+            close(fd);
+            
+        }
+      
+    }
+
     return 0;
 }
