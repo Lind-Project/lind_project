1e724ac746625a245c3b4d15bf01f20a97ac70c6
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-16 23:19:15 -0400

    EINTR pread

--- tests/test_cases/sigpread.c
+++ tests/test_cases/sigpread.c
@@ -10,97 +10,41 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
-#include <string.h>
 
 static void sig_usr(int signum){
     printf("Received signal %d\n", signum);
-    fflush(NULL);
 }
 
 int main() {
-    // Set signal handler
-    struct sigaction sa_usr;
-    sa_usr.sa_flags = 0;
-    sa_usr.sa_handler = sig_usr;      
-    // Register SIG handler
-    sigaction(SIGUSR1, &sa_usr, NULL);
-    // Open a file
-    int fd = open("testfile.txt", O_RDONLY);
 
-    char buffer[1024];
-    ssize_t nread;
+    pid_t pid = fork();
 
-    // Generate a SIGUSR1 signal to simulate interruption
-    printf("Generating signal...\n");
-    kill(getpid(), SIGUSR1);
+    if (pid == 0) {
+        char buf[512];
+        // Set signal handler
+        struct sigaction sa_usr;
+        sa_usr.sa_flags = 0;
+        sa_usr.sa_handler = sig_usr;   
 
-    // Try reading using pread
-    nread = pread(fd, buffer, sizeof(buffer), 0);
-    printf("check point\n");
-    printf("nread: %zu\n", nread);
-    fflush(NULL);
-    if (nread == -1) {
-        if (errno == EINTR) {
-            printf("pread was interrupted by signal\n");
-            fflush(NULL);
-        } 
-    } 
-    close(fd);
-    // pid_t pid = fork();
+        sigaction(SIGUSR2, &sa_usr, NULL);
 
-    // if (pid ==0) {
-    //     // Cage 2
-    //     printf("Start pread()\n");
-    //     char buf[512];  
-    //     off_t offset = 0;
-
-    //     // Set signal handler
-    //     struct sigaction sa_usr;
-    //     sa_usr.sa_flags = 0;
-    //     sa_usr.sa_handler = sig_usr;   
-
-    //     // Create a test file
-    //     int fd = open("testfile.txt", O_CREAT|O_RDWR, 0666);
-    //     if (fd < 0) {
-    //         printf("Error open file\n");
-    //         fflush(NULL);
-    //     }
-
-    //     // // Write to the file
-    //     // const char *data = "TEST";
-    //     // ssize_t wret = write(fd, data, strlen(data));
-    //     // if (wret < 0) {
-    //     //     printf("Error write to file\n");
-    //     //     fflush(NULL);
-    //     // }
-
-    //     // Register SIG handler
-    //     sigaction(SIGUSR2, &sa_usr, NULL);
-    //     ssize_t prret;
-    //     do {
-    //         prret = pread(fd, buf, sizeof(buf), offset);
-    //         printf("Return Value: %zu\n", prret);
-    //         fflush(NULL);
-    //         if(prret < 0) {
-    //             if(errno == EINTR){
-    //                 printf("Error code: %d\n", errno);
-    //                 printf("EINTR error\n");
-    //                 fflush(NULL);
-    //             }
-    //         }
-    //     } while (prret == 0);
-    //     // Blocking read
-    //     printf("pread value: %zu\n", prret);
-    //     fflush(NULL);
-    //     close(fd);
-    // } else {
-    //     sleep(2);
-    //     printf("Killing Cage 2 using PID: %ld\n", (long)pid);
-    //     fflush(stdout);
-    //     kill(pid, SIGUSR2);
-    //     sleep(2);
-    // }
-
-    
+        // Blocking read
+        int ret = pread(STDIN_FILENO, buf, 511, 0);
+        if(ret < 0) {
+            if(errno == EINTR){
+                printf("Error code: %d\n", errno);
+                printf("EINTR error\n");
+                fflush(NULL);
+            }
+        }
+        
+    } else {
+        // Cage 1
+        sleep(2);
+        printf("Killing Cage 2 using PID: %ld\n", (long)pid);
+        fflush(stdout);
+        kill(pid, SIGUSR2);
+        sleep(2);
+    }
     return 0;
 }
