81383d24f89a3923cfd439bcc417add3633e7189
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-21 17:27:11 -0400

    update send test

--- tests/test_cases/send_test.c
+++ tests/test_cases/send_test.c
@@ -7,19 +7,32 @@
 #include <arpa/inet.h> 
 #include <pthread.h> 
 #include <errno.h>
-#define PORT 9995
+#include <signal.h>
+
+static void sig_usr(int signum){
+   printf("Received signal %d\n", signum);
+}
+
 int main() {
     int server_fd; 
     char *hello = "Hello from server"; 
        
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     printf("server fd: %d\n", server_fd);
-    int ret = send(server_fd, hello, strlen(hello), 0); 
-    printf("[send]");
-    fflush(NULL);
-    if(ret < 0) {
-        perror("send");
-        printf("[send]: %d\n", ret);
+
+    struct sigaction sa_usr;
+    sa_usr.sa_flags = 0;
+    sa_usr.sa_handler = sig_usr;  
+    sigaction(SIGINT, &sa_usr, NULL);
+
+    while (1) {
+        int ret = send(server_fd, hello, strlen(hello), 0); 
+        if(ret < 0) {
+            perror("send");
+            printf("[send]: %d\n", ret);
+            return 0;
+        }
     }
+    
     return 0;
 }
