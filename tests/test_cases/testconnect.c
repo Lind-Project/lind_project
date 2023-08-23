ed612f140c0cead1ca424b047026b97c089672ff
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-23 19:28:53 -0400

    test connect

--- tests/test_cases/testconnect.c
+++ tests/test_cases/testconnect.c
@@ -14,7 +14,8 @@
 
 int main() {
     int sockfd;
-    struct sockaddr_in address;
+    // struct sockaddr_in address;
+    struct sockaddr_un address;
 
     /* TEST: not socket */
     // sockfd = open("/home/lind/lind_project/tests/test_cases/write.c", O_RDONLY, 0777);
@@ -28,10 +29,33 @@ int main() {
     // sockfd = -1;
     
     /* TEST: no one listening */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    address.sin_family = AF_INET; 
-    address.sin_port = htons(9995); 
-    inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
+    // sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // address.sin_family = AF_INET; 
+    // address.sin_port = htons(9995); 
+    // inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
+
+    /* TEST: EINPROGRESS*/
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
+        return 1;
+    }
+
+    int flags = fcntl(sockfd, F_GETFL, 0);
+    if (flags == -1) {
+        perror("fcntl F_GETFL");
+        close(sockfd);
+        return 1;
+    }
+    if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        perror("fcntl F_SETFL");
+        close(sockfd);
+        return 1;
+    }
+
+    memset(&address, 0, sizeof(struct sockaddr_un));
+    address.sun_family = AF_UNIX;
+    strncpy(address.sun_path, "/tmp/my_unix_socket", sizeof(address.sun_path) - 1);
        
     int ret = connect(sockfd, (struct sockaddr *)&address, sizeof(address));
     if(ret < 0) {
