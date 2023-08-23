4f6d35909fee07ab2207882399706c03f3a3883a
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-23 18:06:09 -0400

    test bind

--- tests/test_cases/testbind.c
+++ tests/test_cases/testbind.c
@@ -5,6 +5,8 @@
 #include <errno.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <sys/un.h>
+#include <sys/socket.h>
 
 int main() {
     int sockfd;
@@ -26,11 +28,10 @@ int main() {
     /* TEST: invalid path */
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
 
-    struct sockaddr_in address;
-    address.sin_family = AF_UNIX;
-    address.sin_port = htons(12345);
+    struct sockaddr_un address;
+    address.sun_family = AF_UNIX;
     const char *invalidPath = "/path/to/invalid/directory";
-    address.sin_addr.s_addr = inet_addr(invalidPath);
+    strncpy(address.sun_path, invalidPath, sizeof(address.sun_path)-1);
 
     if(bind(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0 ) { 
         perror("bind"); 
