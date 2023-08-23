58488cc594f7f0643248a1aff9aa56cd6bafa90b
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-23 19:17:36 -0400

    test connect

--- tests/test_cases/testconnect.c
+++ tests/test_cases/testconnect.c
@@ -14,6 +14,7 @@
 
 int main() {
     int sockfd;
+    struct sockaddr_in address;
 
     /* TEST: not socket */
     // sockfd = open("/home/lind/lind_project/tests/test_cases/write.c", O_RDONLY, 0777);
@@ -21,12 +22,16 @@ int main() {
     /* TEST: passed address didnt have the correct addr family */
     // sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // struct sockaddr_in address;
     // address.sin_family = AF_UNIX;
 
     /* TEST: invalid file descriptor */
-    sockfd = -1;
-    struct sockaddr_in address;
+    // sockfd = -1;
+    
+    /* TEST: no one listening */
+    address.sin_family = AF_INET; 
+    address.sin_port = htons(9995); 
+    inet_pton(AF_INET, "127.0.0.1", &address.sin_addr);
+       
     int ret = connect(sockfd, (struct sockaddr *)&address, sizeof(address));
     if(ret < 0) {
         perror("connect"); 
