11d575abf216d211984b5e261ade8161f7a16617
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-23 17:39:47 -0400

    test bind

--- tests/test_cases/testbind.c
+++ tests/test_cases/testbind.c
@@ -8,24 +8,29 @@
 
 int main() {
     int sockfd;
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port = htons(12345);
-
-    
 
     /* TEST: non-exitstent address */
     // sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // struct sockaddr_in address;
+    // address.sin_family = AF_INET;
+    // address.sin_port = htons(12345);
     // address.sin_addr.s_addr = inet_addr("192.168.1.100");
 
     /* TEST: null address */ 
-    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-    address.sin_addr.s_addr = inet_addr("");
+    // sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // struct sockaddr_in address;
+    // address.sin_family = AF_INET;
+    // address.sin_port = htons(12345);
+    // address.sin_addr.s_addr = inet_addr("");
 
     /* TEST: invalid path */
-    // sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-    // const char *invalidPath = "/path/to/invalid/directory";
-    // address.sin_addr.s_addr = inet_addr(invalidPath);
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    struct sockaddr_in address;
+    address.sin_family = AF_UNIX;
+    address.sin_port = htons(12345);
+    const char *invalidPath = "/path/to/invalid/directory";
+    address.sin_addr.s_addr = inet_addr(invalidPath);
 
     if(bind(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0 ) { 
         perror("bind"); 
