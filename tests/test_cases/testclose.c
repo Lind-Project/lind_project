4354e3055a650df749a38feb85e42d39fde6b2a5
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-25 21:06:42 -0400

    test close

---
+++ tests/test_cases/testclose.c
@@ -0,0 +1,28 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    int ret;
+    int fd;
+    fd = open("/dev/urandom", O_RDONLY, S_IRWXU|S_IRWXG|S_IRWXO);
+    close(fd);
+
+    ret = close(fd);
+
+    if(ret < 0) {
+        printf("ERROR: %d\n", errno);
+        fflush(NULL);
+        perror("close");
+        printf("Return: %d\n", ret);
+        fflush(NULL);
+        exit(1);
+    }
+    
+    printf("Succeed\n");
+    return 0;
+}
