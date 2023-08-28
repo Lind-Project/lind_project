0fbfb96d94b5f4497c2a83f43919eba016062f6c
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-28 11:41:02 -0400

    test lstat

---
+++ tests/test_cases/testlstat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    const char new_name[] = "testfiles/statfile.txt";
+    struct stat st;
+    int ret;
+
+    ret = lstat(new_name, &st);
+    if(ret < 0) {
+        printf("ERROR: %d\n", errno);
+        perror("fstat");
+        fflush(NULL);
+        printf("Return: %d\n", ret);
+        exit(1);
+    }
+
+    printf("Return: %d\n", ret);
+    return 0;
+}
