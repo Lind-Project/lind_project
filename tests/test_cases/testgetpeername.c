8a0cd2e150853d72f940a94ab51bbb15faf16cae
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-29 17:31:13 -0400

    test getpeername

---
+++ tests/test_cases/testgetpeername.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <errno.h>
+
+int main() {
+    int fd;
+    struct sockaddr_in server_addr;
+    socklen_t len = sizeof(server_addr);
+    int ret = getpeername(fd, (struct sockaddr*)&server_addr, &len);
+    if(ret < 0) {
+        printf("ERROR: %d\n", errno);
+        perror("getpeername");
+        fflush(NULL);
+        printf("Return: %d\n", ret);
+        exit(1);
+    }
+    return 0;
+}
