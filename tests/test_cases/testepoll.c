0bc2aa20b1fef0c45eb46cb1e46ffb30151ecf79
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-29 16:58:00 -0400

    test epoll

--- tests/test_cases/testepoll.c
+++ tests/test_cases/testepoll.c
@@ -3,53 +3,63 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/epoll.h>
+#include <errno.h>
 
 #define MAX_EVENTS 10
 
 int main() {
     int epoll_fd, nfds;
     struct epoll_event ev, events[MAX_EVENTS];
+    int ret_create, ret_ctl, ret_wait;
 
-    epoll_fd = epoll_create(0);
+    ret_create = epoll_create(0);
+    if(ret_create < 0) {
+        printf("ERROR: %d\n", errno);
+        perror("epoll_create");
+        fflush(NULL);
+        printf("Return: %d\n", ret_create);
+    }
+
+    epoll_fd = epoll_create(MAX_EVENTS); 
     if (epoll_fd == -1) {
-        perror("epoll_create1");
+        perror("epoll_create");
+        return 1;
+    }
+    printf("[epoll_create] return fd: %d\n", epoll_fd);
+    fflush(NULL);
+
+    int fds[2]; // File descriptors for pipe
+    if (pipe(fds) == -1) {
+        perror("pipe");
         return 1;
     }
 
-    // Add stdin (file descriptor 0) to the epoll set
+    // Add read end of the pipe to the epoll set
     ev.events = EPOLLIN;
-    ev.data.fd = 0;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &ev) == -1) {
-        perror("epoll_ctl: stdin");
+    ev.data.fd = fds[0];
+    int rv = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fds[0], &ev);
+    if (rv < 0) {
+        perror("epoll_ctl: pipe read end");
         return 1;
     }
+    printf("[epoll_ctl] return: %d\n", epoll_fd);
+    fflush(NULL);
 
-    while (1) {
-        nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1); // Wait for events
-        if (nfds == -1) {
-            perror("epoll_wait");
-            return 1;
-        }
+    ret_ctl = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fds[0], &ev);
+    if(ret_ctl < 0) {
+        printf("ERROR: %d\n", errno);
+        perror("epoll_ctl");
+        fflush(NULL);
+        printf("Return: %d\n", ret_create);
+    }
 
-        for (int i = 0; i < nfds; i++) {
-            if (events[i].data.fd == 0) {
-                printf("Input is available on stdin.\n");
-
-                // Read and print the input
-                char buffer[1024];
-                ssize_t bytesRead = read(0, buffer, sizeof(buffer));
-                if (bytesRead > 0) {
-                    buffer[bytesRead] = '\0';
-                    printf("Read %zd bytes from stdin: %s\n", bytesRead, buffer);
-                } else if (bytesRead == 0) {
-                    printf("EOF detected on stdin.\n");
-                    close(epoll_fd);
-                    return 0;
-                } else {
-                    perror("read");
-                }
-            }
-        }
+    int wait_fd;
+    ret_wait = epoll_wait(wait_fd, events, MAX_EVENTS, -1);
+    if(ret_wait < 0) {
+        printf("ERROR: %d\n", errno);
+        perror("epoll_wait");
+        fflush(NULL);
+        printf("Return: %d\n", ret_create);
     }
 
     close(epoll_fd);
