d5e06979797e0868601f73a91764e065a536f87e
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-29 20:59:29 -0400

    test socketpair

---
+++ tests/test_cases/testsocketpair.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main() {
+    int sockets[2], child;
+    char buf[1024];
+
+    int ret = socketpair(AF_UNIX, SOCK_STREAM, 0, sockets);
+    if(ret < 0) {
+        printf("ERROR: %d\n", errno);
+        perror("socketpair");
+        fflush(NULL);
+        printf("Return: %d\n", ret);
+        exit(1);
+    }
+
+    return 0;
+}
