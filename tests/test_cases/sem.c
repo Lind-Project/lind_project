7beff608dd340d7b37c6ef1a26b1b8b81d26d8da
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-08-31 19:04:13 -0400

    sem test

--- tests/test_cases/sem.c
+++ tests/test_cases/sem.c
@@ -2,38 +2,24 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 #include <semaphore.h>
 
 sem_t sem;
 
-void* func1() {
-    // Lock
-    sem_wait(&sem);
-    printf("[Thread 1] running... \n");
-}
-
-void* func2(void* arg) {
-    printf("[Thread 2] running... \n");
-    // Unlock
-    sem_post(&sem);
-}
-
 int main() {
     // Initial sem with 0
     sem_init(&sem, 0, 0);
-    pthread_t thread1, thread2;
     
-    pthread_create(&thread1, NULL, func1, NULL);
-    printf("[main] created thread 1...\n");
+    int count = 9;
 
-    sleep(5);
+    sem_wait(&sem);
+    printf("shouldn't be executed... %d\n", count);
+    sem_post(&sem);
 
-    pthread_create(&thread2, NULL, func2, NULL);
-    printf("[main] created thread 2...\n");
-
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    sem_wait(&sem);
+    printf("should display: %d\n", count);
+    sem_post(&sem);
+    
     sem_destroy(&sem);
 
     return 0;
