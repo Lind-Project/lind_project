82e3079c8ddb84354861f4f5507c4d5126e8deaa
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-09-05 13:18:29 -0400

    shared memo test

--- tests/test_cases/shm.c
+++ tests/test_cases/shm.c
@@ -2,69 +2,41 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
-#include <wait.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <semaphore.h>
 
 #define SHM_SIZE 1024
 
-// Structure for a semaphore
-union semun {
-    int val;
-    struct semid_ds *buf;
-    unsigned short *array;
-};
-
-// Function to perform the wait operation on a semaphore
-void sem_wait(int sem_id) {
-    struct sembuf sem_buf;
-    sem_buf.sem_num = 0;
-    sem_buf.sem_op = -1;
-    sem_buf.sem_flg = SEM_UNDO;
-    semop(sem_id, &sem_buf, 1);
-}
-
-// Function to perform the signal operation on a semaphore
-void sem_signal(int sem_id) {
-    struct sembuf sem_buf;
-    sem_buf.sem_num = 0;
-    sem_buf.sem_op = 1;
-    sem_buf.sem_flg = SEM_UNDO;
-    semop(sem_id, &sem_buf, 1);
-}
-
 int main() {
-    int shmid, semid;
-    key_t key = ftok("shm_semaphore_example", 'A');
+    sem_t *sem;
+    int shm_fd;
 
-    // Create shared memory segment
-    if ((shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666)) == -1) {
-        perror("shmget");
+    // Create a shared memory region
+    shm_fd = shm_open("/myshm", O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open");
         exit(1);
     }
 
-    // Attach shared memory
-    char *shm_ptr = shmat(shmid, NULL, 0);
-    if (shm_ptr == (char *)-1) {
-        perror("shmat");
+    // Set the size of the shared memory region
+    if (ftruncate(shm_fd, sizeof(sem_t)) == -1) {
+        perror("ftruncate");
         exit(1);
     }
 
-    // Create a semaphore
-    if ((semid = semget(key, 1, IPC_CREAT | 0666)) == -1) {
-        perror("semget");
+    // Map the shared memory region
+    sem = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (sem == MAP_FAILED) {
+        perror("mmap");
         exit(1);
     }
 
-    // Initialize the semaphore to 1 (unlocked)
-    union semun sem_union;
-    sem_union.val = 1;
-    semctl(semid, 0, SETVAL, sem_union);
-
-    // Shared counter
-    int *counter = (int *)shm_ptr;
-    *counter = 0;
+    // Initialize the semaphore
+    if (sem_init(sem, 1, 1) == -1) {
+        perror("sem_init");
+        exit(1);
+    }
 
     pid_t pid = fork();
 
@@ -73,30 +45,30 @@ int main() {
         exit(1);
     } else if (pid == 0) {
         // Child process
-        for (int i = 0; i < 5; i++) {
-            sem_wait(semid);
-            (*counter)++;
-            printf("Child process: Counter = %d\n", *counter);
-            sem_signal(semid);
-            sleep(1);
-        }
+        sem_wait(sem); // Child waits for the semaphore
+        printf("Child process: Acquired semaphore\n");
+        sleep(2); // Simulate some work
+        sem_post(sem); // Release the semaphore
+        printf("Child process: Released semaphore\n");
         exit(0);
     } else {
         // Parent process
-        for (int i = 0; i < 5; i++) {
-            sem_wait(semid);
-            (*counter)++;
-            printf("Parent process: Counter = %d\n", *counter);
-            sem_signal(semid);
-            sleep(1);
-        }
+        sleep(1); // Ensure the child process starts first
+        sem_wait(sem); // Parent waits for the semaphore
+        printf("Parent process: Acquired semaphore\n");
+        sleep(2); // Simulate some work
+        sem_post(sem); // Release the semaphore
+        printf("Parent process: Released semaphore\n");
+
+        // Wait for the child process to finish
+        wait(NULL);
     }
 
     // Cleanup
     wait(NULL);
-    shmdt(shm_ptr);
-    shmctl(shmid, IPC_RMID, NULL);
-    semctl(semid, 0, IPC_RMID);
+    sem_destroy(sem);
+    munmap(sem, sizeof(sem_t));
+    shm_unlink("/myshm");
 
     return 0;
 }
