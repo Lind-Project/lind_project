fa9e7e4aaf244c2f11f74c0a28b52df7605ee3fd
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-09-06 12:36:04 -0400

    shared memo test

--- tests/test_cases/shmwrite1.c
+++ tests/test_cases/shmwrite1.c
@@ -10,29 +10,29 @@
 
 int main() {
     key_t key = 31337;
-    printf("[1] Start\n");
-    fflush(NULL); 
-    sem_unlink("/semaphore_x");
+    printf("[1] start...\n");
+    fflush(stdout); 
     // shmget returns an identifier in shmid
     int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
   
     // shmat to attach to shared memory
-    char *shm = (char *)shmat(shmid, NULL, 0);
+    sem_t *sem_ptr = (sem_t *)shmat(shmid, NULL, 0);
     
-    // Initialize the semaphore
-    sem_t *sem_ptr = sem_open("/semaphore_x", O_CREAT | O_EXCL, S_IRWXU | S_IRWXG  | S_IRWXO, 1);
-    if (sem_ptr == NULL) {
-        perror("sem_open");
+    // Initialize the semaphore - let 2nd argu be nonzero for ipc
+    if (sem_init(sem_ptr, 1, 2) < 0) {
+        perror("sem_init");
         exit(1);
     }
     // Wait for the semaphore - LOCK
-    if (sem_wait(sem_ptr) < 0) {
+    int ret = sem_trywait(sem_ptr);
+    
+    if (ret < 0) {
         perror("sem_wait");
         exit(1);
+    } else {
+        printf("[1] LOCKED\n");
+        fflush(NULL);
     }
-
-    printf("[1] Locked\n");
-    fflush(NULL); 
 
     // UNLOCK
     if(sem_post(sem_ptr) < 0) {
@@ -40,10 +40,10 @@ int main() {
         exit(1);
     }
 
-    printf("[1] End\n");
-    fflush(NULL); 
     //detach from shared memory, we'll rmid in shmwrite2.c
-    shmdt(shm);
-  
+    shmdt(sem_ptr);
+    
+    printf("[1] End...\n");
+    fflush(NULL);
     return 0;
 }
