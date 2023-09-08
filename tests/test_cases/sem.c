e044b9980ec1afbd9e0086d339f16c2bcdb19e51
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-09-08 18:05:17 -0400

    sem test

--- tests/test_cases/sem.c
+++ tests/test_cases/sem.c
@@ -9,32 +9,35 @@
 
 int main() {
     sem_t sem;
+    int val;
+    sem_getvalue(&sem, &val);
+    printf("value: %d\n", val);
     // Initial sem with 0
-    int r = sem_init(&sem, 0, 0);
-    printf("sem_initial: %d\n", r);
+    // int r = sem_init(&sem, 0, 0);
+    // printf("sem_initial: %d\n", r);
     
-    struct timespec ts;
-    int ret;
+    // struct timespec ts;
+    // int ret;
     
-    // 1. sem_timedwait with open sem=1
-    // 2. make sem_timedwait work: maybe check the ts with clock_gettime?
+    // // 1. sem_timedwait with open sem=1
+    // // 2. make sem_timedwait work: maybe check the ts with clock_gettime?
 
 
-    ts.tv_sec = 3;
+    // ts.tv_sec = 3;
 
-    ret = sem_timedwait(&sem, &ts);
-    if(ret < 0) {
-        if (errno == ETIMEDOUT)
-            printf("sem_timedwait() timed out\n");
-        else
-            perror("sem_timedwait");
-    } else {
-        printf("sem_timedwait succeed with %d\n", ret);
-    }
+    // ret = sem_timedwait(&sem, &ts);
+    // if(ret < 0) {
+    //     if (errno == ETIMEDOUT)
+    //         printf("sem_timedwait() timed out\n");
+    //     else
+    //         perror("sem_timedwait");
+    // } else {
+    //     printf("sem_timedwait succeed with %d\n", ret);
+    // }
     
-    sem_post(&sem);
+    // sem_post(&sem);
 
-    sem_destroy(&sem);
+    // sem_destroy(&sem);
 
     return 0;
 }
