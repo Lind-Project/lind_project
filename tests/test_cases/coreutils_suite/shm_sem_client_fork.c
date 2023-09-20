d46880dea30e2b0194a7a74600805ecfaa8d7541
RusherRG <rushang101@gmail.com>
2023-09-20 20:55:34 +0000

    tests: add shared sempahores with multiple cages test
    Signed-off-by: RusherRG <rushang101@gmail.com>

---
+++ tests/test_cases/coreutils_suite/shm_sem_client_fork.c
@@ -0,0 +1,84 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <string.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main()
+{
+    key_t key = 31337;
+    printf("[client] start...\n");
+    fflush(stdout);
+    // shmget returns an identifier in shmid
+    int shmid = shmget(key, 1024, 0666);
+
+    // shmat to attach to shared memory
+    sem_t *sem_ptr = (sem_t *)shmat(shmid, NULL, 0);
+
+    key_t key2 = 13371;
+    // shmget returns an identifier in shmid
+    int shmid2 = shmget(key2, 1024, 0666);
+
+    // shmat to attach to shared memory
+    char *shared_buffer = (char *)shmat(shmid2, NULL, 0);
+
+    if (fork() == 0) {
+        // child process
+        int count = 0;
+        while (count++ < 5)
+        {
+            printf("[client2] wait\n");
+            fflush(stdout);
+            int status = sem_wait(sem_ptr);
+
+            if (status != 0)
+            {
+                perror("in sem_wait");
+            }
+            sleep(2);
+            strcat(shared_buffer, "c");
+            printf("[client2] %s\n", shared_buffer);
+            fflush(stdout);
+            sem_post(sem_ptr);
+            printf("[client2] sent post\n");
+            fflush(stdout);
+            sleep(1);
+        }
+    } else {
+        // parent process
+        int count = 0;
+        while (count++ < 5)
+        {
+            printf("[client] wait\n");
+            fflush(stdout);
+            int status = sem_wait(sem_ptr);
+
+            if (status != 0)
+            {
+                perror("in sem_wait");
+            }
+            sleep(2);
+            strcat(shared_buffer, "u");
+            printf("[client] %s\n", shared_buffer);
+            fflush(stdout);
+            sem_post(sem_ptr);
+            printf("[client] sent post\n");
+            fflush(stdout);
+            sleep(1);
+        }
+    }
+    // detach from shared memory
+    shmdt(sem_ptr);
+    // mark the shared memory for removal
+    shmctl(shmid, IPC_RMID, NULL);
+
+    sem_destroy(sem_ptr);
+
+    printf("[client] end...\n");
+    fflush(stdout);
+    return 0;
+}
