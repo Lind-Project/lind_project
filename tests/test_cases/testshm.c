e91849427b8413a176e959ac3bfcc16d46959004
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-09-21 22:17:14 -0400

    add shm err test

---
+++ tests/test_cases/testshm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHM_SIZE 1024
+
+int main() {
+    int shmid;
+    key_t key = 2000;
+    if ((shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666)) == -1) {
+        perror("shmget");
+        exit(1);
+    }
+    if ((shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666)) == -1) {
+        perror("shmget");
+        exit(1);
+    }
+    printf("Success\n");
+    fflush(NULL);
+    return 0;
+}
