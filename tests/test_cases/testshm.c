387acbebc2bbc9700dfe018b2a0808f354add556
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-09-21 22:50:06 -0400

    add shm err test

--- tests/test_cases/testshm.c
+++ tests/test_cases/testshm.c
@@ -11,18 +11,42 @@
 #include <errno.h>
 
 #define SHM_SIZE 1024
-
 int main() {
     int shmid;
     key_t key = 2000;
-    if ((shmid = shmget(key, SHM_SIZE)) == -1) {
+    if ((shmid = shmget(key, 0, IPC_CREAT | 0666)) == -1) {
         printf("ERROR: %d\n", errno);
         fflush(NULL);
         perror("shmget");
         exit(1);
     }
+    // char *str = (char*) shmat(shmid, NULL, 0);
+    // shmctl(shmid, IPC_RMID, NULL);
+    // shmdt(str);
     
     printf("Success\n");
     fflush(NULL);
     return 0;
 }
+
+/* errno=22 */
+// int main() {
+//     int shmid;
+//     key_t key = 2000;
+//     if ((shmid = shmget(key, SHM_SIZE, IPC_CREAT | 0666)) == -1) {
+//         printf("ERROR: %d\n", errno);
+//         fflush(NULL);
+//         perror("shmget");
+//         exit(1);
+//     }
+//     if ((shmid = shmget(key, 1200, IPC_CREAT | 0666)) == -1) {
+//         printf("ERROR: %d\n", errno);
+//         fflush(NULL);
+//         perror("shmget");
+//         exit(1);
+//     }
+    
+//     printf("Success\n");
+//     fflush(NULL);
+//     return 0;
+// }
