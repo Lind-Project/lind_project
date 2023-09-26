f8cee59788cd487fd6bd99ac8312624a340bd22d
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-09-26 13:55:21 -0400

    update shmget err test

--- tests/test_cases/testshmget.c
+++ tests/test_cases/testshmget.c
@@ -9,42 +9,42 @@
 #include <errno.h>
 
 #define SHM_SIZE 1024
-int main() {
-    int shmid;
-    key_t key;
-    int i = 0;
-    key = ftok("/home/lind/lind_project/tests/test_cases/testfiles/shmfile.txt", 'A' + i);
-    if(key == -1) {
-        perror("ftok");
-        exit(1);
-    }
-    if ((shmid = shmget(key, 0, 0666)) == -1) {
-        printf("ERROR: %d\n", errno);
-        printf("%d\n", i);
-        fflush(NULL);
-        perror("shmget");
-        exit(1);
-    }
-    
-    printf("Success\n");
-    fflush(NULL);
-    return 0;
-}
-
-/* errno = 2 */
 // int main() {
 //     int shmid;
-//     key_t key = 2000;
+//     key_t key;
+//     int i = 0;
+//     key = ftok("/home/lind/lind_project/tests/test_cases/testfiles/shmfile.txt", 'A' + i);
+//     if(key == -1) {
+//         perror("ftok");
+//         exit(1);
+//     }
 //     if ((shmid = shmget(key, 0, 0666)) == -1) {
 //         printf("ERROR: %d\n", errno);
+//         printf("%d\n", i);
 //         fflush(NULL);
 //         perror("shmget");
 //         exit(1);
 //     }
+    
 //     printf("Success\n");
 //     fflush(NULL);
 //     return 0;
 // }
+
+/* errno = 2 */
+int main() {
+    int shmid;
+    key_t key = 2000;
+    if ((shmid = shmget(key, 0, 0666)) == -1) {
+        printf("ERROR: %d\n", errno);
+        fflush(NULL);
+        perror("shmget");
+        exit(1);
+    }
+    printf("Success\n");
+    fflush(NULL);
+    return 0;
+}
 
 /* errno = 22 */
 // int main() {
