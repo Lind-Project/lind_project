78361161db747d451fdaf79edca61b301d332053
Rushang Gajjal <rushang101@gmail.com>
2023-09-29 21:05:58 -0400

    tests: update regex in forkandopen.py (#325)
    Signed-off-by: RusherRG <rushang101@gmail.com>

--- tests/test_cases/forkandopen.py
+++ tests/test_cases/forkandopen.py
@@ -7,9 +7,8 @@ import string
 
 # assert(int(fdlist[-4]) != -1) checks if the parent PID's return -1. This would be an error with parent processes, but not with child processes.
 def forkandopen(results):
-    pidlist = re.findall('[-\d]+', results)
-    # print pidlist
-    assert(int(pidlist[-4]) != -1)  # Checks if parent PID is not -1, which means failure.
+    pidlist = re.findall('Parent:.*fd [-\d]+', results)
+    assert(int(pidlist[0][-1]) != -1)  # Checks if parent PID is not -1, which means failure.
 
 lind_results = sys.argv[1]
 native_results = sys.argv[2]
