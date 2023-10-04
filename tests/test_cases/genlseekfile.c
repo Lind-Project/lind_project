30728f5b7aed76ebccb6f1bf450823a46a4dd42d
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-10-04 00:48:45 -0400

    update lseek general test

--- tests/test_cases/genlseekfile.c
+++ tests/test_cases/genlseekfile.c
@@ -14,7 +14,9 @@ int main() {
     const int buffer_size = 9000;
     char buffer[buffer_size];
     const char* base_string = "discussions of his work—which has inquired into the mechanisms of pain, manipulation, and lies—with a reminder";
+    buffer[0] = '\0';
 
+    printf("1");
 
     for (int i = 0; i < 80; ++i) {
         strcat(buffer, base_string);
