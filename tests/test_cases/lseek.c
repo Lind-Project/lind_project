8e8c5046975d38cdbe6d8a7f669429f8bba040fa
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-10-04 00:17:19 -0400

    update lseek general test

--- tests/test_cases/lseek.c
+++ tests/test_cases/lseek.c
@@ -1,82 +1,22 @@
-#undef _GNU_SOURCE
-#define _GNU_SOURCE
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
-
-const char* FILENAME = "testfiles/lseekfile.txt";
-
-int main() {
-    int fd = open(FILENAME, O_RDONLY);
-    if(fd < 0) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-
-    off_t file_size = lseek(fd, 0, SEEK_END);
-    if(file_size < 0) {
-        perror("lseek");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    printf("File size: %lld\n", file_size);
-    fflush(NULL);
-
-    if(lseek(fd, 0, SEEK_SET) < 0) {
-        perror("lseek");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    char buf[12];
-    if(read(fd, buf, 12) < 0) {
-        perror("read");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    
-    if(write(STDOUT_FILENO, buf, 12) < 0) {
-        perror("write");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    off_t file_cur = lseek(fd, 0, SEEK_CUR);
-    if(file_cur < 0) {
-        perror("lseek");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    if(file_cur != 12){
-        printf("offset after read should be 12\n");
-        perror("lseek");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    
-    off_t next_data = lseek(fd, 0, SEEK_DATA);
-    if(next_data < 0) {
-        perror("lseek");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    printf("\noffset should be 0, actually: %lld\n", next_data);
-    fflush(NULL);
-
-    off_t next_hole = lseek(fd, 0, SEEK_HOLE);
-    if(next_hole < 0) {
-        perror("lseek");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    printf("offset should be 661, actually: %lld\n", next_hole);
-    fflush(NULL);
-
-    close(fd);
-    return 0;
+ 
+int main()
+{
+        int file=0;
+        if((file=open("testfile.txt",O_RDONLY)) < -1)
+                return 1;
+ 
+        char buffer[2048];
+        if(read(file,buffer,19) != 19)  return 1;
+        printf("%s\n",buffer);
+ 
+        if(lseek(file,10,SEEK_SET) < 0) return 1;
+ 
+        if(read(file,buffer,19) != 19)  return 1;
+        printf("%s\n",buffer);
+ 
+        return 0;
 }
