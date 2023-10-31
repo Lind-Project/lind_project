e4538f17a784f5786bba91de7b3274878c718d8b
RusherRG <rushang101@gmail.com>
2023-10-31 01:26:49 +0000

    tests: lamp stack remove create.html
    Signed-off-by: RusherRG <rushang101@gmail.com>

--- tests/lamp_stack/profiling/flask-app/load.sh
+++ tests/lamp_stack/profiling/flask-app/load.sh
@@ -4,6 +4,5 @@ lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/wsgi.py /
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/app.py /app.py
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/init_table.py /init_table.py
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/templates/base.html /templates/base.html
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/templates/create.html /templates/create.html
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/templates/index.html /templates/index.html
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp.sh /run_lamp.sh
