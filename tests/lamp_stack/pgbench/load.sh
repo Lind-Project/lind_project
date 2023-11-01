6064bf88e7e4ae4bc0b1def2ec98c03bdca617d2
RusherRG <rushang101@gmail.com>
2023-11-01 19:24:57 +0000

    tests: add prints to pgbench run
    Signed-off-by: RusherRG <rushang101@gmail.com>

--- tests/lamp_stack/pgbench/load.sh
+++ tests/lamp_stack/pgbench/load.sh
@@ -4,4 +4,4 @@
 /home/lind/lind_project/src/scripts/base/load_bash.sh
 /home/lind/lind_project/src/scripts/base/load_coreutils.sh
 /home/lind/lind_project/src/scripts/postgres/load_postgres.sh
-lindfs cp /home/lind/lind_project/tests/lamp_stack/run_pgbench.sh run_pgbench.sh
+lindfs cp /home/lind/lind_project/tests/lamp_stack/pgbench/run_pgbench.sh run_pgbench.sh
--- tests/lamp_stack/pgbench/run_lind.sh
+++ tests/lamp_stack/pgbench/run_lind.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+echo -e "Resetting lind fs"
 cd /home/lind/lind_project/lind/lindenv/fs
 rm *
-
 cd /home/lind/lind_project/tests/lamp_stack/pgbench
+
+echo -e "Loading lind"
 /home/lind/lind_project/src/mklind install &> /dev/null
+
+echo -e "Loading postgres and bash"
 ./load.sh > /dev/null
-lind /bin/bash init_postgres.sh 
+
+echo -e "Initializing postgres"
+lind /bin/bash init_postgres.sh
+
+echo -e "Running pgbench"
 lind /bin/bash run_pgbench.sh
