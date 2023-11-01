7d2498dd365c142645922b132f11d1ab9e31b0f5
RusherRG <rushang101@gmail.com>
2023-11-01 18:57:21 +0000

    tests: update uds ipc test
    Signed-off-by: RusherRG <rushang101@gmail.com>

--- tests/ipc_performance/uds/scripts/uds_test.c
+++ tests/ipc_performance/uds/scripts/uds_test.c
@@ -1,61 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
 
 #define GB (1 << 30)
 
-void parent(int socket, int buf_size, int N) {
-    char* send_buf = (char*) malloc(buf_size);
-    char* recv_buf = (char*) malloc(buf_size);
+void parent(int socket, int buf_size, int N)
+{
+    char *send_buf = (char *)malloc(buf_size);
 
     memset(send_buf, 'a', buf_size);
-
-    for (int b = 0; b < N; ++b) {
-        for (int i = 0; i < GB/buf_size; ++i) {
-            if (send(socket, send_buf, buf_size, 0) == -1) {
-                perror("Send");
-                exit(1);
-            }
-            if (recv(socket, recv_buf, buf_size, 0) == -1) {
-                perror("Recv");
-                exit(1);
-            }
-        }
-    }
-
-    free(send_buf);
-    free(recv_buf);
-}
-
-void child(int socket, int buf_size, int N) {
-    char* send_buf = (char*) malloc(buf_size);
-    char* recv_buf = (char*) malloc(buf_size);
-
-    memset(send_buf, 'b', buf_size);
-
-    for (int b = 0; b < N; ++b) {
-        for(int i = 0; i < GB/buf_size; ++i) {
-            if (recv(socket, recv_buf, buf_size, 0) == -1) {
-                perror("Recv");
-                exit(1);
-            }
-            if (send(socket, send_buf, buf_size, 0) == -1) {
+    for (int j = 0; j < N; ++j)
+    {
+        for (int i = 0; i < GB / buf_size; ++i)
+        {
+            if (send(socket, send_buf, buf_size, 0) == -1)
+            {
                 perror("Send");
                 exit(1);
             }
         }
     }
-
     free(send_buf);
+}
+
+void child(int socket, int buf_size, int N)
+{
+    char *recv_buf = (char *)malloc(buf_size);
+    for (int j = 0; j < N; ++j)
+    {
+        for (int i = 0; i < GB / buf_size; ++i)
+        {
+            if (recv(socket, recv_buf, buf_size, 0) == -1)
+            {
+                perror("Recv");
+                exit(1);
+            }
+        }
+    }
     free(recv_buf);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: [executable] [send_recv_buf_size] [total_size_in_gbs]\n");
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: [executable] [send_recv_buf_size] [total_size_in_GBs]\n");
         exit(1);
     }
 
@@ -65,27 +58,34 @@ int main(int argc, char *argv[]) {
     int sockets[2];
     pid_t pid;
 
-    if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) == -1) {
+    if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) == -1)
+    {
         perror("Socketpair");
         exit(1);
     }
 
     pid = fork();
-    if (pid == -1) {
+    if (pid == -1)
+    {
         perror("Fork");
         exit(1);
     }
 
-    if (pid == 0) {
+    if (pid == 0)
+    {
         // Child process
         close(sockets[1]);
         child(sockets[0], buf_size, N);
         close(sockets[0]);
-    } else {
+    }
+    else
+    {
         // Parent process
         close(sockets[0]);
         parent(sockets[1], buf_size, N);
         close(sockets[1]);
     }
-    exit(0);
+
+    wait(NULL);
+    return 0;
 }
