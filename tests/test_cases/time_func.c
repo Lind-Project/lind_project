5b464d6b04602a9aba6281694f9d1a6ab9f0c2cc
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-11-03 22:48:08 -0400

    Add timing dummy function test

---
+++ tests/test_cases/time_func.c
@@ -0,0 +1,43 @@
+#include <stdio.h> 
+#include <fcntl.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+#include <time.h>
+
+void dummyFunc() {
+
+}
+
+long long gettimens(void) {
+  struct timespec tp;
+
+  if( clock_gettime(CLOCK_MONOTONIC, &tp) == -1 ) {
+    perror( "clock gettime" );
+    exit( EXIT_FAILURE );
+  }
+
+  return (tp.tv_sec * 1000000000) + tp.tv_nsec;
+}
+
+long long execution_time = 0;
+
+int main(int argc, char *argv[]) {
+    int i = atoi(argv[1]);
+    int count = 0;
+    
+    long long start_time = gettimens();
+    
+    while(count <= i) {
+        dummyFunc();
+    }
+    
+    // Get sum of time
+    long long end_time = gettimens();
+    long long total_time = end_time - start_time;
+    // Average
+    long long average_time = total_time/count;
+    fprintf(stderr, "%d dummy function calls, average time %lld ns\n", count, average_time);
+    fflush(stderr);
+    
+}
+
