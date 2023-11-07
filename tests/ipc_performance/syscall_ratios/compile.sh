0826c521dbef95f58e7027499990e28df5510d32
RusherRG <rushang101@gmail.com>
2023-11-07 21:55:42 +0000

    tests: add optimization to ipc performance tests
    Signed-off-by: lind <lind@nyu.edu>

--- tests/ipc_performance/syscall_ratios/compile.sh
+++ tests/ipc_performance/syscall_ratios/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Compiling native test binaries"
-gcc scripts/write_var_time.c -o scripts/write_var_time -lpthread
-gcc scripts/read_var_time.c -o scripts/read_var_time -lpthread
+gcc scripts/write_var_time.c -o scripts/write_var_time -lpthread -O3
+gcc scripts/read_var_time.c -o scripts/read_var_time -lpthread -O3
 
 echo "Compiling lind test binaries"
-x86_64-nacl-gcc scripts/write_var_time.c -o scripts/write_var_time.nexe -std=gnu99 -lpthread -lrt
-x86_64-nacl-gcc scripts/read_var_time.c -o scripts/read_var_time.nexe -std=gnu99 -lpthread -lrt
+x86_64-nacl-gcc scripts/write_var_time.c -o scripts/write_var_time.nexe -std=gnu99 -lpthread -lrt -O3
+x86_64-nacl-gcc scripts/read_var_time.c -o scripts/read_var_time.nexe -std=gnu99 -lpthread -lrt -O3
--- tests/ipc_performance/total_runtime/compile.sh
+++ tests/ipc_performance/total_runtime/compile.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Compiling native test binaries"
-gcc scripts/write16var.c -o scripts/write16var -lpthread
-gcc scripts/read16var.c -o scripts/read16var -lpthread
+gcc scripts/write16var.c -o scripts/write16var -lpthread -O3
+gcc scripts/read16var.c -o scripts/read16var -lpthread -O3
 
 echo "Compiling lind test binaries"
-x86_64-nacl-gcc scripts/write16var.c -o scripts/write16var.nexe -std=gnu99 -lpthread
-x86_64-nacl-gcc scripts/read16var.c -o scripts/read16var.nexe -std=gnu99 -lpthread
+x86_64-nacl-gcc scripts/write16var.c -o scripts/write16var.nexe -std=gnu99 -lpthread -O3
+x86_64-nacl-gcc scripts/read16var.c -o scripts/read16var.nexe -std=gnu99 -lpthread -O3
 
 echo "Compiling user test binaries"
 cargo build --release --manifest-path=scripts/userpipe/Cargo.toml
