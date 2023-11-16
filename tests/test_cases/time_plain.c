d91f538c258c8892c6067aa3d296a7761fd45920
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-11-16 15:52:58 -0500

    Test timer time

---
+++ tests/test_cases/time_plain.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+#include <fcntl.h> 
+#include <stdlib.h> 
+#include <unistd.h>
+#include <time.h>
+
+long long gettimens(void) {
+  struct timespec tp;
+
+  if( clock_gettime(CLOCK_MONOTONIC, &tp) == -1 ) {
+    perror( "clock gettime" );
+    exit( EXIT_FAILURE );
+  }
+
+  return (tp.tv_sec * 1000000000) + tp.tv_nsec;
+}
+
+long long execution_time = 0;
+
+int main() {
+    long long start_time = gettimens();
+    // Get sum of time
+    long long end_time = gettimens();
+    long long total_time = end_time - start_time;
+
+    printf("total time %lld ns\n", total_time);
+    fflush(NULL);
+    
+}
+
