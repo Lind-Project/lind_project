4d01497d707b97b0bbcad27885cf30cf57ea1d4c
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-11-21 14:23:10 -0500

    Change measure ways

--- tests/test_cases/test.sh
+++ tests/test_cases/test.sh
@@ -9,15 +9,19 @@ COMMAND=$1
 
 max_time=0
 min_time=-1
+max_speed=0
+min_speed=-1
 total_time=0
+total_speed=0
 
-for i in {1..50}
+for i in {1..1073741824}
 do
     # Run
     output=$($COMMAND)
 
     # Get time
     time=$(echo $output | grep -oP ' time \K\d+')
+    speed=$(echo $output | grep -oP 'average speed \K\d+')
 
     # Calculate max and min
     if [ "$time" -gt "$max_time" ]; then
@@ -28,13 +32,27 @@ do
         min_time=$time
     fi
 
+    if [ "$speed" -gt "$max_speed" ]; then
+        max_speed=$speed
+    fi
+
+    if [ "$min_speed" -eq "-1" ] || [ "$speed" -lt "$min_speed" ]; then
+        min_speed=$speed
+    fi
+
+    total_speed=$((total_speed + speed))
+
     total_time=$((total_time + time))
 done
 
 # Calculate average
-average_time=$((total_time / 50))
+average_time=$((total_time / 1073741824))
+average_speed=$((total_speed / 1073741824))
 
 # Output
 echo "Max Time: $max_time ns"
 echo "Min Time: $min_time ns"
 echo "Average Time: $average_time ns"
+echo "Max Speed: $max_speed ns"
+echo "Min Speed: $min_speed ns"
+echo "Average Time: $average_speed ns"
