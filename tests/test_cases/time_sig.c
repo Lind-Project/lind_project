b8d65d091bd360cf62a1674ddd19c9fb093052b8
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-11-21 14:46:58 -0500

    Add sig test

--- tests/test_cases/time_sig.c
+++ tests/test_cases/time_sig.c
@@ -7,26 +7,25 @@
 #include <sys/ipc.h>
 #include <signal.h>
 #include <sys/wait.h>
-​
+
 /*--------Timing functions--------*/
 long long execution_time = 0;
-​
+
 long long gettimens(void) {
     struct timespec tp;
-​
+
     if (clock_gettime(CLOCK_MONOTONIC, &tp) == -1) {
         perror("clock gettime");
         exit(EXIT_FAILURE);
     }
-​
+
     return (tp.tv_sec * 1000000000) + tp.tv_nsec;
 }
-​
+
 long long tr, start, end, td; // Timestamp
 long long td_end, tr_end;
-​
 int pipe_fd[2]; // Pipe fd
-​
+
 /*--------Signal functions--------*/
 static void sig_usr(int signum){
     printf("Received signal %d\n", signum);
@@ -36,7 +35,7 @@ static void sig_usr(int signum){
         write(pipe_fd[1], "r", 1);
     }
 }
-​
+
 /*--------Process functions--------*/
 void process1(int pid) {
     // 2. P1 marks the starting time
@@ -58,7 +57,7 @@ void process2() {
     // Set signal handler
     struct sigaction sa_usr;
     sa_usr.sa_handler = sig_usr;   
-​ 
+
    sigaction(SIGUSR2, &sa_usr, NULL);
     // 1. Blocks awaiting data from P1
     pause();
@@ -69,7 +68,7 @@ void process2() {
     // 7. P2 attempts to read a response token from P1
     _exit(EXIT_SUCCESS);
 }
-​
+
 /*--------Main function--------*/
 int main(int argc, char *argv[]) {
     printf("Start\n");
@@ -78,9 +77,9 @@ int main(int argc, char *argv[]) {
         perror("pipe");
         exit(EXIT_FAILURE);
     }
-​
+
     pid_t pid = fork();
-​
+
     if (pid == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
@@ -94,12 +93,12 @@ int main(int argc, char *argv[]) {
         close(pipe_fd[0]);
         close(pipe_fd[1]);
     }
-​
+
     long long t = end - start;
     long long tc = t/2-td-tr;
     printf("tr: %lld\ntd: %lld\nstart: %lld\nend:%lld\nt:%lld\n", tr, td, start, end, t);
     printf("Context switching time: %lld\n", tc);
     fflush(NULL);
-​
+
     return 0;
 }
