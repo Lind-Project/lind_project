7193f54352937a13fea8054929ea3dc75dd90e23
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-11-21 14:13:22 -0500

    Update

--- tests/test_cases/time_memcpy.c
+++ tests/test_cases/time_memcpy.c
@@ -30,17 +30,17 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    int count = 1; // Need change
+    int count = 0; // Need change
     int data_size_4K = 1024*4;
     int data_size_1M = 1024*1024;
     
     long long start_time = gettimens();
 
-    // while(count <= 1073741824) {
-    //     memcpy(dest, src, data_size_1M);
-    //     count++;
-    // }
-    memcpy(dest, src, data_size_1M);
+    while(count < 1073741824) {
+        memcpy(dest, src, data_size_1M);
+        count++;
+    }
+    // memcpy(dest, src, data_size_1M);
 
     // Get sum of time
     long long end_time = gettimens();
