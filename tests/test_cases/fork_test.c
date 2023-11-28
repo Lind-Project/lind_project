7aae655bd2b1366411f7b2a5941f099197895a4b
Yaxuan-w <wenyaxuan0925@outlook.com>
2023-11-28 13:34:04 -0500

    Add fork test

---
+++ tests/test_cases/fork_test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid;
+
+    pid = vfork(); // Create a child process
+
+    if (pid < 0) {
+        // Error occurred
+        perror("vfork failed");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // Child process
+        printf("Child process, PID = %d\n", getpid());
+        _exit(EXIT_SUCCESS); // Use _exit to exit the child process
+    } else {
+        // Parent process
+        wait(NULL); // Wait for the child process to finish
+        printf("Parent process, PID = %d\n", getpid());
+    }
+
+    return 0;
+}
