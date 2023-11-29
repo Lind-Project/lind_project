1b3166c40da215f67d92ee67d29ccd2503978e69
Alice W. <40227173+Yaxuan-w@users.noreply.github.com>
2023-11-29 16:32:33 -0500

    Add gcc-9.5.0

---
+++ tests/applications/gcc-9.5.0/gcc/testsuite/gcc.dg/gomp/pr82374.c
@@ -0,0 +1,34 @@
+/* PR tree-optimization/82374 */
+/* { dg-do compile } */
+/* Option -fno-common makes vectorizer able to force alignment and ensures
+   vectorization can succeed even on targets lacking of unaligned vector
+   load/store.  */
+/* { dg-options "-O2 -fno-tree-vectorize -fdump-tree-vect-details -fno-common" } */
+/* { dg-additional-options "-mavx -mno-avx2" { target i?86-*-* x86_64-*-* } } */
+/* { dg-additional-options "-mvsx" { target powerpc_vsx_ok } } */
+
+#define SIZE (1024 * 1024)
+
+float a[SIZE];
+float b[SIZE];
+float c[SIZE];
+float d[SIZE];
+
+__attribute__((optimize ("O2", "tree-vectorize"))) void
+foo (void)
+{
+  int i;
+#pragma omp parallel for
+  for (i = 0; i < SIZE; i++)
+    c[i] = a[i] + b[i];
+}
+
+__attribute__((optimize ("O2", "tree-vectorize"))) void
+bar (void)
+{
+  int i;
+  for (i = 0; i < SIZE; i++)
+    d[i] = a[i] + b[i];
+}
+
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 2 "vect" { target { { i?86-*-* x86_64-*-* } || { powerpc_vsx_ok } } } } } */
