48aeed22e6eb6ab97af53845f65c9831a59145d0
Alice W. <40227173+Yaxuan-w@users.noreply.github.com>
2023-12-04 13:19:00 -0500

    Add timing sh

---
+++ tests/test_cases/test_time.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <command>"
+    exit 1
+fi
+
+COMMAND=$1
+
+max_time=0
+min_time=-1
+total_time=0
+
+for i in {1..50}
+do
+    # Run
+    output=$($COMMAND)
+
+    # Get time
+    time=$(echo $output | grep -oP ' time \K\d+')
+
+    # Calculate max and min
+    if [ "$time" -gt "$max_time" ]; then
+        max_time=$time
+    fi
+
+    if [ "$min_time" -eq "-1" ] || [ "$time" -lt "$min_time" ]; then
+        min_time=$time
+    fi
+
+    total_time=$((total_time + time))
+done
+
+# Calculate average
+average_time=$((total_time / 50))
+
+# Output
+echo "Max Time: $max_time ns"
+echo "Min Time: $min_time ns"
+echo "Average Time: $average_time ns"
