c6df94c36a6e4979bbfaebf40ffd267cfb95d704
RusherRG <rushang101@gmail.com>
2024-01-02 18:30:03 +0000

    tests: reformatting lamp stack tests
    Signed-off-by: lind <lind@nyu.edu>

--- tests/lamp_stack/profiling/flask-app/load.sh
+++ tests/lamp_stack/profiling/flask-app/load.sh
@@ -1,7 +1,7 @@
 lindfs mkdir /templates
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/gunicornexec.py /gunicornexec.py
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/wsgi.py /wsgi.py
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/app.py /app.py
+lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/no_pg_app.py /app.py
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/init_table.py /init_table.py
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/templates/base.html /templates/base.html
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask-app/templates/index.html /templates/index.html
