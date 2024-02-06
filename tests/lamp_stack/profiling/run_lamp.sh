615e657184a8fb9273698ddf95b31df883a6aa73
RusherRG <rushang101@gmail.com>
2024-02-06 00:59:42 +0000

    fix: use unix sockets for gunicorn
    Signed-off-by: lind <lind@nyu.edu>

--- tests/lamp_stack/profiling/run_lamp.sh
+++ tests/lamp_stack/profiling/run_lamp.sh
@@ -9,6 +9,6 @@ echo "table ready"
 /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf -p /usr/local/nginx/
 sleep 5
 echo "nginx ready"
-/bin/python gunicornexec.py --bind 0.0.0.0:8000 wsgi:app 2>&1
+/bin/python gunicornexec.py --bind unix:/tmp/gunicorn.sock wsgi:app 2>&1
 sleep 5
 echo "system ready"
