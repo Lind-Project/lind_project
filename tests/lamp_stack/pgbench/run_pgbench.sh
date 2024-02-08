a0dd5acd3e36625535b9756a44c54f04afc340f1
RusherRG <rushang101@gmail.com>
2024-02-08 22:52:39 +0000

    feat: update pgbench scripts
    Signed-off-by: lind <lind@nyu.edu>

--- tests/lamp_stack/pgbench/run_pgbench.sh
+++ tests/lamp_stack/pgbench/run_pgbench.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-/usr/local/pgsql/bin/postgres -F -c listen_addresses='' -D /usr/local/pgsql/data/ &
+/usr/local/pgsql/bin/pg_ctl -o "-c listen_addresses=''" -D /usr/local/pgsql/data/ start &
 sleep 15
 /usr/local/pgsql/bin/pgbench -i -U lind postgres
 echo "PGBENCH -i -U COMPLETE"
 /usr/local/pgsql/bin/pgbench -t 1000 -U lind postgres
 echo "PGBENCH COMPLETE"
+/usr/local/pgsql/bin/pg_ctl stop -D /usr/local/pgsql/data/ -m smart
+echo "POSTGRES STOPPED"
--- tests/lamp_stack/pgbench/run_pgbench2.sh
+++ tests/lamp_stack/pgbench/run_pgbench2.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-/usr/local/pgsql/bin/postgres -F -c listen_addresses='' -D /usr/local/pgsql/data/ &
-sleep 15
+/bin/unlink /usr/local/pgsql/data/postmaster.pid
+/usr/local/pgsql/bin/pg_resetwal -f /usr/local/pgsql/data/
+/usr/local/pgsql/bin/pg_ctl -o "-c listen_addresses=''" -D /usr/local/pgsql/data/ start &
+sleep 20
+/usr/local/pgsql/bin/pgbench -i -U lind postgres
+echo "PGBENCH -i -U COMPLETE"
 /usr/local/pgsql/bin/pgbench -U lind postgres
 echo "PGBENCH COMPLETE"
+/usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data/ -m smart stop
+echo "POSTGRES STOPPED"
