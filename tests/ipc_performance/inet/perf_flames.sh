80f2a24083eba3fbe88ff39f515cbd91d0def531
RusherRG <rushang101@gmail.com>
2024-02-28 23:42:04 +0000

    test: add README.md in inet
    Signed-off-by: lind <lind@nyu.edu>

--- tests/ipc_performance/inet/perf_flames.sh
+++ tests/ipc_performance/inet/perf_flames.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# perf record -F 99 -a --call-graph dwarf -- ./scripts/poll_inet_tcp
-# perf script | ../../../../FlameGraph/stackcollapse-perf.pl > out.perf-folded
-# ../../../../FlameGraph/flamegraph.pl out.perf-folded > data/native.svg
+perf record -F 99 -a --call-graph dwarf -- ./scripts/poll_inet_tcp
+perf script | ../../../../FlameGraph/stackcollapse-perf.pl > out.perf-folded
+../../../../FlameGraph/flamegraph.pl out.perf-folded > data/native.svg
 
 perf record -F 99 -a --call-graph dwarf -- lind /poll_inet_tcp
 perf script | ../../../../FlameGraph/stackcollapse-perf.pl > out.perf-folded
