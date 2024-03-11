726dcdf1561bbe38f30bbe91b71aab46792169f0
RusherRG <rushang101@gmail.com>
2024-03-11 00:39:01 +0000

    test: update context_times test
    Signed-off-by: lind <lind@nyu.edu>

--- tests/context_times/compile.sh
+++ tests/context_times/compile.sh
@@ -2,6 +2,7 @@
 
 echo "Compiling native test binaries"
 gcc scripts/context.c -o scripts/context -O2 -lpthread -lrt
+gcc scripts/context_pipe.c -o scripts/context_pipe -O2 -lpthread -lrt
 gcc scripts/ds.c -o scripts/ds -O2 -lpthread -lrt
 gcc scripts/func.c -o scripts/func -O2 -lpthread -lrt
 gcc scripts/getpid.c -o scripts/getpid -O2 -lpthread -lrt
@@ -11,6 +12,7 @@ gcc scripts/shm.c -o scripts/shm -O2 -lpthread -lrt
 
 echo "Compiling lind test binaries"
 x86_64-nacl-gcc scripts/context.c -o scripts/context.nexe -O2 -std=gnu99 -lpthread -lrt
+x86_64-nacl-gcc scripts/context_pipe.c -o scripts/context_pipe.nexe -O2 -std=gnu99 -lpthread -lrt
 x86_64-nacl-gcc scripts/ds.c -o scripts/ds.nexe -O2 -std=gnu99 -lpthread -lrt
 x86_64-nacl-gcc scripts/func.c -o scripts/func.nexe -O2 -std=gnu99 -lpthread -lrt
 x86_64-nacl-gcc scripts/getpid.c -o scripts/getpid.nexe -O2 -std=gnu99 -lpthread -lrt
@@ -20,6 +22,7 @@ x86_64-nacl-gcc scripts/shm.c -o scripts/shm.nexe -O2 -std=gnu99 -lpthread -lrt
 
 echo "Copying test cases to lindfs"
 lindfs cp $PWD/scripts/context.nexe /context
+lindfs cp $PWD/scripts/context_pipe.nexe /context_pipe
 lindfs cp $PWD/scripts/ds.nexe /ds
 lindfs cp $PWD/scripts/func.nexe /func
 lindfs cp $PWD/scripts/getpid.nexe /getpid
