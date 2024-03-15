d6355980cfd1048410676f447d42401607dba701
Mayank Ramnani <mayank-ramnani@users.noreply.github.com>
2024-03-15 19:15:27 -0400

    Fix UDS tests after signal merge (#357)

--- tests/test_cases/uds-serverclient.py
+++ tests/test_cases/uds-serverclient.py
@@ -1,5 +1,5 @@
 #!/bin/python2
-# Check if uds-socketselect.c runs correctly in native as it does within Lind
+# Check if uds-serverclient.c runs correctly in native as it does within Lind
 
 import re
 import sys
@@ -17,13 +17,6 @@ native_result_split = native_results.split('\n')
 if len(lind_result_split) != len(native_result_split):
     print "Mismatched number of lines!"
     exit(-1)
-
-# Replace specific fds in lines with *
-for i in range(len(lind_result_split)):
-    if lind_result_split[i].find("New incoming connection") != -1 or lind_result_split[i].find("Descriptor") != -1:
-        lind_result_split[i] = re.sub("\d", "*", lind_result_split[i])
-    if native_result_split[i].find("New incoming connection") != -1 or native_result_split[i].find("Descriptor") != -1:
-        native_result_split[i] = re.sub("\d", "*", native_result_split[i])
 
 # Check if same number of each line is contained in both lists
 if Counter(lind_result_split) != Counter(native_result_split):
