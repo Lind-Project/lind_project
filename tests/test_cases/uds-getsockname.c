f0ad5f4f1b85136de89c708c4f72c042bf897c65
lind <lind@nyu.edu>
2024-03-31 19:17:43 +0000

    fixing breaking tests

--- tests/test_cases/uds-getsockname.c
+++ tests/test_cases/uds-getsockname.c
@@ -5,17 +5,31 @@
 #include <string.h>
 #include <sys/un.h>
 
+
+#define SOCK_PATH "unix_sock.tmp"
+
 void main()
 {
     int addrlen;
     int rc;
     int server_sock;
-    struct sockaddr_un server_addr;
+    struct sockaddr_un server_addr, server_addr2;
+
+    unlink(SOCK_PATH); // just in case this didnt get unlinked last time
 
     int server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
 
-    addrlen = sizeof(server_addr);
-    rc = getsockname(server_fd, (struct sockaddr *)&server_addr, &addrlen);
+    server_addr.sun_family = AF_UNIX;
+    strcpy(server_addr.sun_path, SOCK_PATH);
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("BIND ERROR: ");
+        close(server_fd);
+        exit(1);
+    }
+
+    addrlen = sizeof(server_addr2);
+    rc = getsockname(server_fd, (struct sockaddr *)&server_addr2, &addrlen);
 
     if (rc == -1)
     {
@@ -23,4 +37,8 @@ void main()
     }
 
     printf("sun_path = %s\n", server_addr.sun_path);
+
+    close(server_fd);
+    unlink(SOCK_PATH);
+
 }
