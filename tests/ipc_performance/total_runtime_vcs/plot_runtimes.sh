8e36ce3d71f860d982c5478d7c9c425d80b3e0ae
RusherRG <rushang101@gmail.com>
2024-04-11 01:08:14 +0000

    test: remove unnecessary data files
    Signed-off-by: lind <lind@nyu.edu>

--- tests/ipc_performance/total_runtime_vcs/plot_runtimes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 gen_runtimes_plots.py data/nat_x_x.json data/lind_x_x.json data/user_x_x.json N-N bar3-N-N.png
-python3 gen_runtimes_plots.py data/nat_16_x.json data/lind_16_x.json data/user_16_x.json 16-N bar3-16-N.png
-python3 gen_runtimes_plots.py data/nat_x_16.json data/lind_x_16.json data/user_x_16.json N-16 bar3-N-16.png
