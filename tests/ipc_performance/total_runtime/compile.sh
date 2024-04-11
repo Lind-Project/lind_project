6f29c7be2770819a4c0538955966fcff8314a71d
RusherRG <rushang101@gmail.com>
2024-04-11 00:39:36 +0000

    test: using native-rustposix module for user tests
    Signed-off-by: lind <lind@nyu.edu>

--- tests/ipc_performance/total_runtime/compile.sh
+++ tests/ipc_performance/total_runtime/compile.sh
@@ -13,5 +13,6 @@ x86_64-nacl-gcc scripts/write32var.c -o scripts/write32var.nexe -std=gnu99 -lpth
 x86_64-nacl-gcc scripts/read32var.c -o scripts/read32var.nexe -std=gnu99 -lpthread -O3
 
 echo "Compiling user test binaries"
-cargo build --release --manifest-path=scripts/userpipe/Cargo.toml
-cp scripts/userpipe/target/release/userpipe scripts/userpipetest
+cd scripts/native-rustposix
+make perf-test
+cd ../..
