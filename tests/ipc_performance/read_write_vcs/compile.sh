343ddbda8bd69758d9112548077dd4325b30de0e
RusherRG <rushang101@gmail.com>
2024-04-11 03:46:13 +0000

    test: update paths to native-rustposix
    Signed-off-by: lind <lind@nyu.edu>

--- tests/ipc_performance/read_write_vcs/compile.sh
+++ tests/ipc_performance/read_write_vcs/compile.sh
@@ -13,6 +13,6 @@ x86_64-nacl-gcc scripts/write32var.c -o scripts/write32var.nexe -std=gnu99 -lpth
 x86_64-nacl-gcc scripts/read32var.c -o scripts/read32var.nexe -std=gnu99 -lpthread -O3
 
 echo "Compiling user test binaries"
-cd scripts/native-rustposix
+cd /home/lind/lind_project/tests/native-rustposix
 make perf-test
-cd ../..
+cd /home/lind/lind_project/tests/ipc_performance/total_runtime
