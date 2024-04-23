948252595e68100e86d0057b16b9252078606781
RusherRG <rushang101@gmail.com>
2024-04-23 02:32:34 +0000

    tests: lamp-stack merge develop cleanup
    Signed-off-by: lind <lind@nyu.edu>

--- tests/lamp_stack/profiling/lind_setup_no_pg.sh
+++ tests/lamp_stack/profiling/lind_setup_no_pg.sh
@@ -7,6 +7,3 @@ rm *
 echo -e "Loading LAMP stack\n"
 /home/lind/lind_project/src/scripts/lamp/load_lamp.sh > /dev/null
 /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/load.sh > /dev/null
-
-echo -e "Initializing Postgres"
-lind /bin/bash init_postgres.sh > /dev/null
