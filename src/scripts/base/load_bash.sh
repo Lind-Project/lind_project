ba231f353a85ce8abe37812c55f9bea17a1d6840
Alice W. <wenyaxuan0925@outlook.com>
2024-06-11 17:04:30 -0400

    change coreutils test suite script

--- src/scripts/base/load_bash.sh
+++ src/scripts/base/load_bash.sh
@@ -6,7 +6,7 @@ cd /home/lind/lind_project/tests/applications/bash/
 # lindfs cp /home/lind/lind_project/tests/applications/bash/bash /bin/sh
 
 # Replaced by new lindfs
-cp /home/lind/lind_project/tests/applications/bash/bash /home/lind/lind_project/src/safeposix-rust/tmp/bin/bash
-cp /home/lind/lind_project/tests/applications/bash/bash /home/lind/lind_project/src/safeposix-rust/tmp/bin/sh
+cp -r /home/lind/lind_project/tests/applications/bash/bash /home/lind/lind_project/src/safeposix-rust/tmp/bin/bash
+cp -r /home/lind/lind_project/tests/applications/bash/bash /home/lind/lind_project/src/safeposix-rust/tmp/bin/sh
 
 echo "Bash has been installed. Usage: lind /bin/bash /script.sh"
--- src/scripts/base/load_coreutils.sh
+++ src/scripts/base/load_coreutils.sh
@@ -5,6 +5,6 @@ cd /home/lind/lind_project/tests/applications/coreutils/src;
 find . -perm /a+x -type f -exec lindfs cp /home/lind/lind_project/tests/applications/coreutils/src/{} /bin/{} \;
 
 # Replaced (AW)
-find . -perm /a+x -type f -exec cp /home/lind/lind_project/tests/applications/coreutils/src/{} /home/lind/lind_project/src/safeposix-rust/tmp/bin/{} \;
+find . -perm /a+x -type f -exec cp -r /home/lind/lind_project/tests/applications/coreutils/src/{} /home/lind/lind_project/src/safeposix-rust/tmp/bin/{} \;
 
 echo "Coreutils has been installed. Usage: lind /bin/(anycoretuil)"
