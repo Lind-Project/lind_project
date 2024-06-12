d4880349e71479c791c1e94103684117d9dda775
Alice W. <wenyaxuan0925@outlook.com>
2024-06-11 22:19:04 -0400

    change script of uds

--- tests/ipc_performance/uds/compile.sh
+++ tests/ipc_performance/uds/compile.sh
@@ -5,4 +5,5 @@ gcc scripts/uds_send_recv.c -o scripts/uds -O3 -std=gnu99
 
 echo "Compiling lind test binaries"
 x86_64-nacl-gcc scripts/uds_send_recv.c -o scripts/uds.nexe -std=gnu99 -O3
-lindfs cp /home/lind/lind_project/tests/ipc_performance/uds/scripts/uds.nexe /uds
+# lindfs cp /home/lind/lind_project/tests/ipc_performance/uds/scripts/uds.nexe /uds
+cp /home/lind/lind_project/tests/ipc_performance/uds/scripts/uds.nexe /home/lind/lind_project/src/safeposix-rust/tmp/uds
