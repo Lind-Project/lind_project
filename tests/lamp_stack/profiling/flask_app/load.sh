eda6a9bf9453c6779fd2f18f6dd60843949bae15
Alice W. <wenyaxuan0925@outlook.com>
2024-06-14 16:14:20 -0400

    lamp stack script

--- tests/lamp_stack/profiling/flask_app/load.sh
+++ tests/lamp_stack/profiling/flask_app/load.sh
@@ -1,13 +1,26 @@
-lindfs mkdir /templates
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/gunicornexec.py /gunicornexec.py
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/wsgi.py /wsgi.py
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/wsgi_no_pg.py /wsgi_no_pg.py
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/app.py /app.py
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/test_pg.py /test_pg.py
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/no_pg_app.py /no_pg_app.py
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/init_table.py /init_table.py
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/templates/base.html /templates/base.html
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/templates/index.html /templates/index.html
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp.sh /run_lamp.sh
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp_no_flask.sh /run_lamp_no_flask.sh
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp_no_pg.sh /run_lamp_no_pg.sh
+# lindfs mkdir /templates
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/gunicornexec.py /gunicornexec.py
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/wsgi.py /wsgi.py
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/wsgi_no_pg.py /wsgi_no_pg.py
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/app.py /app.py
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/test_pg.py /test_pg.py
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/no_pg_app.py /no_pg_app.py
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/init_table.py /init_table.py
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/templates/base.html /templates/base.html
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/templates/index.html /templates/index.html
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp.sh /run_lamp.sh
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp_no_flask.sh /run_lamp_no_flask.sh
+# lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp_no_pg.sh /run_lamp_no_pg.sh
+mkdir /home/lind/lind_project/src/safeposix-rust/tmp/templates
+cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/gunicornexec.py /home/lind/lind_project/src/safeposix-rust/tmp/gunicornexec.py
+cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/wsgi.py /home/lind/lind_project/src/safeposix-rust/tmp/wsgi.py
+cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/wsgi_no_pg.py /home/lind/lind_project/src/safeposix-rust/tmp/wsgi_no_pg.py
+cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/app.py /home/lind/lind_project/src/safeposix-rust/tmp/app.py
+cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/test_pg.py /home/lind/lind_project/src/safeposix-rust/tmp/test_pg.py
+cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/no_pg_app.py /home/lind/lind_project/src/safeposix-rust/tmp/no_pg_app.py
+cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/init_table.py /home/lind/lind_project/src/safeposix-rust/tmp/init_table.py
+cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/templates/base.html /home/lind/lind_project/src/safeposix-rust/tmp/templates/base.html
+cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/templates/index.html /home/lind/lind_project/src/safeposix-rust/tmp/templates/index.html
+cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp.sh /home/lind/lind_project/src/safeposix-rust/tmp/run_lamp.sh
+cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp_no_flask.sh /home/lind/lind_project/src/safeposix-rust/tmp/run_lamp_no_flask.sh
+cp /home/lind/lind_project/tests/lamp_stack/profiling/run_lamp_no_pg.sh /home/lind/lind_project/src/safeposix-rust/tmp/run_lamp_no_pg.sh
