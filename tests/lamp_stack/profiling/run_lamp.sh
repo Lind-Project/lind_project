f1bf385a311834632e83c06a0fcc35b98002d2b2
Alice W. <wenyaxuan0925@outlook.com>
2024-06-19 11:23:49 -0400

    update run_lamp.sh

--- tests/lamp_stack/profiling/run_lamp.sh
+++ tests/lamp_stack/profiling/run_lamp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export PYTHONHOME=/usr/local/lib/python2.7
-export PYTHONPATH=/usr/local/lib/python2.7/Lib
+export PYTHONHOME=/usr
+export PYTHONPATH=/usr/local/lib/python2.7
 
 /usr/local/pgsql/bin/postgres -F -c listen_addresses='' -D /usr/local/pgsql/data/ &
 sleep 15
