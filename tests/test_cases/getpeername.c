b9812dffccb8050a9e6d8dac8ec39881b30621c0
Alice W. <wenyaxuan0925@outlook.com>
2024-06-26 11:16:24 -0400

    add getpeername test

---
+++ tests/test_cases/getpeername.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void error(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void *run_server(void *arg) {
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+    
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        error("Socket failed");
+    }
+
+    // Set server sockaddr structure
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        error("Bind failed");
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        error("Listen failed");
+    }
+
+    printf("Server is listening on port %d\n", PORT);
+    fflush(stdout);
+
+    if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
+        error("Accept failed");
+    }
+
+    printf("Client connected\n");
+    fflush(stdout);
+
+    struct sockaddr_in peer_addr;
+    socklen_t peer_addr_len = sizeof(peer_addr);
+    if (getpeername(client_fd, (struct sockaddr *)&peer_addr, &peer_addr_len) == -1) {
+        error("getpeername failed");
+    }
+
+    char peer_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &peer_addr.sin_addr, peer_ip, sizeof(peer_ip));
+    printf("Client IP: %s, Client Port: %d\n", peer_ip, ntohs(peer_addr.sin_port));
+
+    close(client_fd);
+    close(server_fd);
+
+    return NULL;
+}
+
+void run_client() {
+    int sock = 0;
+    struct sockaddr_in server_addr;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        error("Socket creation error");
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        error("Invalid address or address not supported");
+    }
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        error("Connection failed");
+    }
+
+    printf("Connected to server\n");
+    fflush(stdout);
+
+    close(sock);
+}
+
+int main() {
+    pthread_t server_thread;
+
+    if (pthread_create(&server_thread, NULL, run_server, NULL) != 0) {
+        error("Failed to create server thread");
+    }
+
+    sleep(1);
+
+    run_client();
+
+    pthread_join(server_thread, NULL);
+
+    return 0;
+}
