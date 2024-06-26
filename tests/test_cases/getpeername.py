42c069c6bfd012b13c35a480769a36943d428c19
Alice W. <wenyaxuan0925@outlook.com>
2024-06-26 11:24:57 -0400

    Change to nondet test

---
+++ tests/test_cases/getpeername.py
@@ -0,0 +1,30 @@
+#!/bin/python2
+import sys
+lindoutput = sys.argv[1].split('\n')
+hostoutput = sys.argv[2].split('\n')
+
+def parse_output(output):
+    # Split the output into lines
+    lines = output.split('\n')
+    
+    # Extract the parts of interest
+    server_status = lines[0]
+    server_connection = lines[1]
+    client_status = lines[2]
+    client_ip_port = lines[3].rsplit(',', 1)[0]  # Remove the port part
+    
+    return server_status, server_connection, client_status, client_ip_port
+
+def compare_outputs(output1, output2):
+    # Parse both outputs
+    parsed_output1 = parse_output(output1)
+    parsed_output2 = parse_output(output2)
+    
+    # Compare the parsed outputs
+    return parsed_output1 == parsed_output2
+
+# Compare the outputs
+if compare_outputs(lindoutput, hostoutput) == False:
+    print("The outputs are different.")
+    exit(1)
+
