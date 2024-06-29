7a49d8e69941ca6ca66f3519c257e0d05c8a1701
Alice W. <wenyaxuan0925@outlook.com>
2024-06-29 18:12:11 -0400

    draw lamp diagram

--- tests/lamp_stack/profiling/plot.sh
+++ tests/lamp_stack/profiling/plot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 gen_nginx_plots.py data/native_nginx.json data/lind_nginx.json nginx_bar.png
-python3 gen_lamp_plots.py data/native_lamp.json data/lind_lamp.json lamp_bar.png
+python3 gen_nginx_plots.py data/native_nginx.json data/lind_nginx.json data/rawposix_nginx.json nginx_bar.png
+python3 gen_lamp_plots.py data/native_lamp.json data/lind_lamp.json data/rawposix_lamp.json lamp_bar.png
