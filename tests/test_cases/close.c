8f061228a66e1379eb47e2a75899439d7e690d72
Alice W. <wenyaxuan0925@outlook.com>
2024-06-29 22:13:26 -0400

    ready to merge

--- tests/test_cases/close.c
+++ tests/test_cases/close.c
@@ -7,6 +7,17 @@
 #define FILE_PATH "close.txt"
 #define ITERATIONS 2000
 
+/*
+*   The purpose of adding this test is to ensure that file descriptors (fd) 
+*   are properly closed, which is crucial for large software systems such 
+*   as the LAMP stack that frequently open and close fds during runtime. 
+*   Improper handling of fds can have significant implications because both 
+*   RustPOSIX / RawPOSIX and Linux impose limits on the maximum number of 
+*   fds that a single process can use (typically set to 1024). This test 
+*   simplifies the debugging process for large software systems by providing 
+*   a straightforward method to verify correct fd management.
+*/
+
 int main(int argc, char **argv) {
     int fd;
     char *filename = FILE_PATH;
