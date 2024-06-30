5bd928c5c2cc78ad0f0b9b7f660027cba1c97c44
Alice W. <wenyaxuan0925@outlook.com>
2024-06-29 22:19:47 -0400

    ready to merge

--- tests/test_cases/close.c
+++ tests/test_cases/close.c
@@ -9,12 +9,12 @@
 
 /*
 *   The purpose of adding this test is to ensure that file descriptors (fd) 
-*   are properly closed, which is crucial for large software systems such 
+*   are properly closed, which is useful for large software systems such 
 *   as the LAMP stack that frequently open and close fds during runtime. 
-*   Improper handling of fds can have significant implications because both 
-*   RustPOSIX / RawPOSIX and Linux impose limits on the maximum number of 
-*   fds that a single process can use (typically set to 1024). This test 
-*   simplifies the debugging process for large software systems by providing 
+*   Improper handling of fds can have implications because both RustPOSIX 
+*   / RawPOSIX and Linux impose limits on the maximum number of fds that a 
+*   single process can use (typically set to 1024). This test simplifies 
+*   the debugging process for large software systems by providing 
 *   a straightforward method to verify correct fd management.
 */
 
