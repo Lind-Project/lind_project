b4489aad1e4ed6cf0a75a011c1bea988f2a144e4
Alice W. <wenyaxuan0925@outlook.com>
2024-07-01 13:06:42 -0400

    modify pgbench loading script

--- tests/lamp_stack/pgbench/load.sh
+++ tests/lamp_stack/pgbench/load.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -r /home/lind/lind_project/src/safeposix-rust/tmp/usr/local/pgsql
 /home/lind/lind_project/src/scripts/base/load_confs.sh
 /home/lind/lind_project/src/scripts/base/load_bash.sh
 /home/lind/lind_project/src/scripts/base/load_coreutils.sh
@@ -7,6 +8,6 @@
 # lindfs cp /home/lind/lind_project/tests/lamp_stack/pgbench/run_pg.sh run_pg.sh
 # lindfs cp /home/lind/lind_project/tests/lamp_stack/pgbench/run_pgbench.sh run_pgbench.sh
 # lindfs cp /home/lind/lind_project/tests/lamp_stack/pgbench/run_pgbench_2.sh run_pgbench_2.sh
-cp /home/lind/lind_project/tests/lamp_stack/pgbench/run_pg.sh /home/lind/lind_project/src/safeposix-rust/tmp/run_pg.sh
+cp /home/lind/lind_project/tests/lamp_stack/pgbench/run_lind.sh /home/lind/lind_project/src/safeposix-rust/tmp/run_lind.sh
 cp /home/lind/lind_project/tests/lamp_stack/pgbench/run_pgbench.sh /home/lind/lind_project/src/safeposix-rust/tmp/run_pgbench.sh
 cp /home/lind/lind_project/tests/lamp_stack/pgbench/run_pgbench_2.sh /home/lind/lind_project/src/safeposix-rust/tmp/run_pgbench_2.sh
