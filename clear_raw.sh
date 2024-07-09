50537ab591b2693b22878a5062e1c813ca6db7c1
Alice W. <wenyaxuan0925@outlook.com>
2024-07-09 11:05:09 +0800

    Add setup/clear script

--- clear_raw.sh
+++ clear_raw.sh
@@ -2,6 +2,18 @@ rm -rf /home/lind/lind_project/src/safeposix-rust/tmp/
 
 ./src/mklind install
 
+mkdir /home/lind/lind_project/src/safeposix-rust/tmp/dev
+sudo mknod /home/lind/lind_project/src/safeposix-rust/tmp/dev/tty c 5 0
+sudo chmod 666 /home/lind/lind_project/src/safeposix-rust/tmp/dev/tty
+sudo mknod /home/lind/lind_project/src/safeposix-rust/tmp/dev/zero c 1 5
+sudo chmod 666 /home/lind/lind_project/src/safeposix-rust/tmp/dev/zero
+sudo mknod /home/lind/lind_project/src/safeposix-rust/tmp/dev/random c 1 8
+sudo chmod 666 /home/lind/lind_project/src/safeposix-rust/tmp/dev/random
+sudo mknod /home/lind/lind_project/src/safeposix-rust/tmp/dev/null c 1 3
+sudo chmod 666 /home/lind/lind_project/src/safeposix-rust/tmp/dev/null
+sudo mknod /home/lind/lind_project/src/safeposix-rust/tmp/dev/urandom c 1 9
+sudo chmod 666 /home/lind/lind_project/src/safeposix-rust/tmp/dev/urandom
+
 /home/lind/lind_project/src/scripts/lamp/load_lamp.sh
 /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/load.sh
 
