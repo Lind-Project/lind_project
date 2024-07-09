3fd5057ea9682a6ee35b44e6b1f5e19ccff8a134
Alice W. <wenyaxuan0925@outlook.com>
2024-07-09 22:22:31 +0800

    change script for native lamp

--- tests/lamp_stack/profiling/run_lamp_native.sh
+++ tests/lamp_stack/profiling/run_lamp_native.sh
@@ -3,13 +3,13 @@
 /usr/local/pgsql/bin/postgres -F -d 0 -c listen_addresses='' -D /usr/local/pgsql/data/ &
 sleep 15
 echo "postgres ready"
-/bin/python init_table.py
+/bin/python flask_app/init_table.py
 sleep 3
 echo "table ready"
 /home/lind/nginx/usr/local/nginx/sbin/nginx -c /home/lind/nginx/usr/local/nginx/conf/nginx.conf -p /home/lind/nginx/usr/local/nginx/
 sleep 5
 echo "nginx ready"
-cd flask-app/
+cd flask_app/
 /bin/python gunicornexec.py --bind 0.0.0.0:8000 wsgi:app 2>&1
 sleep 5
 echo "system ready"
