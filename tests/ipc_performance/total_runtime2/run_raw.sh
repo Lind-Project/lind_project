15b581db87785f844dce91429a11678a00907c8a
Alice W. <wenyaxuan0925@outlook.com>
2024-08-13 09:26:18 -0400

    update raw runtime script

--- tests/ipc_performance/total_runtime2/run_raw.sh
+++ tests/ipc_performance/total_runtime2/run_raw.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Copying scripts to rawfs"
-lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/pipescript.sh /home/lind/lind_project/src/safeposix-rust/pipescript.sh
-lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/writepipe.nexe /home/lind/lind_project/src/safeposix-rust/writepipe
-lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/readpipe.nexe /home/lind/lind_project/src/safeposix-rust/readpipe
+cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/pipescript.sh /home/lind/lind_project/src/safeposix-rust/pipescript.sh
+cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/writepipe.nexe /home/lind/lind_project/src/safeposix-rust/writepipe
+cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/readpipe.nexe /home/lind/lind_project/src/safeposix-rust/readpipe
 
 echo "Varying write and read buffers"
 python3 gen_rawps2.py -w x -r x -c $1
