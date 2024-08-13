55788aa5074159d5871d1a64d33801e6c4ad3359
Alice W. <wenyaxuan0925@outlook.com>
2024-08-13 19:36:33 -0400

    update execution py

--- tests/ipc_performance/total_runtime2/run_pipe_performance.sh
+++ tests/ipc_performance/total_runtime2/run_pipe_performance.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-echo "Loading files into file system of lind and rawposix"
-lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/pipescript.sh /scripts/pipescript.sh
-lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/writepipe.nexe /scripts/writepipe
-lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/readpipe.nexe /scripts/readpipe
+if [ "$2" = "raw" ]; then
+    echo "Loading files into rawposix"
+    cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/readpipe.nexe /home/lind/lind_project/src/safeposix-rust/tmp/writepipe
+    cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/readpipe.nexe /home/lind/lind_project/src/safeposix-rust/tmp/readpipe
+    platforms=("lind")
+elif [ "$2" = "other" ]; then
+    echo "Loading files into file system of lind"
+    lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/pipescript.sh /scripts/pipescript.sh
+    lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/writepipe.nexe /scripts/writepipe
+    lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/readpipe.nexe /scripts/readpipe
+    echo -e "\nLoading files into native desired location"
+    cp scripts/readpipe .
+    cp scripts/writepipe .
+    platforms=("lind /bin/bash /scripts/pipescript.sh" "/bin/bash scripts/pipescript.sh" "scripts/unsafe-pipe")
+else
+    echo "Usage: ./run_pipe_performance.sh [count] [platform]"
+    exit 1
+fi
 
-echo -e "\nLoading files into native desired location"
-cp scripts/readpipe .
-cp scripts/writepipe .
-
-# cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/readpipe.nexe /home/lind/lind_project/src/safeposix-rust/tmp/writepipe
-# cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/readpipe.nexe /home/lind/lind_project/src/safeposix-rust/tmp/readpipe
-
-# platforms=("lind" "native" "rawposix" "unsafe")
-platforms=("lind /bin/bash /scripts/pipescript.sh" "/bin/bash scripts/pipescript.sh" "scripts/unsafe-pipe")
 count=$1
 
 for platform in "${platforms[@]}"; do
