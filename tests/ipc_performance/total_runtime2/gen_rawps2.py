86b3be77c070370d5aba35eb1d45fa110e6daf79
Alice W. <wenyaxuan0925@outlook.com>
2024-08-13 09:24:59 -0400

    add rawposix normal pipe short ipc scripts

--- tests/ipc_performance/total_runtime2/gen_rawps2.py
+++ tests/ipc_performance/total_runtime2/gen_rawps2.py
@@ -1,7 +1,18 @@
 import argparse
 import json
-
+import re
 from subprocess import Popen, PIPE
+
+def extract_times(output):
+    # Extract numbers from the 'write-start' and 'read-end' lines
+    write_start_match = re.search(r'write-start: (\d+)', output)
+    read_end_match = re.search(r'read-end: (\d+)', output)
+    if write_start_match and read_end_match:
+        write_start = int(write_start_match.group(1))
+        read_end = int(read_end_match.group(1))
+        return (read_end - write_start) / 1000000
+    else:
+        return None
 
 parser = argparse.ArgumentParser(
     description="Script to benchmark piping 16GB varying buffersize in lind"
@@ -38,9 +49,8 @@ for size in range(4, 17, 2):
         output = Popen(
             [
                 "lind",
-                "-t",
                 "/bin/bash",
-                "raw1proc.sh",
+                "pipescript.sh",
                 write_buffer_size,
                 read_buffer_size,
             ],
@@ -48,13 +58,14 @@ for size in range(4, 17, 2):
             stderr=PIPE,
         )
         stdout, stderr = output.communicate()
-
+        stdout = stdout.decode('utf-8') # Decode bytes to string
         try:
-            run_time = int(float(stderr.split()[-1]) * 1000)
+            run_time = extract_times(stdout)
+            if run_time is not None:
+                run_times[size].append(run_time)
         except ValueError:
             continue
-        run_times[size].append(run_time)
     print(f"Average runtime: {sum(run_times[size]) / args.count}")
 
-    with open(f"data/raw1_{args.write_buffer}_{args.read_buffer}.json", "w") as fp:
+    with open(f"data/raw_{args.write_buffer}_{args.read_buffer}.json", "w") as fp:
         json.dump(run_times, fp)
--- tests/ipc_performance/total_runtime2/run_raw.sh
+++ tests/ipc_performance/total_runtime2/run_raw.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-echo "Compile"
-x86_64-nacl-gcc /home/lind/lind_project/tests/ipc_performance/total_runtime/scripts/two-process-write-namedpp.c -o /home/lind/lind_project/src/safeposix-rust/tmp/two-process-write-namedpp.nexe -lpthread -O3 -std=gnu99
-x86_64-nacl-gcc /home/lind/lind_project/tests/ipc_performance/total_runtime/scripts/two-process-read-namedpp.c -o /home/lind/lind_project/src/safeposix-rust/tmp/two-process-read-namedpp.nexe -lpthread -O3 -std=gnu99
-x86_64-nacl-gcc /home/lind/lind_project/tests/ipc_performance/total_runtime/scripts/one-proces-namedpp.c -o /home/lind/lind_project/src/safeposix-rust/tmp/one-proces-namedpp.nexe -lpthread -O3 -std=gnu99
+echo "Copying scripts to rawfs"
+lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/pipescript.sh /home/lind/lind_project/src/safeposix-rust/pipescript.sh
+lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/writepipe.nexe /home/lind/lind_project/src/safeposix-rust/writepipe
+lindfs cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/readpipe.nexe /home/lind/lind_project/src/safeposix-rust/readpipe
 
-echo "Copying scripts to lindfs"
-cp /home/lind/lind_project/tests/ipc_performance/total_runtime/scripts/raw2proc.sh /home/lind/lind_project/src/safeposix-rust/tmp/raw2proc.sh
-cp /home/lind/lind_project/tests/ipc_performance/total_runtime/scripts/raw1proc.sh /home/lind/lind_project/src/safeposix-rust/tmp/raw1proc.sh
-
-echo "\nRawPOSIX named pipes (1 process)"
-echo "\nVarying write and read buffers"
-python3 gen_raw1.py -w x -r x -c $1
+echo "Varying write and read buffers"
+python3 gen_rawps2.py -w x -r x -c $1
 
 echo -e "\nVarying write buffers"
-python3 gen_raw1.py -w x -r 16 -c $1
+python3 gen_rawps2.py -w x -r 16 -c $1
 
 echo -e "\nVarying read buffers"
-python3 gen_raw1.py -w 16 -r x -c $1
-
-
-echo "\nRawPOSIX named pipes (2 process)"
-echo "\nVarying write and read buffers"
-python3 gen_rawnamedpp2.py -w x -r x -c $1
-
-echo -e "\nVarying write buffers"
-python3 gen_rawnamedpp2.py -w x -r 16 -c $1
-
-echo -e "\nVarying read buffers"
-python3 gen_rawnamedpp2.py -w 16 -r x -c $1
+python3 gen_rawps2.py -w 16 -r x -c $1
