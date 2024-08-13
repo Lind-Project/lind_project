0418139b9be5259098daf66b6d82b5c177e6b7d5
Alice W. <wenyaxuan0925@outlook.com>
2024-08-13 16:28:24 -0400

    udpate compile script

--- tests/ipc_performance/total_runtime2/compile.sh
+++ tests/ipc_performance/total_runtime2/compile.sh
@@ -2,8 +2,8 @@
 export PATH=/usr/local/gcc-4.4.3/bin:$PATH
 
 echo "Compiling native test binaries"
-gcc-4 scripts/read.c -o scripts/readpipe -O3
-gcc-4 scripts/write.c -o scripts/writepipe -O3
+/usr/local/gcc-4.4.3/bin/gcc scripts/read.c -o scripts/readpipe -O3
+/usr/local/gcc-4.4.3/bin/gcc scripts/write.c -o scripts/writepipe -O3
 
 echo "Compiling lind / rawposix test binaries"
 x86_64-nacl-gcc scripts/read.c -o scripts/readpipe.nexe -std=gnu99 -lrt -O3
@@ -11,7 +11,7 @@ x86_64-nacl-gcc scripts/write.c -o scripts/writepipe.nexe -std=gnu99 -lrt -O3
 
 echo "Compiling unsafe test binaries"
 export LD_LIBRARY_PATH=/home/lind/lind_project/tests/native-rustposix:$LD_LIBRARY_PATH
-gcc-4 -c /home/lind/lind_project/tests/native-rustposix/tests/lind_platform.c -o /home/lind/lind_project/tests/native-rustposix/tests/lind_platform.o -O3
-gcc-4 -c scripts/unsafe-pipe.c -o scripts/unsafe-pipe.o -I/home/lind/lind_project/tests/native-rustposix/tests/ -std=gnu99 -O3
-gcc-4 scripts/unsafe-pipe.o /home/lind/lind_project/tests/native-rustposix/tests/lind_platform.o -L/home/lind/lind_project/src/safeposix-rust/target/release -lrustposix -o scripts/unsafe-pipe -O3
+/usr/local/gcc-4.4.3/bin/gcc -c /home/lind/lind_project/tests/native-rustposix/tests/lind_platform.c -o /home/lind/lind_project/tests/native-rustposix/tests/lind_platform.o -O3
+/usr/local/gcc-4.4.3/bin/gcc -c scripts/unsafe-pipe.c -o scripts/unsafe-pipe.o -I/home/lind/lind_project/tests/native-rustposix/tests/ -std=gnu99 -O3
+/usr/local/gcc-4.4.3/bin/gcc scripts/unsafe-pipe.o /home/lind/lind_project/tests/native-rustposix/tests/lind_platform.o -L/home/lind/lind_project/src/safeposix-rust/target/release -lrustposix -o scripts/unsafe-pipe -O3
 
