947ba29930ea29349ffbb234a8e9587b38142a0f
Alice W. <wenyaxuan0925@outlook.com>
2024-08-15 10:52:07 -0400

    update compile script

--- tests/ipc_performance/total_runtime2/compile.sh
+++ tests/ipc_performance/total_runtime2/compile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Compiling lind test binaries"
-x86_64-nacl-gcc scripts/read.c -o scripts/readshort.nexe -std=gnu99 -lpthread -O3
-x86_64-nacl-gcc scripts/write.c -o scripts/writeshort.nexe -std=gnu99 -lpthread -O3
-x86_64-nacl-gcc scripts/short1proc.c -o scripts/short1proc.nexe -std=gnu99 -lpthread -O3
+x86_64-nacl-gcc scripts/readnamed.c -o /home/lind/lind_project/src/safeposix-rust/tmp/readnamed.nexe -std=gnu99 -lpthread -O3
+x86_64-nacl-gcc scripts/writenamed.c -o /home/lind/lind_project/src/safeposix-rust/tmp/writenamed.nexe -std=gnu99 -lpthread -O3
