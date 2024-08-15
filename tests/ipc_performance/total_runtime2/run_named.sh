e8b2a3557005d81d77cd2499e5376b81e8767283
Alice W. <wenyaxuan0925@outlook.com>
2024-08-15 11:02:14 -0400

    update runtime script: 1. checking fifo file. 2. update runtime command

--- tests/ipc_performance/total_runtime2/run_named.sh
+++ tests/ipc_performance/total_runtime2/run_named.sh
@@ -8,15 +8,21 @@ echo "Copying scripts to lindfs"
 cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/namedsingle.sh /home/lind/lind_project/src/safeposix-rust/tmp/namedsingle.sh
 cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/namedmulti.sh /home/lind/lind_project/src/safeposix-rust/tmp/namedmulti.sh
 
+echo "Check if FIFO file exists"
+FIFO_PATH="/home/lind/lind_project/src/safeposix-rust/tmp/myfifo"
+if [ ! -p "$FIFO_PATH" ]; then
+    mkfifo "FIFO_PATH"
+fi
+
 echo -e "\nRawPOSIX named pipes (Single process)"
 echo -e "\nVarying write and read buffers"
-python3 gen_named.py -p "/bin/bash scripts/namedsingle.sh" -w x -r x -c $1
+python3 gen_named.py -p "lind /bin/bash /namedsingle.sh" -w x -r x -c $1
 
 echo -e "\nVarying write buffers"
-python3 gen_named.py -p "/bin/bash scripts/namedsingle.sh" -w x -r 16 -c $1
+python3 gen_named.py -p "lind /bin/bash /namedsingle.sh" -w x -r 16 -c $1
 
 echo -e "\nVarying read buffers"
-python3 gen_named.py -p "/bin/bash scripts/namedsingle.sh" -w 16 -r x -c $1
+python3 gen_named.py -p "lind /bin/bash /namedsingle.sh" -w 16 -r x -c $1
 
 
 echo -e "\nRawPOSIX named pipes (Multiple process)"
