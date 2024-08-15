beee1c3bf9a8f1b6050abacf61c3b3bca3f4716f
Alice W. <wenyaxuan0925@outlook.com>
2024-08-15 10:54:23 -0400

    update compile and runtime script

--- tests/ipc_performance/total_runtime2/compile.sh
+++ tests/ipc_performance/total_runtime2/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Compiling lind test binaries"
-x86_64-nacl-gcc scripts/readnamed.c -o /home/lind/lind_project/src/safeposix-rust/tmp/readnamed.nexe -std=gnu99 -lpthread -O3
-x86_64-nacl-gcc scripts/writenamed.c -o /home/lind/lind_project/src/safeposix-rust/tmp/writenamed.nexe -std=gnu99 -lpthread -O3
+x86_64-nacl-gcc scripts/readnamed.c -o /home/lind/lind_project/src/safeposix-rust/tmp/readnamed.nexe -lrt -std=gnu99 -O3
+x86_64-nacl-gcc scripts/writenamed.c -o /home/lind/lind_project/src/safeposix-rust/tmp/writenamed.nexe -lrt -std=gnu99 -O3
--- tests/ipc_performance/total_runtime2/run_named.sh
+++ tests/ipc_performance/total_runtime2/run_named.sh
@@ -5,8 +5,8 @@ x86_64-nacl-gcc scripts/readnamed.c -o /home/lind/lind_project/src/safeposix-rus
 x86_64-nacl-gcc scripts/writenamed.c -o /home/lind/lind_project/src/safeposix-rust/tmp/writenamed.nexe -std=gnu99 -lpthread -O3
 
 echo "Copying scripts to lindfs"
-cp /home/lind/lind_project/tests/ipc_performance/total_runtime/scripts/namedsingle.sh /home/lind/lind_project/src/safeposix-rust/tmp/namedsingle.sh
-cp /home/lind/lind_project/tests/ipc_performance/total_runtime/scripts/namedmulti.sh /home/lind/lind_project/src/safeposix-rust/tmp/namedmulti.sh
+cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/namedsingle.sh /home/lind/lind_project/src/safeposix-rust/tmp/namedsingle.sh
+cp /home/lind/lind_project/tests/ipc_performance/total_runtime2/scripts/namedmulti.sh /home/lind/lind_project/src/safeposix-rust/tmp/namedmulti.sh
 
 echo -e "\nRawPOSIX named pipes (Single process)"
 echo -e "\nVarying write and read buffers"
