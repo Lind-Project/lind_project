d407ccf163ff16a4f5e955b62eb7706762e6265b
Alice W. <wenyaxuan0925@outlook.com>
2024-09-04 12:46:26 -0400

    use pthread_barrier and update test case

--- tests/ipc_performance/uds/scripts/uds_send_recv.c
+++ tests/ipc_performance/uds/scripts/uds_send_recv.c
@@ -5,8 +5,11 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
 
 #define GB (1 << 30)
+
+pthread_barrier_t barrier;
 
 long long gettimens() {
     struct timespec tp;
@@ -14,33 +17,33 @@ long long gettimens() {
     return (long long)tp.tv_sec * 1000000000LL + tp.tv_nsec;
 }
 
-void parent(int socket, int buf_size, int N) {
+void parent(int socket, int buf_size) {
     char *send_buf = (char *)malloc(buf_size);
     char *recv_buf = (char *)malloc(buf_size);
 
     memset(send_buf, 'a', buf_size);
 
+    pthread_barrier_wait(&barrier);
+
     fprintf(stderr, "Starts sending: %lld\n", gettimens());
     fflush(stderr);
 
-    for (int a = 0; a < N; ++a) {
-        for (int i = 0; i < GB / buf_size; ++i) {
-            if (send(socket, send_buf, buf_size, 0) == -1)
-            {
-                perror("Send");
+    for (int i = 0; i < GB / buf_size; ++i) {
+        if (send(socket, send_buf, buf_size, 0) == -1)
+        {
+            perror("Send");
+            exit(1);
+        }
+    }
+    for (int j = 0; j < GB / buf_size; ++j) {
+        int total_received = 0;
+        while (total_received < buf_size) {
+            int received = recv(socket, recv_buf + total_received, buf_size - total_received, 0);
+            if (received == -1) {
+                perror("Recv");
                 exit(1);
             }
-        }
-        for (int j = 0; j < GB / buf_size; ++j) {
-            int total_received = 0;
-            while (total_received < buf_size) {
-                int received = recv(socket, recv_buf + total_received, buf_size - total_received, 0);
-                if (received == -1) {
-                    perror("Recv");
-                    exit(1);
-                }
-                total_received += received;
-            }
+            total_received += received;
         }
     }
 
@@ -51,48 +54,46 @@ void parent(int socket, int buf_size, int N) {
     free(recv_buf);
 }
 
-void child(int socket, int buf_size, int N) {
+void child(int socket, int buf_size) {
     char *send_buf = (char *)malloc(buf_size);
     char *recv_buf = (char *)malloc(buf_size);
 
     memset(send_buf, 'b', buf_size);
-    for (int b = 0; b < N; ++b) {
-        // printf("iter,b : %d\n", b);
-        for (int x = 0; x < GB / buf_size; ++x) {
-            // printf("child recv\n");
-            int total_received = 0;
-            while (total_received < buf_size) {
-                int received = recv(socket, recv_buf + total_received, buf_size - total_received, 0);
-                if (received == -1)
-                {
-                    perror("Recv");
-                    exit(1);
-                }
-                total_received += received;
-            }
-        }
-        // printf("Done receiving in child\n");
-        for (int y = 0; y < GB / buf_size; ++y) {
-            // printf("child send\n");
-            if (send(socket, send_buf, buf_size, 0) == -1) {
-                perror("Send");
+
+    pthread_barrier_wait(&barrier);
+
+    for (int x = 0; x < GB / buf_size; ++x) {
+        int total_received = 0;
+        while (total_received < buf_size) {
+            int received = recv(socket, recv_buf + total_received, buf_size - total_received, 0);
+            if (received == -1)
+            {
+                perror("Recv");
                 exit(1);
             }
+            total_received += received;
         }
-        // printf("Done sending in child\n");
+    }
+
+    for (int y = 0; y < GB / buf_size; ++y) {
+        if (send(socket, send_buf, buf_size, 0) == -1) {
+            perror("Send");
+            exit(1);
+        }
     }
     free(send_buf);
     free(recv_buf);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: [executable] [send_recv_buf_size] [total_size_in_GBs]\n");
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s [send_recv_buf_size]\n", argv[0]);
         exit(1);
     }
 
     int buf_size = 1 << atoi(argv[1]);
-    int N = atoi(argv[2]);
+
+    pthread_barrier_init(&barrier, NULL, 2);
 
     int sockets[2];
     pid_t pid;
@@ -111,13 +112,18 @@ int main(int argc, char *argv[]) {
     if (pid == 0) {
         // Child process
         close(sockets[1]);
-        child(sockets[0], buf_size, N);
+        pthread_barrier_wait(&barrier);
+        child(sockets[0], buf_size);
         close(sockets[0]);
     } else {
         // Parent process
         close(sockets[0]);
-        parent(sockets[1], buf_size, N);
+        pthread_barrier_wait(&barrier);
+        parent(sockets[1], buf_size);
         close(sockets[1]);
     }
+
+    pthread_barrier_destroy(&barrier);
+
     return 0;
 }
