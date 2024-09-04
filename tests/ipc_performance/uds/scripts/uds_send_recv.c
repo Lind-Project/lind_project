6a18cb8bd0f2f3abfd87f7ef5e8f9adb0bf0c4c5
Alice W. <wenyaxuan0925@outlook.com>
2024-09-04 13:50:40 -0400

    change to use while-blocking in uds

--- tests/ipc_performance/uds/scripts/uds_send_recv.c
+++ tests/ipc_performance/uds/scripts/uds_send_recv.c
@@ -5,16 +5,10 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <time.h>
-#include <pthread.h>
 
 #define GB (1 << 30)
 
-pthread_barrier_t barrier;
-
-struct thread_args {
-    int socket;
-    int buf_size;
-};
+int lock_count = 0;
 
 long long gettimens() {
     struct timespec tp;
@@ -22,20 +16,22 @@ long long gettimens() {
     return (long long)tp.tv_sec * 1000000000LL + tp.tv_nsec;
 }
 
-void *parent(void *arg) {
-    struct thread_args *args = (struct thread_args *)arg;
-    int socket = args->socket;
-    int buf_size = args->buf_size; 
+void parent(int socket, int buf_size) {
     char *send_buf = (char *)malloc(buf_size);
     char *recv_buf = (char *)malloc(buf_size);
 
     memset(send_buf, 'a', buf_size);
 
-    pthread_barrier_wait(&barrier);
-
     fprintf(stderr, "Starts sending: %lld\n", gettimens());
     fflush(stderr);
-
+    
+    // Blocking to wait close() finishes 
+    while (lock_count != 2) {
+        if (lock_count == 2) {
+            break;
+        }
+    }
+    
     for (int i = 0; i < GB / buf_size; ++i) {
         if (send(socket, send_buf, buf_size, 0) == -1) {
             perror("Send");
@@ -54,37 +50,40 @@ void *parent(void *arg) {
         }
     }
 
+    
+
     fprintf(stderr, "Ends receiving: %lld\n", gettimens());
     fflush(stderr);
 
     free(send_buf);
     free(recv_buf);
-    return NULL;
 }
 
-void *child(void *arg) {
-    struct thread_args *args = (struct thread_args *)arg;
-    int socket = args->socket;
-    int buf_size = args->buf_size; 
+void child(int socket, int buf_size) {
     char *send_buf = (char *)malloc(buf_size);
     char *recv_buf = (char *)malloc(buf_size);
 
     memset(send_buf, 'b', buf_size);
 
-    pthread_barrier_wait(&barrier);
+    // Blocking to wait close() finishes 
+    while (lock_count != 2) {
+        if (lock_count == 2) {
+            break;
+        }
+    }
 
     for (int x = 0; x < GB / buf_size; ++x) {
         int total_received = 0;
         while (total_received < buf_size) {
             int received = recv(socket, recv_buf + total_received, buf_size - total_received, 0);
-            if (received == -1) {
+            if (received == -1)
+            {
                 perror("Recv");
                 exit(1);
             }
             total_received += received;
         }
     }
-
     for (int y = 0; y < GB / buf_size; ++y) {
         if (send(socket, send_buf, buf_size, 0) == -1) {
             perror("Send");
@@ -93,7 +92,6 @@ void *child(void *arg) {
     }
     free(send_buf);
     free(recv_buf);
-    return NULL;
 }
 
 int main(int argc, char *argv[]) {
@@ -103,28 +101,33 @@ int main(int argc, char *argv[]) {
     }
 
     int buf_size = 1 << atoi(argv[1]);
-    struct thread_args parent_args = {sockets[1], buf_size};
-    struct thread_args child_args = {sockets[0], buf_size};
-
-    pthread_t parent_thread, child_thread;
-    pthread_barrier_init(&barrier, NULL, 2);
 
     int sockets[2];
+    pid_t pid;
+
     if (socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) == -1) {
         perror("Socketpair");
         exit(1);
     }
-    
-    pthread_create(&child_thread, NULL, child, &child_args);
-    pthread_create(&parent_thread, NULL, parent, &parent_args);
 
-    pthread_join(parent_thread, NULL);
-    pthread_join(child_thread, NULL);
+    pid = fork();
+    if (pid == -1) {
+        perror("Fork");
+        exit(1);
+    }
 
-    close(sockets[0]);
-    close(sockets[1]);
-
-    pthread_barrier_destroy(&barrier);
-
+    if (pid == 0) {
+        // Child process
+        close(sockets[1]);
+        lock_count++;
+        child(sockets[0], buf_size);
+        close(sockets[0]);
+    } else {
+        // Parent process
+        close(sockets[0]);
+        lock_count++;
+        parent(sockets[1], buf_size);
+        close(sockets[1]);
+    }
     return 0;
 }
