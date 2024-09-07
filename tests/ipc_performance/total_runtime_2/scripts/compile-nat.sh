3876c8c838e411203e44ae14d3678546e8eae289
Alice W. <wenyaxuan0925@outlook.com>
2024-09-07 12:40:36 -0400

    update compilation scripts

--- tests/ipc_performance/total_runtime_2/scripts/compile-nat.sh
+++ tests/ipc_performance/total_runtime_2/scripts/compile-nat.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo "alias gcc-4='/usr/local/gcc-4.4.3/bin/gcc'" >> "$HOME/.bashrc"
+source "$HOME/.bashrc"
+
 gcc-4 scripts/pipe_write_read.c -o scripts/pipe -O3 -std=gnu99 -lrt -lpthread
