464d4cb731f1eff5880095a9097cfd266e183817
Alice W. <wenyaxuan0925@outlook.com>
2024-09-10 21:04:48 -0400

    update compilation scripts for ubuntu

--- tests/ipc_performance/total_runtime/scripts/compile-native.sh
+++ tests/ipc_performance/total_runtime/scripts/compile-native.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
+
 /usr/local/gcc-4.4.3/bin/gcc scripts/read.c -o scripts/readpipe -std=gnu99 -O3
 /usr/local/gcc-4.4.3/bin/gcc scripts/write.c -o scripts/writepipe -std=gnu99 -O3
--- tests/ipc_performance/total_runtime/scripts/compile-unsafe.sh
+++ tests/ipc_performance/total_runtime/scripts/compile-unsafe.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
+
 export LD_LIBRARY_PATH=/home/lind/lind_project/tests/native-rustposix:$LD_LIBRARY_PATH
 /usr/local/gcc-4.4.3/bin/gcc -c /home/lind/lind_project/tests/native-rustposix/tests/lind_platform.c -o /home/lind/lind_project/tests/native-rustposix/tests/lind_platform.o -O3
 /usr/local/gcc-4.4.3/bin/gcc -c scripts/unsafe-pipe.c -o scripts/unsafe-pipe.o -I/home/lind/lind_project/tests/native-rustposix/tests/ -std=gnu99 -O3
