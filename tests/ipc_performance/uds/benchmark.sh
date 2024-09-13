0e37547de24c4ef250ba8ffd3f3042bee50ae42c
Alice W. <wenyaxuan0925@outlook.com>
2024-09-12 23:16:12 -0400

    update runtime scripts for uds chunk

--- tests/ipc_performance/uds/benchmark.sh
+++ tests/ipc_performance/uds/benchmark.sh
@@ -3,14 +3,20 @@
 mkdir -p data/
 ./compile.sh
 
+# echo "Running on lind stack"
+# python3 gen_udsdata.py -c $1 -p "lind /uds"
+
+# echo -e "\nRunning on native linux"
+# python3 gen_udsdata.py -c $1 -p "scripts/uds"
+
+# echo -e "\nRunning on unsafe"
+# python3 gen_udsdata.py -c $1 -p "scripts/unsafe-uds"
+
 echo "Running on lind stack"
-python3 gen_udsdata.py -c $1 -p "lind /uds"
+python3 gen_uds_chunk.py -c $1 -p "lind /uds"
 
 echo -e "\nRunning on native linux"
-python3 gen_udsdata.py -c $1 -p "scripts/uds"
-
-echo -e "\nRunning on unsafe"
-python3 gen_udsdata.py -c $1 -p "scripts/unsafe-uds"
+python3 gen_uds_chunk.py -c $1 -p "scripts/uds"
 
 echo -e "\nCleaning up"
 ./cleanup.sh
