2af88e6b8eb2c2c33a4df55a7597f9b6ad605884
Alice W. <wenyaxuan0925@outlook.com>
2024-09-12 23:08:14 -0400

    add pipe with chunk

--- tests/ipc_performance/total_runtime_2/benchmark.sh
+++ tests/ipc_performance/total_runtime_2/benchmark.sh
@@ -3,11 +3,17 @@
 mkdir -p data/
 ./compile.sh
 
+# echo "Running on lind stack"
+# python3 gen_pipedata.py -c $1 -p "lind /pipe"
+
+# echo -e "\nRunning on native linux"
+# python3 gen_pipedata.py -c $1 -p "scripts/pipe"
+
 echo "Running on lind stack"
-python3 gen_pipedata.py -c $1 -p "lind /pipe"
+python3 gen_pipechunk.py -c $1 -p "lind /pipe"
 
 echo -e "\nRunning on native linux"
-python3 gen_pipedata.py -c $1 -p "scripts/pipe"
+python3 gen_pipechunk.py -c $1 -p "scripts/pipe"
 
 echo -e "\nCleaning up"
 ./cleanup.sh
