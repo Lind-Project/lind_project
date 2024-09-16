920ab9bd77e3daf85f0dc8760a576e9137743681
Yashaswi Makula <yashaswi.makula@gmail.com>
2024-09-16 14:47:16 -0400

    Update compile.sh
    fixes for build

--- tests/ipc_performance/total_runtime/compile.sh
+++ tests/ipc_performance/total_runtime/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Compiling native test binaries"
-scripts/compile-native.sh
+./scripts/compile-native.sh
 
 echo "Compiling lind / rawposix test binaries"
-scripts/compile-lind.sh
+./scripts/compile-lind.sh
 
 echo "Compiling unsafe test binaries"
-scripts/compile-unsafe.sh
+./scripts/compile-unsafe.sh
