3ff0f42bc89ce7dc463f5e31c2d694af44fe2e44
Alice W. <wenyaxuan0925@outlook.com>
2024-09-18 19:50:31 -0400

    Fix type

--- src/scripts/rawposix/rawposix_install.sh
+++ src/scripts/rawposix/rawposix_install.sh
@@ -4,8 +4,9 @@ rm -rf /home/lind/lind_project/src/safeposix-rust
 cd /home/lind/lind_project/src/
 git clone https://github.com/Lind-Project/RawPOSIX.git
 mv /home/lind/lind_project/src/RawPOSIX /home/lind/lind_project/src/safeposix-rust
+
 # Replace Makefile by new one
-cp /home/lind/lind_project/tests/ipc_performance/total_runtime/rawposix_mklind.sh /home/lind/lind_project/src/mklind
+cp /home/lind/lind_projec/lind_project/src/scripts/rawposix/rawposix_mklind.sh /home/lind/lind_project/src/mklind
 
 /home/lind/lind_project/src/mklind rustposix
 /home/lind/lind_project/src/mklind install
