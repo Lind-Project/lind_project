f189958fb7ab74625b2448ab0c82376aa7511cbc
Alice W. <wenyaxuan0925@outlook.com>
2024-09-18 19:57:51 -0400

    Refine rawposix compilation

--- src/scripts/rawposix/rawposix_install.sh
+++ src/scripts/rawposix/rawposix_install.sh
@@ -5,9 +5,6 @@ cd /home/lind/lind_project/src/
 git clone https://github.com/Lind-Project/RawPOSIX.git
 mv /home/lind/lind_project/src/RawPOSIX /home/lind/lind_project/src/safeposix-rust
 
-# Replace Makefile by new one
-cp /home/lind/lind_projec/lind_project/src/scripts/rawposix/rawposix_mklind.sh /home/lind/lind_project/src/mklind
-
-/home/lind/lind_project/src/mklind rustposix
-/home/lind/lind_project/src/mklind install
+/home/lind/lind_projec/lind_project/src/scripts/rawposix/rawposix_mklind.sh rustposix
+/home/lind/lind_projec/lind_project/src/scripts/rawposix/rawposix_mklind.sh install
 
