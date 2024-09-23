c31d8b687646e9ea9655ae985bfde5f9e567df0a
Alice W. <wenyaxuan0925@outlook.com>
2024-09-23 19:27:30 -0400

    Add loaing to lindfs

--- tests/ipc_performance/sfi_overhead/compile.sh
+++ tests/ipc_performance/sfi_overhead/compile.sh
@@ -4,6 +4,9 @@ echo "Compiling lind SFI test binaries"
 x86_64-nacl-gcc scripts/write.c -o scripts/write.nexe -lrt -std=gnu99 -O3
 x86_64-nacl-gcc scripts/close.c -o scripts/close.nexe -lrt -std=gnu99 -O3
 x86_64-nacl-gcc scripts/getpid.c -o scripts/getpid.nexe -lrt -std=gnu99 -O3
+lindfs cp /home/lind/lind_project/tests/ipc_performance/sfi_overhead/scripts/write.nexe /write.nexe
+lindfs cp /home/lind/lind_project/tests/ipc_performance/sfi_overhead/scripts/close.nexe /close.nexe
+lindfs cp /home/lind/lind_project/tests/ipc_performance/sfi_overhead/scripts/getpid.nexe /getpid.nexe
 
 echo "Compiling native SFI test binaries"
 /usr/local/gcc-4.4.3/bin/gcc scripts/write.c -o scripts/write -lrt -std=gnu99 -O3
