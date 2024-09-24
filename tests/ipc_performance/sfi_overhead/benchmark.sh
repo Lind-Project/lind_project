2ca8efb1841e8284878d1b4d1204a9beedc45995
Alice W. <wenyaxuan0925@outlook.com>
2024-09-24 10:30:48 -0400

    overall testing

--- tests/ipc_performance/sfi_overhead/benchmark.sh
+++ tests/ipc_performance/sfi_overhead/benchmark.sh
@@ -54,66 +54,66 @@ echo '  }' >> data/write_times.json
 echo '}' >> data/write_times.json
 
 #####################################################################################
-# # ------
-# # Use regular RustPOSIX
-# ./rustposix.sh
-# # ------
+# ------
+# Use regular RustPOSIX
+./rustposix.sh
+# ------
 
-# echo "SFI test -- close()"
+echo "SFI test -- close()"
 
-# # Initialization
-# echo '{' > data/close_result.json
-# close_native_times=()
-# close_lind_times=()
+# Initialization
+echo '{' > data/close_result.json
+close_native_times=()
+close_lind_times=()
 
-# for ((i=1; i<=$1; i++)); do
-#     echo "----- native -----"
-#     native_close_output=$(scripts/close | grep 'average time' | awk '{print $6}')
-#     close_native_times+=($native_close_output)
-#     echo "----- lind -----"
-#     lind_close_output=$(lind /close.nexe | grep 'average time' | awk '{print $6}')
-#     close_lind_times+=($lind_close_output)
-# done
+for ((i=1; i<=$1; i++)); do
+    echo "----- native -----"
+    native_close_output=$(scripts/close | grep 'average time' | awk '{print $6}')
+    close_native_times+=($native_close_output)
+    echo "----- lind -----"
+    lind_close_output=$(lind /close.nexe | grep 'average time' | awk '{print $6}')
+    close_lind_times+=($lind_close_output)
+done
 
-# # Write results to json file
-# echo '  "native": [' >> data/close_result.json
-# for time in "${close_native_times[@]}"; do
-#     echo "    $time," >> data/close_result.json
-# done
-# echo '  ],' >> data/close_result.json
+# Write results to json file
+echo '  "native": [' >> data/close_result.json
+for time in "${close_native_times[@]}"; do
+    echo "    $time," >> data/close_result.json
+done
+echo '  ],' >> data/close_result.json
 
-# echo '  "lind": [' >> data/close_result.json
-# for time in "${close_lind_times[@]}";  do
-#     echo "    $time," >> data/close_result.json
-# done
-# echo '  ]' >> data/close_result.json
-# echo '}' >> data/close_result.json
-# #####################################################################################
+echo '  "lind": [' >> data/close_result.json
+for time in "${close_lind_times[@]}";  do
+    echo "    $time," >> data/close_result.json
+done
+echo '  ]' >> data/close_result.json
+echo '}' >> data/close_result.json
+#####################################################################################
 
-# echo "SFI test -- getpid()"
+echo "SFI test -- getpid()"
 
-# echo '{' > data/getpid_result.json
-# getpid_native_times=()
-# getpid_lind_times=()
-# for ((i=1; i<=$1; i++)); do
-#     echo "----- native -----"
-#     native_getpid_output=$(scripts/getpid | grep 'average time' | awk '{print $6}')
-#     getpid_native_times+=($native_getpid_output)
-#     echo "----- lind -----"
-#     lind_getpid_output=$(lind /getpid.nexe | grep 'average time' | awk '{print $6}')
-#     getpid_lind_times+=($lind_getpid_output)
-# done
+echo '{' > data/getpid_result.json
+getpid_native_times=()
+getpid_lind_times=()
+for ((i=1; i<=$1; i++)); do
+    echo "----- native -----"
+    native_getpid_output=$(scripts/getpid | grep 'average time' | awk '{print $6}')
+    getpid_native_times+=($native_getpid_output)
+    echo "----- lind -----"
+    lind_getpid_output=$(lind /getpid.nexe | grep 'average time' | awk '{print $6}')
+    getpid_lind_times+=($lind_getpid_output)
+done
 
-# # Write results to json file
-# echo '  "native": [' >> data/getpid_result.json
-# for time in "${getpid_native_times[@]}"; do
-#     echo "    $time," >> data/getpid_result.json
-# done
-# echo '  ],' >> data/getpid_result.json
+# Write results to json file
+echo '  "native": [' >> data/getpid_result.json
+for time in "${getpid_native_times[@]}"; do
+    echo "    $time," >> data/getpid_result.json
+done
+echo '  ],' >> data/getpid_result.json
 
-# echo '  "lind": [' >> data/getpid_result.json
-# for time in "${getpid_lind_times[@]}"; do
-#     echo "    $time," >> data/getpid_result.json
-# done
-# echo '  ],' >> data/getpid_result.json
-# echo '}' >> data/getpid_result.json
+echo '  "lind": [' >> data/getpid_result.json
+for time in "${getpid_lind_times[@]}"; do
+    echo "    $time," >> data/getpid_result.json
+done
+echo '  ],' >> data/getpid_result.json
+echo '}' >> data/getpid_result.json
