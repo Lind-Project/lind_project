1023d2f321bd18718a11c95d939f3598cd9a852a
Alice W. <wenyaxuan0925@outlook.com>
2024-09-24 09:22:17 -0400

    fix location issue

--- ipc_test.sh
+++ ipc_test.sh
@@ -3,7 +3,8 @@
 # This tests will use regular RustPOSIX and Native linux only, so we run this test
 # first with pre-built binaries
 echo "=============== Running Unix Domain Socket v.s. Pipe Tests ==============="
-/home/lind/lind_project/tests/ipc_performance/buf_allocate/benchmark.sh $1
+cd /home/lind/lind_project/tests/ipc_performance/buf_allocate/
+./benchmark.sh $1
 
 # Setup rust compiler in sudo mode
 rustup default nightly
@@ -12,10 +13,12 @@ rustup default nightly
 # second run. Native / Unsafe / Lind + RustPOSIX wil run first and then benchmark.sh
 # inside total_runtime folder will automatically recompile and reinstall RawPOSIX
 echo "=============== Running IPC Total Runtime Tests ==============="
-/home/lind/lind_project/tests/ipc_performance/total_runtime/benchmark.sh $1
+cd /home/lind/lind_project/tests/ipc_performance/total_runtime/
+./benchmark.sh $1
 
 # We will need to run write() with RustPOSIX under branch `libc-write`, but close()
 # and getpid() with regular compiled RustPOSIX. So at first we'll re-compile the 
 # RustPOSIX under normal then compile again to see the 
 echo "=============== Running SFI Tests ==============="
-/home/lind/lind_project/tests/ipc_performance/sfi_overhead/benchmark.sh $1
+cd /home/lind/lind_project/tests/ipc_performance/sfi_overhead/
+./benchmark.sh $1
