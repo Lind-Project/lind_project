7cb0929f5d11b297b9a707aec36645be6b4eb73a
Alice W. <wenyaxuan0925@outlook.com>
2024-09-23 20:36:13 -0400

    debug

--- tests/ipc_performance/sfi_overhead/benchmark.sh
+++ tests/ipc_performance/sfi_overhead/benchmark.sh
@@ -8,48 +8,48 @@ mkdir outputs/
 
 #####################################################################################
 
-echo "SFI test -- write()"
+# echo "SFI test -- write()"
 
-echo "----- native -----"
-scripts/write 1 > outputs/nat_1_write.txt
-scripts/write 16 > outputs/nat_16_write.txt
-scripts/write 256 > outputs/nat_256_write.txt
-scripts/write 4096 > outputs/nat_4096_write.txt
-scripts/write 65536 > outputs/nat_65536_write.txt
-echo "----- lind -----"
-lind /write.nexe 1 > outputs/lind_1_write.txt
-lind /write.nexe 16 > outputs/lind_16_write.txt
-lind /write.nexe 256 > outputs/lind_256_write.txt
-lind /write.nexe 4096 > outputs/lind_4096_write.txt
-lind /write.nexe 65536 > outputs/lind_65536_write.txt
+# echo "----- native -----"
+# scripts/write 1 > outputs/nat_1_write.txt
+# scripts/write 16 > outputs/nat_16_write.txt
+# scripts/write 256 > outputs/nat_256_write.txt
+# scripts/write 4096 > outputs/nat_4096_write.txt
+# scripts/write 65536 > outputs/nat_65536_write.txt
+# echo "----- lind -----"
+# lind /write.nexe 1 > outputs/lind_1_write.txt
+# lind /write.nexe 16 > outputs/lind_16_write.txt
+# lind /write.nexe 256 > outputs/lind_256_write.txt
+# lind /write.nexe 4096 > outputs/lind_4096_write.txt
+# lind /write.nexe 65536 > outputs/lind_65536_write.txt
 
-echo '{' > data/write_times.json
+# echo '{' > data/write_times.json
 
-# Lind
-echo '  "lind": {' >> data/write_times.json
-for file in outputs/lind_*_write.txt
-do
-    buffer_size=$(tail -n 1 "$file" | awk '{print $3}')
-    avg_time=$(tail -n 1 "$file" | awk '{print $9}')
-    echo "    \"$buffer_size\": $avg_time," >> data/write_times.json
-done
-# Remove last
-sed -i '$ s/,$//' data/write_times.json
-echo '  },' >>data/ write_times.json
+# # Lind
+# echo '  "lind": {' >> data/write_times.json
+# for file in outputs/lind_*_write.txt
+# do
+#     buffer_size=$(tail -n 1 "$file" | awk '{print $3}')
+#     avg_time=$(tail -n 1 "$file" | awk '{print $9}')
+#     echo "    \"$buffer_size\": $avg_time," >> data/write_times.json
+# done
+# # Remove last
+# sed -i '$ s/,$//' data/write_times.json
+# echo '  },' >>data/ write_times.json
 
-# Native
-echo '  "native": {' >> data/write_times.json
-for file in outputs/nat_*_write.txt
-do
-    buffer_size=$(tail -n 1 "$file" | awk '{print $3}')
-    avg_time=$(tail -n 1 "$file" | awk '{print $9}')
-    echo "    \"$buffer_size\": $avg_time," >> data/write_times.json
-done
-# Remove
-sed -i '$ s/,$//' data/write_times.json
-echo '  }' >> data/write_times.json
+# # Native
+# echo '  "native": {' >> data/write_times.json
+# for file in outputs/nat_*_write.txt
+# do
+#     buffer_size=$(tail -n 1 "$file" | awk '{print $3}')
+#     avg_time=$(tail -n 1 "$file" | awk '{print $9}')
+#     echo "    \"$buffer_size\": $avg_time," >> data/write_times.json
+# done
+# # Remove
+# sed -i '$ s/,$//' data/write_times.json
+# echo '  }' >> data/write_times.json
 
-echo '}' >> data/write_times.json
+# echo '}' >> data/write_times.json
 
 #####################################################################################
 
