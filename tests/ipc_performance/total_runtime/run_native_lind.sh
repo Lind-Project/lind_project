3a12f4bc6c2bf507b91b529d0569d88d209db108
Alice W. <wenyaxuan0925@outlook.com>
2024-09-25 14:57:23 -0400

    only runtime scripts

--- tests/ipc_performance/total_runtime/run_native_lind.sh
+++ tests/ipc_performance/total_runtime/run_native_lind.sh
@@ -1,13 +1,6 @@
 #!/bin/bash    
 
-scripts/compile-lind.sh
-
 mkdir data/
-
-echo "Loading files into file system of Native-lind"
-lindfs cp $PWD/scripts/pipescript.sh /pipescript.sh
-lindfs cp $PWD/scripts/writepipe.nexe /writepipe
-lindfs cp $PWD/scripts/readpipe.nexe /readpipe
 
 echo -e "\nRunning with: -p lind /bin/bash /pipescript.sh -w 16 -r x -c $1"
 python3 gen_pipedata.py -w 16 -r x -p "lind /bin/bash /pipescript.sh" -c $1
