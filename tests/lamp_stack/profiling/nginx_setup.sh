ffc2a3a0c174ae0aeb3301dc80d129b05195f9e3
Alice W. <wenyaxuan0925@outlook.com>
2024-09-27 14:38:36 -0400

    nginx native

--- tests/lamp_stack/profiling/nginx_setup.sh
+++ tests/lamp_stack/profiling/nginx_setup.sh
@@ -3,7 +3,8 @@
 cd /home/lind/lind_project/tests/applications/nginx
 ./auto/configure
 make
-sudo make install DESTDIR=/usr/local/nginx
-cp /home/lind/lind_project/src/scripts/nginx/nginx-conf/nginx.conf /usr/local/nginx/conf/nginx.conf
-touch /usr/local/nginx/logs/access.log
-touch /usr/local/nginx/logs/error.log
+sudo make install
+sudo cp /home/lind/lind_project/src/scripts/nginx/nginx-conf/nginx.conf /usr/local/nginx/conf/nginx.conf
+sudo cp /home/lind/lind_project/src/scripts/nginx/nginx-conf/nginx_static.conf /usr/local/nginx/conf/nginx_static.conf
+sudo touch /usr/local/nginx/logs/access.log
+sudo touch /usr/local/nginx/logs/error.log
