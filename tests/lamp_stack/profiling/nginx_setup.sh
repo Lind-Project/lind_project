2d9a85bc7c996b6a4a63a3d247e544165603cf19
Alice W. <wenyaxuan0925@outlook.com>
2024-09-27 14:10:50 -0400

    nginx native

--- tests/lamp_stack/profiling/nginx_setup.sh
+++ tests/lamp_stack/profiling/nginx_setup.sh
@@ -3,7 +3,7 @@
 cd /home/lind/lind_project/tests/applications/nginx
 ./auto/configure
 make
-make install DESTDIR=/home/lind/nginx
-cp /home/lind/lind_project/src/scripts/nginx/nginx-conf/nginx.conf /home/lind/nginx/usr/local/nginx/conf/nginx.conf
-touch /home/lind/nginx/usr/local/nginx/logs/access.log
-touch /home/lind/nginx/usr/local/nginx/logs/error.log
+sudo make install DESTDIR=/usr/local/nginx
+cp /home/lind/lind_project/src/scripts/nginx/nginx-conf/nginx.conf /usr/local/nginx/conf/nginx.conf
+touch /usr/local/nginx/logs/access.log
+touch /usr/local/nginx/logs/error.log
--- tests/lamp_stack/profiling/run_lamp_native.sh
+++ tests/lamp_stack/profiling/run_lamp_native.sh
@@ -3,13 +3,13 @@
 /usr/local/pgsql/bin/postgres -F -d 0 -c listen_addresses='' -D /usr/local/pgsql/data/ &
 sleep 15
 echo "postgres ready"
-/bin/python init_table.py
+/bin/python flask_app/init_table.py
 sleep 3
 echo "table ready"
-/home/lind/nginx/usr/local/nginx/sbin/nginx -c /home/lind/nginx/usr/local/nginx/conf/nginx.conf -p /home/lind/nginx/usr/local/nginx/
+sudo /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx_static.conf -p /usr/local/nginx/
 sleep 5
 echo "nginx ready"
-cd flask-app/
-/bin/python gunicornexec.py --bind 0.0.0.0:8000 wsgi:app 2>&1
+cd flask_app/
+/bin/python2.7 gunicornexec.py --bind=unix:/tmp/gunicorn.sock wsgi:app
 sleep 5
 echo "system ready"
