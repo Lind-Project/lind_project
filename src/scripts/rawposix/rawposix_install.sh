089a128211ea42172ad04e5a6e0e988819817793
Alice W. <wenyaxuan0925@outlook.com>
2024-10-01 19:56:01 -0400

    switch to use stable version of rawposix

--- src/scripts/rawposix/rawposix_install.sh
+++ src/scripts/rawposix/rawposix_install.sh
@@ -2,8 +2,10 @@
 # Replace RustPOSIX by RawPOSIX
 rm -rf /home/lind/lind_project/src/safeposix-rust
 cd /home/lind/lind_project/src/
-git clone https://github.com/Lind-Project/RawPOSIX.git
-mv /home/lind/lind_project/src/RawPOSIX /home/lind/lind_project/src/safeposix-rust
+git clone https://github.com/JustinCappos/rawposix.git
+mv /home/lind/lind_project/src/rawposix /home/lind/lind_project/src/safeposix-rust
+cd safeposix-rust
+git checkout alice
 
 /home/lind/lind_project/src/scripts/rawposix/rawposix_mklind.sh rustposix
 /home/lind/lind_project/src/scripts/rawposix/rawposix_mklind.sh install
