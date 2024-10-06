68a490a76068c4d8f271a420ff6132dac9ae1a83
Alice W. <wenyaxuan0925@outlook.com>
2024-10-06 10:11:02 -0400

    Change to use gcc-4 to compile

--- tests/lamp_stack/profiling/nginx_setup.sh
+++ tests/lamp_stack/profiling/nginx_setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    if [ "$DISTRIB_ID" = "Ubuntu" ]; then
+        export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
+    fi
+fi
+
 cd /home/lind/lind_project/tests/applications/nginx
-./auto/configure
+./auto/configure CC=/usr/local/gcc-4.4.3/bin/gcc CXX=/usr/local/gcc-4.4.3/bin/g++
 make
 sudo make install
 sudo cp /home/lind/lind_project/src/scripts/nginx/nginx-conf/nginx.conf /usr/local/nginx/conf/nginx.conf
--- tests/lamp_stack/profiling/pg_setup.sh
+++ tests/lamp_stack/profiling/pg_setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    if [ "$DISTRIB_ID" = "Ubuntu" ]; then
+        export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
+    fi
+fi
+
 cd /home/lind/lind_project/tests/applications/postgres/
 
-./configure
+./configure CC=/usr/local/gcc-4.4.3/bin/gcc CXX=/usr/local/gcc-4.4.3/bin/g++
 make
 sudo make install
 sudo mkdir -m 770 /usr/local/pgsql/data/
