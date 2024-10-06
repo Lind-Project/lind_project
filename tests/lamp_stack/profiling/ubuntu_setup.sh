38c7aa97c6af048ec89b26b76d88ff9c3e7d519d
Alice W. <wenyaxuan0925@outlook.com>
2024-10-06 10:54:34 -0400

    Add ubuntu22.04 setup script for lampstack

---
+++ tests/lamp_stack/profiling/ubuntu_setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y curl git vim build-essential libreadline-dev libpcre3 libpcre3-dev wrk python3 python3-pip python2.7
+pip2 install gunicorn psycopg2-binary Flask
