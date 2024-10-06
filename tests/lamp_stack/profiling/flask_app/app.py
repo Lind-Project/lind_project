691d754952baa67838f490f4ddeac418553daebb
Alice W. <wenyaxuan0925@outlook.com>
2024-10-06 10:52:35 -0400

    change flask app.py

--- tests/lamp_stack/profiling/flask_app/app.py
+++ tests/lamp_stack/profiling/flask_app/app.py
@@ -1,8 +1,7 @@
 import psycopg2
 import string
 import random
-
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 html_size_128KBs = 2 ** 0
@@ -10,46 +9,35 @@ size = 2 ** 16
 num_pages = int((html_size_128KBs * (2 ** 17)) / (2 * size)) # size * num_pages = html_size_128KBs
 conn = psycopg2.connect(database="postgres", user="lind", host="/tmp")
 
+def get_db_connection():
+    conn = psycopg2.connect(database="postgres", user="lind", host="/tmp")
+    return conn
 
-def rand_generator(size=size, chars=string.ascii_uppercase + string.digits):
-    return "".join(random.choice(chars) for _ in range(size - 1))
-
-
-data = []
-for n in range(num_pages):
-    title = rand_generator()
-    review = rand_generator()
-    data.append((title, n, review))
-
-
-@app.route("/")
-def index():
+def _get_random_row():
+    conn = get_db_connection()
     cur = conn.cursor()
-
-    for n in range(num_pages):
-        cur.execute(
-            "INSERT INTO books (title, pages_num, review)"
-            "VALUES (%s, %s, %s)",
-            data[n],
-        )
-        if n % 5 == 0:
-            conn.commit()
-    conn.commit()
-
-    books = []
-    for n in range(num_pages):
-        cur.execute("SELECT * FROM books WHERE pages_num = %s", (n,))
-        books.append(cur.fetchone())
-
-    for n in range(num_pages):
-        cur.execute("DELETE FROM books WHERE pages_num = %s", (n,))
-        if n % 5 == 0:
-            conn.commit()
-    conn.commit()
-
+    value = random.randint(1, 10000)
+    cur.execute(f'SELECT * FROM world WHERE ID={value};')
+    result = cur.fetchall()
     cur.close()
-    return render_template("index.html", books=books)
+    conn.close()
+    return result
 
+@app.route('/db')
+def db():
+    result = _get_random_row()
+    return jsonify(result)
+
+@app.route('/queries')
+def queries():
+    num_queries = int(request.args.get('queries'))
+    num_queries = min(500, max(num_queries, 1))
+    result = [_get_random_row()[0] for i in range(num_queries)]
+    return jsonify(result)
+
+@app.route('/plaintext')
+def plaintext():
+    return 'Hello, world'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
