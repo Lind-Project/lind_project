194fb551f497a64dff4440e78ff182a8cc51c071
Alice W. <wenyaxuan0925@outlook.com>
2024-10-06 13:51:20 -0400

    Add initialization of postgres table

--- tests/lamp_stack/profiling/flask_app/app.py
+++ tests/lamp_stack/profiling/flask_app/app.py
@@ -4,10 +4,21 @@ import random
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
 html_size_128KBs = 2 ** 0
 size = 2 ** 16
 num_pages = int((html_size_128KBs * (2 ** 17)) / (2 * size)) # size * num_pages = html_size_128KBs
-conn = psycopg2.connect(database="postgres", user="lind", host="/tmp")
+
+
+def rand_generator(size=size, chars=string.ascii_uppercase + string.digits):
+    return "".join(random.choice(chars) for _ in range(size - 1))
+
+# Initialize data table
+data = []
+for n in range(num_pages):
+    title = rand_generator()
+    review = rand_generator()
+    data.append((title, n, review))
 
 def get_db_connection():
     conn = psycopg2.connect(database="postgres", user="lind", host="/tmp")
@@ -17,7 +28,7 @@ def _get_random_row():
     conn = get_db_connection()
     cur = conn.cursor()
     value = random.randint(1, 10000)
-    cur.execute('SELECT * FROM world WHERE ID=%s;' % value)
+    cur.execute('SELECT * FROM data WHERE ID=%s;' % value)
     result = cur.fetchall()
     cur.close()
     conn.close()
@@ -41,4 +52,3 @@ def plaintext():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-    conn.close()
