050ec0a50fe55b6a07fe1b5245c81d4a1a489322
Alice W. <wenyaxuan0925@outlook.com>
2024-10-08 18:38:07 -0400

    remove unused lines and convert back to older version

--- tests/lamp_stack/profiling/flask_app/app.py
+++ tests/lamp_stack/profiling/flask_app/app.py
@@ -1,18 +1,10 @@
 import psycopg2
-import string
 import random
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-html_size_128KBs = 2 ** 0
-size = 2 ** 16
-num_pages = int((html_size_128KBs * (2 ** 17)) / (2 * size))  # size * num_pages = html_size_128KBs
-
 conn = psycopg2.connect(database="postgres", user="lind", host="/tmp")
-
-def rand_generator(size=size, chars=string.ascii_uppercase + string.digits):
-    return "".join(random.choice(chars) for _ in range(size - 1))
 
 def _get_random_row():
     cur = conn.cursor()
@@ -31,22 +23,13 @@ def db():
     # recognize and parse it.
     return jsonify(result)
 
-# Check the average queries size return (just printout)
 @app.route('/queries')
 def queries():
     power = int(request.args.get('power', 16))
 
-    cur = conn.cursor()
-
     # The average size of each query is 16 bytes
     num_queries = 2**(power-4)
-    for _ in range(num_queries):
-        value = random.randint(1, 10000)
-        cur.execute('SELECT * FROM world WHERE id = %s;', (value,))
-        result = cur.fetchall()
-        result = [_get_random_row()[0] for _ in range(num_queries)]
-
-    cur.close()
+    result = [_get_random_row()[0] for _ in range(num_queries)]
 
     return jsonify(result)
 
@@ -78,5 +61,5 @@ def plaintext():
     return response_string
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
     conn.close()
