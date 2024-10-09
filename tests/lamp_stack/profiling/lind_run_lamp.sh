93032cf7303067336c1bf6237d3aa4ead0955475
Alice W. <wenyaxuan0925@outlook.com>
2024-10-08 20:10:02 -0400

    Add scripts for lind/native nginx and lamp

--- tests/lamp_stack/profiling/lind_run_lamp.sh
+++ tests/lamp_stack/profiling/lind_run_lamp.sh
@@ -6,7 +6,7 @@ echo "postgres ready"
 /bin/python /init_table.py
 sleep 3
 echo "table ready"
-/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx_static.conf -p /usr/local/nginx/
+/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf -p /usr/local/nginx/
 sleep 5
 echo "nginx ready"
 /bin/python gunicornexec.py --bind unix:/tmp/gunicorn.sock wsgi:app 2>&1
