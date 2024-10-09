cf01e2f639c992f5bdb21cdb56a170cdafe29be8
Alice W. <wenyaxuan0925@outlook.com>
2024-10-08 21:43:37 -0400

    update lind setup file

--- tests/lamp_stack/profiling/lind_setup.sh
+++ tests/lamp_stack/profiling/lind_setup.sh
@@ -7,7 +7,6 @@ rm *
 echo -e "Loading LAMP stack\n"
 /home/lind/lind_project/src/scripts/lamp/load_lamp.sh > /dev/null
 /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/load.sh > /dev/null
-lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/lind_run_nginx.sh /lind_run_nginx.sh
 lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/lind_run_lamp.sh /lind_run_lamp.sh
 
 echo -e "Initializing Postgres"
