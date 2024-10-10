ee664acbf53291c51103a0c5e4e8bbd2e9787dd5
Alice W. <wenyaxuan0925@outlook.com>
2024-10-10 18:48:27 -0400

    flask app: loop in Python instead of batch the data for the query

--- tests/lamp_stack/profiling/flask_app/app.py
+++ tests/lamp_stack/profiling/flask_app/app.py
@@ -16,15 +16,14 @@ def _get_random_rows(loops):
     # Calculate how many loops do we want when exceeding 1000
     for _ in range(0, loops):
 
-        ids = tuple(random.randint(0, 1000) for _ in range(batch_size))
+        # Generate a random ID
+        random_id = random.randint(0, 1000)
+        
+        # Query for each individual ID
+        query = 'SELECT * FROM world WHERE id = %s;'
+        cur.execute(query, (random_id,))
 
-        # It will meet syntax error with ","
-        if len(ids) == 1:
-            query = 'SELECT * FROM world WHERE id = {};'.format(ids[0])
-        else:
-            query = 'SELECT * FROM world WHERE id IN {};'.format(ids)
-        cur.execute(query)
-
+        # Append the result of each query to the results list
         results.extend(cur.fetchall())
 
 
