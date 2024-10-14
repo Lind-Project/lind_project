90e8fb267683af244a982a4569be95c0a636cf6e
Alice W. <wenyaxuan0925@outlook.com>
2024-10-13 20:07:43 -0400

    update native lamp runtime script and update loading script

--- tests/lamp_stack/profiling/nat_run_lamp.sh
+++ tests/lamp_stack/profiling/nat_run_lamp.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+export PATH=/usr/local/python-gcc4/bin/:$PATH
+
 /usr/local/pgsql/bin/postgres -F -d 0 -c listen_addresses='' -D /usr/local/pgsql/data/ &
 sleep 15
 echo "postgres ready"
-/bin/python flask_app/init_table.py
+/usr/local/python-gcc4/bin/python flask_app/init_table.py
 sleep 3
 echo "table ready"
 sudo /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf -p /usr/local/nginx/
 sleep 5
 echo "nginx ready"
 cd flask_app/
-/bin/python2.7 gunicornexec.py --bind=unix:/tmp/gunicorn.sock wsgi:app
+/usr/local/python-gcc4/bin/python gunicornexec.py --bind=unix:/tmp/gunicorn.sock wsgi:app
 sleep 5
 echo "system ready"
