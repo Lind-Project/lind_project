ea96275eddb8bf25f090b8f35f0b278e0f9d2cfe
Alice W. <wenyaxuan0925@outlook.com>
2024-10-13 21:42:05 -0400

    change to normal

--- tests/lamp_stack/profiling/nat_run_lamp.sh
+++ tests/lamp_stack/profiling/nat_run_lamp.sh
@@ -5,14 +5,14 @@ export PATH=/usr/local/python-gcc4/bin/:$PATH
 /usr/local/pgsql/bin/postgres -F -d 0 -c listen_addresses='' -D /usr/local/pgsql/data/ &
 sleep 15
 echo "postgres ready"
-cd flask_app/
+# cd flask_app/
 /usr/local/python-gcc4/bin/python init_table.py
 sleep 3
 echo "table ready"
 sudo /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf -p /usr/local/nginx/
 sleep 5
 echo "nginx ready"
-# cd flask_app/
+cd flask_app/
 /usr/local/python-gcc4/bin/python gunicornexec.py --bind=unix:/tmp/gunicorn.sock wsgi:app
 # /usr/local/python-gcc4/bin/python app.py
 sleep 5
