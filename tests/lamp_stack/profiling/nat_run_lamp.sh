0feeb19a78e8f402dfa8f479eac0caf02d9e9136
Alice W. <wenyaxuan0925@outlook.com>
2024-10-13 20:43:04 -0400

    repalce tests

--- tests/lamp_stack/profiling/nat_run_lamp.sh
+++ tests/lamp_stack/profiling/nat_run_lamp.sh
@@ -5,13 +5,15 @@ export PATH=/usr/local/python-gcc4/bin/:$PATH
 /usr/local/pgsql/bin/postgres -F -d 0 -c listen_addresses='' -D /usr/local/pgsql/data/ &
 sleep 15
 echo "postgres ready"
-/usr/local/python-gcc4/bin/python flask_app/init_table.py
+cd flask_app/
+/usr/local/python-gcc4/bin/python init_table.py
 sleep 3
 echo "table ready"
 sudo /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf -p /usr/local/nginx/
 sleep 5
 echo "nginx ready"
 cd flask_app/
-/usr/local/python-gcc4/bin/python gunicornexec.py --bind=unix:/tmp/gunicorn.sock wsgi:app
+# /usr/local/python-gcc4/bin/python gunicornexec.py --bind=unix:/tmp/gunicorn.sock wsgi:app
+/usr/local/python-gcc4/bin/python app.py
 sleep 5
 echo "system ready"
