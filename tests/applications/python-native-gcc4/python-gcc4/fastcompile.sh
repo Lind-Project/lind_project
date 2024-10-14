d6066c90b67eadf5f88645a77bb1f8066dadad68
Alice W. <wenyaxuan0925@outlook.com>
2024-10-14 14:50:07 -0400

    add compilation for _sysconfigdata_nd

--- tests/applications/python-native-gcc4/python-gcc4/fastcompile.sh
+++ tests/applications/python-native-gcc4/python-gcc4/fastcompile.sh
@@ -7,8 +7,8 @@ fi
 
 mkdir -p /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib
 
-modulesarray=('_collectionsmodule' 'operator' 'itertoolsmodule' '_struct' 'mathmodule' 'binascii' 'timemodule' 'cStringIO' '_randommodule' 'arraymodule' 'socketmodule' '_functoolsmodule' 'cPickle' '_struct' 'selectmodule' 'arraymodule' 'unicodedata' 'fcntlmodule' 'grpmodule')
-libsarray=('_collections' 'operator' 'itertools' '_struct' 'math' 'binascii' 'time' 'cStringIO' '_random' 'array' '_socket' '_functools' 'cPickle' '_struct' 'select' 'array' 'unicodedata' 'fcntl' 'grp')
+modulesarray=('_collectionsmodule' 'operator' 'itertoolsmodule' '_struct' 'mathmodule' 'binascii' 'timemodule' 'cStringIO' '_randommodule' 'arraymodule' 'socketmodule' '_functoolsmodule' 'cPickle' '_struct' 'selectmodule' 'arraymodule' 'unicodedata' 'fcntlmodule' 'grpmodule' '_sysconfigdata_nd')
+libsarray=('_collections' 'operator' 'itertools' '_struct' 'math' 'binascii' 'time' 'cStringIO' '_random' 'array' '_socket' '_functools' 'cPickle' '_struct' 'select' 'array' 'unicodedata' 'fcntl' 'grp' '_sysconfigdata_nd')
 
 echo Compiling...
 for var in "${modulesarray[@]}"
