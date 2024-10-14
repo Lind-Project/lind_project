f8a399784f2d4e24f6e7525f5e10f7ff0595c4a5
Alice W. <wenyaxuan0925@outlook.com>
2024-10-14 09:23:13 -0400

    change native pg/py compilation scripts to align to lind

--- tests/applications/python-native-gcc4/python-gcc4/bootstrap_native.sh
+++ tests/applications/python-native-gcc4/python-gcc4/bootstrap_native.sh
@@ -7,5 +7,11 @@ if [ -f /etc/lsb-release ]; then
     fi
 fi
 
-./configure CC=/usr/local/gcc-4.4.3/bin/gcc CXX=/usr/local/gcc-4.4.3/bin/g++ --enable-shared "$@"
+export CC=/usr/local/gcc-4.4.3/bin/gcc
+export CXX=/usr/local/gcc-4.4.3/bin/g++
+export CFLAGS='-g -e -fPIC -fno-pie -DPY_FORMAT_LONG_LONG="ll" -O3 -std=c99'
+export CXXFLAGS='-g -fPIC -fno-pie -O3'
+export LDFLAGS='-g -fno-pie -O3'
+
+./configure --enable-shared "$@"
 make
--- tests/lamp_stack/profiling/flask_app/wsgi.py
+++ tests/lamp_stack/profiling/flask_app/wsgi.py
@@ -1,4 +1,5 @@
 from app import app
 
 if __name__ == "__main__":
+    app.env = "development"
     app.run()
