100bbe596b010a05c0824e8d018fba4de5004d27
Alice W. <wenyaxuan0925@outlook.com>
2024-10-14 17:40:07 -0400

    loading file updates

--- load_python.sh
+++ load_python.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo mkdir -p /usr/local/python-gcc4/{bin,lib,include,lib-dynload,site-packages}
+sudo mkdir -p /usr/local/python-gcc4/{bin,lib,include}
+sudo mkdir -p /usr/local/python-gcc4/lib/python2.7/{config, site-packages,lib-dynload}
+sudo mkdir -p /usr/local/python-gcc4/include/python2.7/
 
 echo "Copying libraries and configs"
 sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/python /usr/local/python-gcc4/bin/python;
@@ -8,10 +10,8 @@ sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc
 sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/libpython2.7.so.1.0 /usr/local/python-gcc4/lib/
 sudo cp -r /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/Lib /usr/local/python-gcc4/lib/
 sudo cp -r /home/lind/lind_project/tests/applications/python/Lib /usr/local/python-gcc4/lib
-# lindfs cp /home/lind/lind_project/tests/applications/python/Makefile /usr/local/lib/python2.7/config/Makefile
-sudo mkdir -p /usr/local/python-gcc4/config
-sudo cp /home/lind/lind_project/tests/applications/python/Makefile /usr/local/python-gcc4/config/
-sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/pyconfig.h /usr/local/python-gcc4/pyconfig.h
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/Makefile /usr/local/python-gcc4/lib/python2.7/config/
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/pyconfig.h /usr/local/python-gcc4/include/python2.7/pyconfig.h
 # lindfs cp /home/lind/lind_project/src/scripts/includes/passwd /etc/passwd
 
 echo "Python installed!"
@@ -24,23 +24,23 @@ echo Copying files...
 libsarray=('_collections' 'operator' 'itertools' '_struct' 'math' 'binascii' 'time' 'cStringIO' '_random' 'array' '_socket' '_functools' 'cPickle' '_struct' 'select' 'array' 'unicodedata' 'fcntl' 'grp')
 for lib in "${libsarray[@]}"
 do
-    sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/$lib.so /usr/local/python-gcc4/lib-dynload/$lib.so
+    sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/$lib.so /usr/local/python-gcc4/lib/python2.7/lib-dynload/$lib.so
 done
 
-sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/datetime.so /usr/local/python-gcc4/lib-dynload/datetime.so
-sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/_io.so /usr/local/python-gcc4/lib-dynload/_io.so
-sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/zlib.so /usr/local/python-gcc4/lib-dynload/zlib.so
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/datetime.so /usr/local/python-gcc4/lib/python2.7/lib-dynload/datetime.so
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/_io.so /usr/local/python-gcc/lib/python2.7/lib-dynload/_io.so
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/zlib.so /usr/local/python-gcc4/lib/python2.7/lib-dynload/zlib.so
 sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/Modules/zlib/libz.so.1.2.3 /usr/local/python-gcc4/libz.so.1
-sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/_ssl.so /usr/local/python-gcc4/lib-dynload/_ssl.so
-sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/_hashlib.so /usr/local/python-gcc4/lib-dynload/_hashlib.so
-sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/_ctypes.so /usr/local/python-gcc4/lib-dynload/_ctypes.so
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/_ssl.so /usr/local/python-gcc4/lib/python2.7/lib-dynload/_ssl.so
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/_hashlib.so /usr/local/python-gcc4/lib/python2.7/lib-dynload/_hashlib.so
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/_ctypes.so /usr/local/python-gcc4/lib/python2.7/lib-dynload/_ctypes.so
 sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/libffi.so /usr/local/python-gcc4/lib/libffi.so
 sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-gcc4/build/lib/libffi.so /usr/local/python-gcc4/lib/libffi.so.5
 
 # echo "Copying flask files:"
-# sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-modules/site-packages/markupsafe/_speedups.so /usr/local/python-gcc4/site-packages/_speedups.so
-# sudo cp -r /home/lind/lind_project/tests/applications/python-native-gcc4/python-modules/site-packages/ /usr/local/python-gcc4/
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-modules/site-packages/markupsafe/_speedups.so /usr/local/python-gcc4/site-packages/_speedups.so
+sudo cp -r /home/lind/lind_project/tests/applications/python-native-gcc4/python-modules/site-packages/ /usr/local/python-gcc4/lib/python2.7/
 
 # echo "Copying psycopg2..."
-# sudo mkdir -p /usr/local/python-gcc4/lib-dynload/psycopg2/
-# sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-modules/psycopg2/lib/* /usr/local/python-gcc4/lib-dynload/psycopg2/
+sudo mkdir -p /usr/local/python-gcc4/lib/python2.7/lib-dynload/psycopg2/
+sudo cp /home/lind/lind_project/tests/applications/python-native-gcc4/python-modules/psycopg2/lib/* /usr/local/python-gcc4/lib/python2.7/lib-dynload/psycopg2/
