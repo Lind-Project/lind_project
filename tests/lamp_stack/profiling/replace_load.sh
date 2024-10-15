06ff88b07e227dda387a537281385b0671f25742
Alice W. <wenyaxuan0925@outlook.com>
2024-10-14 20:57:19 -0400

    change mtime to be static

---
+++ tests/lamp_stack/profiling/replace_load.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/pgbench_accounts.txt /pgbench_accounts.txt
+lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/pgbench_branches.txt /pgbench_branches.txt
+lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/pgbench_tellers.txt /pgbench_tellers.txt
+lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/replace_app.py /replace_app.py
+lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/replace_init.py /replace_init.py
+lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/lind_replace.sh /lind_replace.sh
