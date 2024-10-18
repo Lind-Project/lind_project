3a6a3b57a2b9f4bdb247fb6bf550773d49668680
Alice W. <wenyaxuan0925@outlook.com>
2024-10-18 19:42:18 -0400

    updaet

--- tests/lamp_stack/profiling/gen_static.sh
+++ tests/lamp_stack/profiling/gen_static.sh
@@ -2,10 +2,10 @@
 
 # Execute (17, 27, 2)
 for param in {17..27..2}; do
-    /bin/python /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/gen_static_html.py -s $param -o static_${param}.html
+    python /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/gen_static_html.py -s $param -o static_${param}.html
     echo "Generated static_${param}.html"
 
-    lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/static_${param}.html /static_${param}.html
+    # lindfs cp /home/lind/lind_project/tests/lamp_stack/profiling/flask_app/static_${param}.html /static_${param}.html
     echo "Copied static_${param}.html to /static_${param}.html"
 done
 
