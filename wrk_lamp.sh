696f6ebd022f2834c71c43bc06e9a621ab4b2dd2
Alice W. <wenyaxuan0925@outlook.com>
2024-10-19 12:48:19 -0400

    update wrk

--- wrk_lamp.sh
+++ wrk_lamp.sh
@@ -14,44 +14,124 @@ platform=$3
 # Plaintext wrk runtime function
 run_wrk() {
     local output_file=$1
-    echo -e "\n --------------Start running mixed and Save to $output_file--------------\n"
 
-    echo "{" > $output_file
+
+    ##plaintext
+
+    plainoutput="${output_file}_plaintext.json"
+    echo -e "\n --------------Start running plaintext and Save to $plainoutput--------------\n"
+
+    echo "{" >> $plainoutput
 
     for power in {16..26..2}; do
-        echo "  \"$power\": [" >> $output_file
+        echo "  \"$power\": [" >> $plainoutput
+
+        for ((i=1; i<=iterations; i++)); do
+            # Execute wrk
+            output=$(wrk -t1 -c1 -d${duration} --timeout 30 http://localhost:80/plaintext?power=$power)
+            echo "executing: wrk -t1 -c1 -d${duration} --timeout 30 http://localhost:80/plaintext?power=$power"
+            # Get Transfer/sec
+            reqs_sec=$(echo "$output" | grep "Transfer/sec" | awk '{print $2}')
+            
+            echo "    $reqs_sec" >> $plainoutput
+
+            # Add ",", not add if it's last one
+            if [ $i -lt 10 ]; then
+                echo "," >> $plainoutput
+            fi
+        done
+
+        # End each label
+        echo "  ]" >> $plainoutput
+        if [ $power -lt 26 ]; then
+            echo "," >> $plainoutput
+        fi
+    done
+
+    echo "}" >> $plainoutput
+
+    echo "Plaintext Results saved to $plainoutput"
+
+
+    ##queries
+    queriesoutput="${output_file}_queries.json"
+
+    echo -e "\n --------------Start running queries and Save to $queriesoutput--------------\n"
+
+    echo "{" > $queriesoutput
+
+    for power in {16..26..2}; do
+        echo "  \"$power\": [" >> $queriesoutput
+
+        for ((i=1; i<=iterations; i++)); do
+            # Execute wrk
+            output=$(wrk -t1 -c1 -d${duration} --timeout 30 http://localhost:80/queries?power=$power)
+            echo "executing: wrk -t1 -c1 -d${duration} --timeout 30 http://localhost:80/queries?power=$power"
+            # Get Transfer/sec
+            reqs_sec=$(echo "$output" | grep "Transfer/sec" | awk '{print $2}')
+            
+            echo "    $reqs_sec" >> $queriesoutput
+
+            # Add ",", not add if it's last one
+            if [ $i -lt 10 ]; then
+                echo "," >> $queriesoutput
+            fi
+        done
+
+        # End each label
+        echo "  ]" >> $queriesoutput
+        if [ $power -lt 26 ]; then
+            echo "," >> $queriesoutput
+        fi
+    done
+
+    echo "}" >> $queriesoutput
+
+    echo "Plaintext Results saved to $queriesoutput"
+
+
+    ##mixed
+
+    mixedoutput="${output_file}_mixed.json"
+
+    echo -e "\n --------------Start running mixed and Save to $mixedoutput--------------\n"
+
+    echo "{" > $mixedoutput
+
+    for power in {16..26..2}; do
+        echo "  \"$power\": [" >> $mixedoutput
 
         for ((i=1; i<=iterations; i++)); do
             # Execute wrk
             output=$(wrk -t1 -c1 -d${duration} --timeout 30 http://localhost:80/mixed?power=$power)
             echo "executing: wrk -t1 -c1 -d${duration} --timeout 30 http://localhost:80/mixed?power=$power"
-            # Get Requests/sec
-            reqs_sec=$(echo "$output" | grep "Requests/sec" | awk '{print $2}')
+            # Get Transfer/sec
+            reqs_sec=$(echo "$output" | grep "Transfer/sec" | awk '{print $2}')
             
-            echo "    $reqs_sec" >> $output_file
+            echo "    $reqs_sec" >> $mixedoutput
 
             # Add ",", not add if it's last one
             if [ $i -lt 10 ]; then
-                echo "," >> $output_file
+                echo "," >> $mixedoutput
             fi
         done
 
         # End each label
-        echo "  ]" >> $output_file
+        echo "  ]" >> $mixedoutput
         if [ $power -lt 26 ]; then
-            echo "," >> $output_file
+            echo "," >> $mixedoutput
         fi
     done
 
-    echo "}" >> $output_file
+    echo "}" >> $mixedoutput
 
-    echo "Results saved to $output_file"
+    echo "Mixed Results saved to $mixedoutput"
 }
 
 if [[ "$platform" == "lind" ]]; then
     echo "================================ Running Lind Mixed LAMP ================================"
-    run_wrk "data/lind_lamp.json"
+    run_wrk "data/lind_lamp"
 else
     echo "================================ Running Native Mixed LAMP ================================"
-    run_wrk "data/nat_lamp.json"
+    run_wrk "data/nat_lamp"
 fi
