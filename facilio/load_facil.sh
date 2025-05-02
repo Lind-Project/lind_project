45cce13860e2ccc4d470a932bdb76b25d78e229f
lind <lind@nyu.edu>
2025-05-02 22:21:03 +0000

    load scripts

---
+++ facilio/load_facil.sh
@@ -0,0 +1,5 @@
+lindfs cp  /home/lind/lind_project/facilio/tmp/fioapp fioapp
+lindfs cp /home/lind/lind_project/facilio/nginx_facil.conf /usr/local/nginx/conf/nginx.conf
+lindfs cp /home/lind/lind_project/facilio/hitchdata2k.csv hitchdata2k.csv
+lindfs cp /home/lind/lind_project/facilio/init_table.py init_table.py
+lindfs cp /home/lind/lind_project/facilio/facil_lind_stack.sh facil_lind_stack.sh
